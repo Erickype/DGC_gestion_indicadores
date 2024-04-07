74f1d05752de3936589ce7763e80046461a5fa81
Erick Carrasco <erickype@hotmail.com>
2024-04-06 19:56:29 -0500

    Register basic page and server method

---
+++ DGC_gestion_indicadores_frontend/src/routes/register/+page.server.ts
@@ -0,0 +1,42 @@
+import { fail, redirect } from '@sveltejs/kit'
+import type { Action, Actions, PageServerLoad } from './$types'
+import type { RegisterError, RegisterRequest } from '$lib/api/model/auth/register'
+import { Register } from '$lib/api/controller/auth/auth'
+
+export const load: PageServerLoad = async () => {
+}
+
+const register: Action = async ({ request }) => {
+    const data = await request.formData()
+    const username = data.get('username')
+    const email = data.get('email')
+    const password = data.get('password')
+
+    if (
+        typeof username !== 'string' ||
+        typeof password !== 'string' ||
+        typeof email !== 'string' ||
+        !username ||
+        !email ||
+        !password
+    ) {
+        return fail(400, { invalid: true })
+    }
+
+    const registerRequest: RegisterRequest = {
+        username: username,
+        email: email,
+        password: password
+    }
+
+    const res = await Register(registerRequest)
+
+    if(!res.ok){
+        const err: RegisterError = await res.json()
+        return fail(res.status, {error: err.error})
+    }
+
+    redirect(303, "/login")
+}
+
+export const actions: Actions = { register }
---
+++ DGC_gestion_indicadores_frontend/src/routes/register/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import Avatar from '$lib/icons/avatar.svelte';
+	import Key from '$lib/icons/key.svelte';
+	import Letter from '$lib/icons/letter.svelte';
+	import type { ActionData } from './$types';
+
+	export let form: ActionData;
+</script>
+
+<h1 class="text-4xl font-bold">Registrarse</h1>
+
+<form action="?/register" method="POST">
+	<label class="input input-bordered flex items-center gap-2" for="username">
+		<Avatar customClass="w-4 h-4 opacity-70"></Avatar>
+		<input class="glow" placeholder="Usuario" id="username" name="username" type="text" required />
+	</label>
+
+	<label class="input input-bordered flex items-center gap-2" for="email">
+		<Letter customClass="w-4 h-4 opacity-70"></Letter>
+		<input class="glow" placeholder="Email" id="email" name="email" type="email" required />
+	</label>
+
+	<label class="input input-bordered flex items-center gap-2" for="password">
+		<Key customClass="w-4 h-4 opacity-70"></Key>
+		<input
+			class="glow"
+			placeholder="Password"
+			id="password"
+			name="password"
+			type="password"
+			required
+		/>
+	</label>
+
+	{#if form?.error}
+		<p class="error">{form?.error}</p>
+	{/if}
+
+	<button class="btn btn-success" type="submit">Enviar</button>
+</form>
