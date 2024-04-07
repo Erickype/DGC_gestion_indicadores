6dbefc534734df2abdbda8e2dc88aa773b2d77ff
Erick Carrasco <erickype@hotmail.com>
2024-04-07 01:20:26 -0500

    Navbar using store

--- DGC_gestion_indicadores_frontend/src/routes/+layout.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/+layout.svelte
@@ -1,7 +1,30 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
+	import { page } from '$app/stores';
 	import '../app.css';
 </script>
 
-<div class="min-h-screen flex items-center justify-center">
-	<slot />
+<div class="navbar bg-primary">
+	<div class="flex-1">
+		{#if $page.data.user}
+			<a href="/admin" class="btn btn-ghost text-lg">Admin</a>
+		{/if}
+	</div>
+	<div class="flex-none">
+		{#if !$page.data.user}
+			<div class="gap-2">
+				<a href="/login" class="btn btn-ghost text-lg">Ingresar</a>
+				<a href="/register" class="btn btn-ghost text-lg">Registrarse</a>
+			</div>
+		{/if}
+		{#if $page.data.user}
+			<form action="/logout" method="post" use:enhance>
+				<button class="btn btn-ghost" type="submit">Salir</button>
+			</form>
+		{/if}
+	</div>
 </div>
+
+<main>
+	<slot />
+</main>
