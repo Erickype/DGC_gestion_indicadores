6fe451d0b3e79ec20a3999cd223cb99d6fec14aa
Erick Carrasco <erickype@hotmail.com>
2024-04-07 12:51:13 -0500

    Any unauthorized role will be redirected to "/"

--- DGC_gestion_indicadores_frontend/src/routes/(admin)/admin/+page.server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(admin)/admin/+page.server.ts
@@ -2,15 +2,11 @@ import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals }) => {
-    if (!locals.user) {
+    if(!locals.user){
         throw redirect(302, "/login")
     }
 
-    const role = locals.user.role
-    switch (role) {
-        case 2:
-            throw redirect(302, "/information")
-        case 3:
-            throw redirect(302, "/")
+    if(locals.user.role !== 1){
+        throw redirect(302, "/")
     }
 };
