20c44b615b684cd3cc9818c4c3a41ff20be5ef12
Erick Carrasco <erickype@hotmail.com>
2024-04-09 18:45:15 -0500

    Academic periods select

---
+++ DGC_gestion_indicadores_frontend/src/lib/components/select/academicPeriodSelect.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { GetAcademicPeriods } from '$lib/api/controller/view/academicPeriod';
+	import type { AcademicPeriod } from '$lib/api/model/view/academicPeriod';
+	import type { Message } from './select';
+	import Select from './select.svelte';
+	import { onMount } from 'svelte';
+
+	let messages: Message[] = [];
+	export let selected: number;
+	let periods: AcademicPeriod[] = [];
+
+	onMount(async () => {
+		const res = await GetAcademicPeriods();
+		if (!res.ok) {
+			throw new Error('Fallo cargando periodos');
+		}
+		periods = await res.json();
+		messages = messages.concat(
+			periods.map((period) => ({
+				id: period.ID,
+				name: period.name
+			}))
+		);
+	});
+
+	$: selectedPeriod = periods.find((period) => period.ID === selected);
+</script>
+
+<Select {messages} bind:selected></Select>
+{#if selectedPeriod}
+<div class="flex flex-col text-sm text-accent-content p-2 m-2">
+
+	<p>Selected id: {selectedPeriod?.ID}</p>
+	<p>Selected name: {selectedPeriod?.name}</p>
+</div>
+{/if}
--- DGC_gestion_indicadores_frontend/src/routes/+page.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/+page.svelte
@@ -1,7 +1,12 @@
+<script>
+	import AcademicPeriodSelect from "$lib/components/select/academicPeriodSelect.svelte";
+</script>
+
 <svelte:head>
 	<title>Dashboard</title>
 </svelte:head>
 
 <div class="flex items-center justify-center mt-10">
 	<h1 class="text-xl">Dashboard</h1>
+	<AcademicPeriodSelect></AcademicPeriodSelect>
 </div>
