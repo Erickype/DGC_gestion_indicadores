11738b837c7a85d88c09a3778f28efc9346b3df6
Erick Carrasco <erickype@hotmail.com>
2024-04-10 17:58:58 -0500

    File and Menu icons

---
+++ DGC_gestion_indicadores_frontend/src/lib/icons/menu.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let fill = 'fill-info';
+	export let width = 'w-8';
+	export let height = 'h-8';
+	export let opacity = 'opacity-80';
+</script>
+
+<svg
+	class="{fill} {width} {height} {opacity}"
+	viewBox="0 0 24 24"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path
+		d="M4 6H20M4 12H20M4 18H20"
+		stroke="#000000"
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
