725d7464badd97ba3d67534cc7869cb8c527afdb
Erick Carrasco <erickype@hotmail.com>
2024-04-10 18:00:27 -0500

    Drawer added to layout

--- DGC_gestion_indicadores_frontend/src/routes/+layout.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/+layout.svelte
@@ -1,11 +1,42 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import MenuIcon from '$lib/icons/menu.svelte';
+	import MainMenu from '$lib/components/menu/mainMenu.svelte';
 	import { enhance } from '$app/forms';
 	import { page } from '$app/stores';
 	import '../app.css';
+
+	// Function to close the drawer
+	function closeDrawer() {
+		const drawerCheckbox = document.getElementById('my-drawer') as HTMLInputElement;
+		if (drawerCheckbox) {
+			drawerCheckbox.checked = false;
+		}
+	}
+
+	onMount(() => {
+		// Add click event listeners to all anchor elements in the sidebar
+		const sidebarLinks = document.querySelectorAll('.menu a');
+		sidebarLinks.forEach((link) => {
+			link.addEventListener('click', closeDrawer);
+		});
+
+		// Clean up event listeners on component unmount
+		return () => {
+			sidebarLinks.forEach((link) => {
+				link.removeEventListener('click', closeDrawer);
+			});
+		};
+	});
 </script>
 
 <div class="navbar bg-primary">
 	<div class="flex-1 pl-4">
+		{#if $page.data.user}
+			<label for="my-drawer" class="btn btn-ghost drawer-button mr-4">
+				<MenuIcon></MenuIcon>
+			</label>
+		{/if}
 		<h1 class="text-xl font-semibold">Gestión de Indicadores</h1>
 	</div>
 	<div class="flex-none">
@@ -23,6 +54,18 @@
 	</div>
 </div>
 
-<main>
-	<slot />
-</main>
+<div class="drawer">
+	<input id="my-drawer" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content">
+		<!-- Page content here -->
+		<main>
+			<slot />
+		</main>
+	</div>
+	<div class="drawer-side">
+		<label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+		<ul class="menu bg-base-200 w-1/4">
+			<MainMenu></MainMenu>
+		</ul>
+	</div>
+</div>
