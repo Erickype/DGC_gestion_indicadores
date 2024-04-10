a196f82ef4e2f67e2e088c48b9988366f0d182ec
Erick Carrasco <erickype@hotmail.com>
2024-04-09 21:58:44 -0500

    filtered dropdown based on input text

--- DGC_gestion_indicadores_frontend/src/lib/components/select/select.svelte
+++ DGC_gestion_indicadores_frontend/src/lib/components/select/select.svelte
@@ -4,9 +4,14 @@
 	import { createEventDispatcher } from 'svelte';
 
 	export let messages: Message[] = [];
+	let messagesFilter: Message[] = [];
 	export let selected: number = 0;
 
 	const dispatch = createEventDispatcher();
+
+	$: if (messages.length > 0) {
+		messagesFilter = messages;
+	}
 
 	function itemSelected(event: Event) {
 		const button = event.target as HTMLButtonElement;
@@ -23,17 +28,33 @@
 		const dropdown = input.closest('.dropdown');
 		dropdown!.setAttribute('open', 'open');
 	}
+
+	function filterMessages(event: Event) {
+		const input = event.target as HTMLInputElement;
+
+		const searchText = input.value.toLowerCase();
+		messagesFilter = messages.filter((message) => message.name.toLowerCase().includes(searchText));
+
+		const dropdown = input.closest('.dropdown');
+		dropdown!.setAttribute('open', 'open');
+	}
 </script>
 
 <details class="dropdown w-1/3">
 	<summary class="list-none">
 		<label class="input input-bordered flex items-center gap-2">
-			<input type="text" class="grow" placeholder="Search" on:focus={openSelect} />
+			<input
+				type="text"
+				class="grow"
+				placeholder="Buscar"
+				on:focus={openSelect}
+				on:input={filterMessages}
+			/>
 			<Search customClass="w-4 h-4"></Search>
 		</label>
 	</summary>
-	<ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-full">
-		{#each messages as message}
+	<ul id="items" class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-full">
+		{#each messagesFilter as message}
 			<li value={message.id}>
 				<button value={message.id} type="button" on:click={itemSelected}>{message.name}</button>
 			</li>
