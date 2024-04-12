995e5777ef2ebd6e39d1572bd81e4410ff126958
Erick Carrasco <erickype@hotmail.com>
2024-04-12 18:29:01 -0500

    Ui change to shadcn

--- DGC_gestion_indicadores_frontend/tailwind.config.js
+++ DGC_gestion_indicadores_frontend/tailwind.config.js
@@ -1,39 +1,64 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui"), require('@tailwindcss/typography'),],
-  daisyui: {
-    themes: [
-      {
-        myLigthTheme: {
-          "primary": "#F35353",
-          "secondary": "#B8B8B8",
-          "accent": "#B7A38F",
-          "neutral": "#87BED4",
-          "base-100": "#F0F0F0",
-          "info": "#70ADD7",
-          "success": "#41DC91",
-          "warning": "#FAE49E",
-          "error": "#E74B72",
-        },
-        dark: {
-          "primary": "#990B0B",
-          "secondary": "#525252",
-          "accent": "#705C48",
-          "neutral": "#316E87",
-          "base-100": "#0A0A0A",
-          "info": "#2D719F",
-          "success": "#167949",
-          "warning": "#F7D564",
-          "error": "#C51B46",
-        },
-      },
-    ],
-    darkTheme: "dark",
-    base: true
-  },
-}
+import { fontFamily } from "tailwindcss/defaultTheme";
 
+/** @type {import('tailwindcss').Config} */
+const config = {
+	darkMode: ["class"],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	safelist: ["dark"],
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px"
+			}
+		},
+		extend: {
+			colors: {
+				border: "hsl(var(--border) / <alpha-value>)",
+				input: "hsl(var(--input) / <alpha-value>)",
+				ring: "hsl(var(--ring) / <alpha-value>)",
+				background: "hsl(var(--background) / <alpha-value>)",
+				foreground: "hsl(var(--foreground) / <alpha-value>)",
+				primary: {
+					DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+					foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+					foreground: "hsl(var(--secondary-foreground) / <alpha-value>)"
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+					foreground: "hsl(var(--destructive-foreground) / <alpha-value>)"
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+					foreground: "hsl(var(--muted-foreground) / <alpha-value>)"
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+					foreground: "hsl(var(--accent-foreground) / <alpha-value>)"
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover) / <alpha-value>)",
+					foreground: "hsl(var(--popover-foreground) / <alpha-value>)"
+				},
+				card: {
+					DEFAULT: "hsl(var(--card) / <alpha-value>)",
+					foreground: "hsl(var(--card-foreground) / <alpha-value>)"
+				}
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)"
+			},
+			fontFamily: {
+				sans: [...fontFamily.sans]
+			}
+		}
+	},
+};
+
+export default config;
