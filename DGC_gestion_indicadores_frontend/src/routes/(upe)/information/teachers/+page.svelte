ee2f4998de4c2fef4be09c885d9e1329a40dbf7b
Erick Carrasco <erickype@hotmail.com>
2024-04-11 21:51:00 -0500

    Layout for one page data

--- DGC_gestion_indicadores_frontend/src/routes/(upe)/information/teachers/+page.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/(upe)/information/teachers/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import AcademicPeriodAutocomplete from '$lib/components/autocomplete/academicPeriodAutocomplete.svelte';
 
-	let selectedPeriod: number = 0
+	let selectedPeriod: number = 0;
 
-	$:if(selectedPeriod > 0){
+	$: if (selectedPeriod > 0) {
 		// todo: load data based on this selected period
 	}
 </script>
@@ -12,8 +12,27 @@
 	<title>Docentes</title>
 </svelte:head>
 
-<div class="flex items-center justify-center">
-	<h1 class="text-xl font-bold">Docentes</h1>
-	<AcademicPeriodAutocomplete id="period" name="period" bind:selected={selectedPeriod}></AcademicPeriodAutocomplete>
-	{selectedPeriod}
+<div id="headerSection" class="px-4 py-2 items-center">
+	<div class="flex-row h-full justify-around items-center card bg-base-300 rounded-box">
+		<h1 class="text-xl font-bold">Docentes</h1>
+		<AcademicPeriodAutocomplete id="period" name="period" bind:selected={selectedPeriod} />
+	</div>
 </div>
+<div id="formSection" class="overflow-auto flex-grow px-4 py-2 items-center">
+	<div class="flex h-full justify-center items-center bg-base-300 rounded-box"></div>
+</div>
+<div id="contentSection" class="flex-grow px-4 py-2 items-center">
+	<div class="overflow-x-auto h-full bg-base-300 px-6 py-2 rounded-box"></div>
+</div>
+
+<style>
+	#headerSection {
+		height: 10%;
+	}
+	#formSection {
+		height: 35%;
+	}
+	#contentSection {
+		height: 55%;
+	}
+</style>
--- DGC_gestion_indicadores_frontend/src/routes/+layout.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/+layout.svelte
@@ -6,8 +6,7 @@
 	import { page } from '$app/stores';
 	import '../app.css';
 
-	// Function to close the drawer
-	function closeDrawer(event: CustomEvent<MouseEvent>) {
+	function closeDrawer() {
 		const drawerCheckbox = document.getElementById('my-drawer') as HTMLInputElement;
 		if (drawerCheckbox) {
 			drawerCheckbox.checked = false;
@@ -15,50 +14,51 @@
 	}
 </script>
 
-<div class="navbar bg-primary">
-	<div class="flex-1 pl-4">
-		{#if $page.data.user}
-			<label for="my-drawer" class="btn btn-ghost drawer-button mr-4">
-				<MenuIcon></MenuIcon>
-			</label>
-		{/if}
-		<h1 class="text-xl font-semibold">Gestión de Indicadores</h1>
-	</div>
-	<div class="flex-none">
-		{#if !$page.data.user}
-			<div class="gap-2">
-				<a href="/login" class="btn btn-ghost text-lg">Ingresar</a>
-				<a href="/register" class="btn btn-ghost text-lg">Registrarse</a>
-			</div>
-		{/if}
-		{#if $page.data.user}
-			<form
-				action="/logout"
-				method="POST"
-				use:enhance={() => {
-					return async ({ result }) => {
-						invalidateAll();
-						await applyAction(result);
-					};
-				}}
-			>
-				<button class="btn btn-ghost" type="submit">Salir</button>
-			</form>
-		{/if}
-	</div>
-</div>
-
 <div class="drawer">
 	<input id="my-drawer" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-content">
-		<!-- Page content here -->
-		<main>
+	<div class="drawer-content h-screen flex flex-col overflow-hidden">
+		<div class="h-min">
+			<div class="navbar bg-primary">
+				<div class="flex-1 pl-4">
+					{#if $page.data.user}
+						<label for="my-drawer" class="btn btn-ghost drawer-button mr-4">
+							<MenuIcon></MenuIcon>
+						</label>
+					{/if}
+					<h1 class="text-xl font-semibold">Gestión de Indicadores</h1>
+				</div>
+				<div class="flex-none">
+					{#if !$page.data.user}
+						<div class="gap-2">
+							<a href="/login" class="btn btn-ghost text-lg">Ingresar</a>
+							<a href="/register" class="btn btn-ghost text-lg">Registrarse</a>
+						</div>
+					{/if}
+					{#if $page.data.user}
+						<form
+							action="/logout"
+							method="POST"
+							use:enhance={() => {
+								return async ({ result }) => {
+									invalidateAll();
+									await applyAction(result);
+								};
+							}}
+						>
+							<button class="btn btn-ghost" type="submit">Salir</button>
+						</form>
+					{/if}
+				</div>
+			</div>
+		</div>
+
+		<main style="height: 90%;">
 			<slot />
 		</main>
 	</div>
 	<div class="drawer-side">
 		<label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-		<ul class="menu bg-base-200 w-1/4">
+		<ul class="h-full menu bg-base-200 w-1/4">
 			<MainMenu on:clicked={closeDrawer}></MainMenu>
 		</ul>
 	</div>
