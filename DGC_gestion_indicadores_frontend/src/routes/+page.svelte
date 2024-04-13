83e431752f7dfc95af8ebfe22c084b7cc1136a9a
Erick Carrasco <erickype@hotmail.com>
2024-04-12 19:01:26 -0500

    Favicon changed to uta icon

--- DGC_gestion_indicadores_frontend/src/routes/+page.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/+page.svelte
@@ -1,13 +1,6 @@
-<script>
-	import AcademicPeriodSelect from "$lib/components/autocomplete/academicPeriodAutocomplete.svelte";
-</script>
-
 <svelte:head>
-	<title>Dashboard</title>
+	<title>Home</title>
 </svelte:head>
 
 <div class="flex items-center justify-center mt-10">
-	<h1 class="text-xl">Dashboard</h1>
-	<AcademicPeriodSelect id="s1" name="s1" selected={0}></AcademicPeriodSelect>
-	<AcademicPeriodSelect id="s2" name="s2" selected={0}></AcademicPeriodSelect>
 </div>
