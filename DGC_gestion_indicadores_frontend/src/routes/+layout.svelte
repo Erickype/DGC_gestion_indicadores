3fd9bd88ba75f94d88e8c33c9bb1b1e6a48bfa8f
Erick Carrasco <erickype@hotmail.com>
2024-04-13 20:38:31 -0500

    Layout with: header bar and sidebar main menu

--- DGC_gestion_indicadores_frontend/src/routes/+layout.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/+layout.svelte
@@ -1,7 +1,25 @@
 <script>
 	import { ModeWatcher } from 'mode-watcher';
+	import { page } from '$app/stores';
 	import '../app.pcss';
+	import MainMenu from '$lib/components/menu/mainMenu.svelte';
+	import Header from '$lib/components/menu/header.svelte';
 </script>
 
 <ModeWatcher />
-<slot />
+
+{#if $page.data.user}
+	<div class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+		<MainMenu></MainMenu>
+		<div class="flex flex-col">
+			<Header></Header>
+			<main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+				<slot />
+			</main>
+		</div>
+	</div>
+{/if}
+
+{#if !$page.data.user}
+	<slot />
+{/if}
