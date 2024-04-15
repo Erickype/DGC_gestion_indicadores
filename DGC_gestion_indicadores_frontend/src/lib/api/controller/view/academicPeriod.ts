7ab54750bd43dc9b32fc9b2b21cc0a6e712c754a
Erick Carrasco <erickype@hotmail.com>
2024-04-14 19:30:04 -0500

    Adademic periods load function to generate combo data

--- DGC_gestion_indicadores_frontend/src/lib/api/controller/view/academicPeriod.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/view/academicPeriod.ts
@@ -1,4 +1,6 @@
+import type { AcademicPeriod } from "$lib/api/model/view/academicPeriod";
 import { getAcademicPeriodsRoute } from "$lib/api/routes/view/academicPeriod";
+import type { Message } from "$lib/components/combobox/combobox";
 
 export async function GetAcademicPeriods() {
     return await fetch(getAcademicPeriodsRoute, {
@@ -7,4 +9,26 @@ export async function GetAcademicPeriods() {
             'Content-Type': 'application/json'
         }
     });
+}
+
+
+export async function LoadAcademicPeriodsWithComboMessages() {
+    const res = await GetAcademicPeriods();
+    if (!res.ok) {
+        throw new Error('Fallo cargando periodos');
+    }
+    const periods: AcademicPeriod[] = await res.json();
+    let messages: Message[] = []
+    messages = messages.concat(
+        periods.map((period) => ({
+            value: period.name,
+            label: period.name,
+        }))
+    );
+    console.log(messages);
+
+    return {
+        periods,
+        messages
+    }
 }
