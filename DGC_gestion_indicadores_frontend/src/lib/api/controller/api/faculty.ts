0b6b4ed174c3fdc5f10dc6c87ff8b17bff009e80
Erick Carrasco <erickype@hotmail.com>
2024-04-15 12:53:17 -0500

    Faculty controller with load function for combobox

---
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/api/faculty.ts
@@ -0,0 +1,34 @@
+import type { Faculty } from "$lib/api/model/api/faculty";
+import { getFacultiesRoute } from "$lib/api/routes/api/faculty";
+import type { Message } from "$lib/components/combobox/combobox";
+
+export async function GetFaculties(token: string) {
+    return await fetch(getFacultiesRoute, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token
+        }
+    });
+}
+
+
+export async function LoadFacultiesWithComboMessages(token: string) {
+    const res = await GetFaculties(token);
+    if (!res.ok) {
+        throw new Error('Fallo cargando facultades.');
+    }
+    const faculties: Faculty[] = await res.json();
+    let messages: Message[] = []
+    messages = messages.concat(
+        faculties.map((faculty) => ({
+            value: faculty.abbreviation,
+            label: faculty.abbreviation,
+        }))
+    );
+
+    return {
+        faculties,
+        messages
+    }
+}
