bfc42e701e55acaa245fb5feb4a0433b8bbf65a5
Erick Carrasco <erickype@hotmail.com>
2024-04-19 20:03:38 -0500

    Initial table actions

---
+++ DGC_gestion_indicadores_frontend/src/routes/(upe)/information/teachers/teachers-data-table-actions.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import CircleX from 'lucide-svelte/icons/circle-x';
+	import Pencil from 'lucide-svelte/icons/pencil';
+
+	import { Button } from '$lib/components/ui/button';
+
+	export let id: string;
+</script>
+
+<div class="flex flex-auto">
+    <Button variant="ghost" size="icon" on:click={()=>{
+		console.log("Update: "+id);
+		
+	}}>
+        <Pencil class="h-4 w-4" />
+    </Button>
+    <Button variant="ghost" size="icon" on:click={()=>{
+        console.log("Delete: "+id);        
+    }}>
+        <CircleX class="stroke-primary h-4 w-4" />
+    </Button>
+</div>
