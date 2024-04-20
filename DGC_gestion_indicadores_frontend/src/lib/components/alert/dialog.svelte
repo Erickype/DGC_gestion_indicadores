12610a57905aa0dc5bbb0cb1d85f22437059d926
Erick Carrasco <erickype@hotmail.com>
2024-04-19 20:26:42 -0500

    Alert dialog added to addTeachers table action delete

---
+++ DGC_gestion_indicadores_frontend/src/lib/components/alert/dialog.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
+
+	export let dialogOpen = false;
+
+	export let title = 'Esta totalmente seguro?';
+	export let description =
+		'Esta es una acción irreversible. Esto borrará permanentemente el registro sin opción a recuperarlo.';
+</script>
+
+<AlertDialog.Root bind:open={dialogOpen}>
+	<AlertDialog.Content>
+		<AlertDialog.Header>
+			<AlertDialog.Title>{title}</AlertDialog.Title>
+			<AlertDialog.Description>
+				{description}
+			</AlertDialog.Description>
+		</AlertDialog.Header>
+		<AlertDialog.Footer>
+			<AlertDialog.Cancel>Cancelar</AlertDialog.Cancel>
+			<AlertDialog.Action>Continuar</AlertDialog.Action>
+		</AlertDialog.Footer>
+	</AlertDialog.Content>
+</AlertDialog.Root>
--- DGC_gestion_indicadores_frontend/src/routes/(upe)/information/teachers/teachers-data-table-actions.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/(upe)/information/teachers/teachers-data-table-actions.svelte
@@ -3,9 +3,14 @@
 	import Pencil from 'lucide-svelte/icons/pencil';
 
 	import { Button } from '$lib/components/ui/button';
+	import Dialog from '$lib/components/alert/dialog.svelte';
 
 	export let id: string;
+
+    let dialogOpen = false
 </script>
+
+<Dialog bind:dialogOpen={dialogOpen}></Dialog>
 
 <div class="flex flex-auto">
     <Button variant="ghost" size="icon" on:click={()=>{
@@ -15,7 +20,8 @@
         <Pencil class="h-4 w-4" />
     </Button>
     <Button variant="ghost" size="icon" on:click={()=>{
-        console.log("Delete: "+id);        
+        console.log("Delete: "+id);      
+        dialogOpen = !dialogOpen  
     }}>
         <CircleX class="stroke-primary h-4 w-4" />
     </Button>
