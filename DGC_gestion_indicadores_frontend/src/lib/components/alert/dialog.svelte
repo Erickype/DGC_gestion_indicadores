9505d61020dbe65dee33c2fc774b9d21466786bb
Erick Carrasco <erickype@hotmail.com>
2024-04-19 22:21:58 -0500

    Dialog continue action dispatch event

--- DGC_gestion_indicadores_frontend/src/lib/components/alert/dialog.svelte
+++ DGC_gestion_indicadores_frontend/src/lib/components/alert/dialog.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
 	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
+	import { createEventDispatcher } from 'svelte';
 
 	export let dialogOpen = false;
 
 	export let title = 'Esta totalmente seguro?';
 	export let description =
 		'Esta es una acción irreversible. Esto borrará permanentemente el registro sin opción a recuperarlo.';
+
+	const dispatch = createEventDispatcher();
+
+	function dispatchContinuePressed() {
+		dispatch('dialog-continue', {
+			status: true
+		});
+	}
 </script>
 
 <AlertDialog.Root bind:open={dialogOpen}>
@@ -18,7 +27,7 @@
 		</AlertDialog.Header>
 		<AlertDialog.Footer>
 			<AlertDialog.Cancel>Cancelar</AlertDialog.Cancel>
-			<AlertDialog.Action>Continuar</AlertDialog.Action>
+			<AlertDialog.Action on:click={dispatchContinuePressed}>Continuar</AlertDialog.Action>
 		</AlertDialog.Footer>
 	</AlertDialog.Content>
 </AlertDialog.Root>
