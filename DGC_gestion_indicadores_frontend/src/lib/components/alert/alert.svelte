2a7e5d8dbc176aa240ad0179e3a55e0f27977555
Erick Carrasco <erickype@hotmail.com>
2024-05-18 18:48:58 +0000

    Add general alert to show status

---
+++ DGC_gestion_indicadores_frontend/src/lib/components/alert/alert.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import CircleAlert from 'lucide-svelte/icons/circle-alert';
+	import * as Alert from '$lib/components/ui/alert/index.js';
+
+	export let title = 'Error';
+	export let description = 'Algo salió mal, intentar nuevamente.';
+	export let variant: 'default' | 'destructive' | undefined = 'default';
+</script>
+
+<Alert.Root {variant}>
+	<CircleAlert class="h-4 w-4" />
+	<Alert.Title>{title}</Alert.Title>
+	<Alert.Description>{description}</Alert.Description>
+</Alert.Root>
