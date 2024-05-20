06752acbe6d4a31e15e27195d27e0ade67543f9a
Erick Carrasco <erickype@hotmail.com>
2024-05-20 19:47:23 +0000

    Create table component

---
+++ DGC_gestion_indicadores_frontend/src/lib/components/table/table.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+	import type { Table as HeadlessTable, Column } from 'svelte-headless-table';
+	import { Render, Subscribe } from 'svelte-headless-table';
+
+	import ArrowUpDown from 'lucide-svelte/icons/arrow-up-down';
+
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Button } from '$lib/components/ui/button';
+	import * as Table from '$lib/components/ui/table';
+
+	export let table: HeadlessTable<any, any>;
+	export let columns: Column<any, any>[];
+	export let filterFields: string[];
+
+	const { headerRows, pageRows, tableAttrs, tableBodyAttrs, pluginStates } =
+		table.createViewModel(columns);
+
+	const { hasNextPage, hasPreviousPage, pageIndex, pageSize } = pluginStates.page;
+
+	const { filterValue } = pluginStates.filter;
+</script>
+
+<div class="flex items-center gap-2 py-4">
+	<Input class="w-3/4" placeholder="Filtrar..." type="text" bind:value={$filterValue} />
+	<Input
+		type="number"
+		placeholder="Tamaño página"
+		min="1"
+		max="10"
+		class="w-1/4"
+		bind:value={$pageSize}
+	/>
+</div>
+<div class="rounded-md border">
+	<Table.Root {...$tableAttrs}>
+		<Table.Header>
+			{#each $headerRows as headerRow}
+				<Subscribe rowAttrs={headerRow.attrs()}>
+					<Table.Row>
+						{#each headerRow.cells as cell (cell.id)}
+							<Subscribe attrs={cell.attrs()} let:attrs props={cell.props()} let:props>
+								<Table.Head {...attrs}>
+									{#if filterFields.find((field) => cell.id === field)}
+										<Button class="p-0" variant="ghost" on:click={props.sort.toggle}>
+											<Render of={cell.render()} />
+											<ArrowUpDown class={'ml-2 h-4 w-4'} />
+										</Button>
+									{:else}
+										<Render of={cell.render()} />
+									{/if}
+								</Table.Head>
+							</Subscribe>
+						{/each}
+					</Table.Row>
+				</Subscribe>
+			{/each}
+		</Table.Header>
+		<Table.Body {...$tableBodyAttrs}>
+			{#each $pageRows as row (row.id)}
+				<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
+					<Table.Row {...rowAttrs}>
+						{#each row.cells as cell (cell.id)}
+							<Subscribe attrs={cell.attrs()} let:attrs>
+								<Table.Cell {...attrs}>
+									<Render of={cell.render()} />
+								</Table.Cell>
+							</Subscribe>
+						{/each}
+					</Table.Row>
+				</Subscribe>
+			{/each}
+		</Table.Body>
+	</Table.Root>
+</div>
+<div class="flex items-center justify-end space-x-4 py-4">
+	<Button
+		variant="outline"
+		size="sm"
+		on:click={() => ($pageIndex = $pageIndex - 1)}
+		disabled={!$hasPreviousPage}>Anterior</Button
+	>
+	<Button
+		variant="outline"
+		size="sm"
+		disabled={!$hasNextPage}
+		on:click={() => ($pageIndex = $pageIndex + 1)}>Siguiente</Button
+	>
+</div>
--- DGC_gestion_indicadores_frontend/src/routes/(admin)/admin/evaluation-periods/Table.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/(admin)/admin/evaluation-periods/Table.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
-	import ArrowUpDown from 'lucide-svelte/icons/arrow-up-down';
-
-	import DataTableActions from './TableActions.svelte';
-
 	import { addPagination, addSortBy, addTableFilter } from 'svelte-headless-table/plugins';
-	import { createTable, Render, Subscribe, createRender } from 'svelte-headless-table';
+	import { createTable, createRender } from 'svelte-headless-table';
 	import { readable } from 'svelte/store';
 
-	import { Button } from '$lib/components/ui/button';
-	import * as Table from '$lib/components/ui/table';
-	import { Input } from '$lib/components/ui/input/index.js';
+	import DataTableActions from '$lib/components/table/tableActions.svelte';
+
+	import Table from '$lib/components/table/table.svelte';
+
 	import type { EvaluationPeriod } from '$lib/api/model/view/evaluationPeriod';
 
 	export let periods: EvaluationPeriod[];
+
+	const filterFields = ['name', 'abbreviation'];
 
 	const table = createTable(readable(periods), {
 		page: addPagination({
@@ -70,84 +69,25 @@
 			accessor: ({ ID }) => ID,
 			header: '',
 			cell: ({ value }) => {
-				return createRender(DataTableActions, { id: value.toString() });
+				const actions = createRender(DataTableActions, { id: value.toString() });
+				actions.on('delete-confirmation', handleDeleteConfirmation);
+				actions.on('update-action', handleUpdateAction);
+				return actions;
 			}
 		})
 	]);
 
-	const { headerRows, pageRows, tableAttrs, tableBodyAttrs, pluginStates } =
-		table.createViewModel(columns);
+	function handleDeleteConfirmation(event: any) {
+		const detail: { status: boolean; id: number } = event.detail;
+		console.log(detail);
+	}
 
-	const { hasNextPage, hasPreviousPage, pageIndex, pageSize } = pluginStates.page;
-
-	const { filterValue } = pluginStates.filter;
+	function handleUpdateAction(event: any) {
+		const detail: { status: boolean; id: number } = event.detail;
+		console.log(detail);
+	}
 </script>
 
 <div class="w-full">
-	<div class="flex items-center gap-2 py-4">
-		<Input class="w-3/4" placeholder="Filtrar..." type="text" bind:value={$filterValue} />
-		<Input
-			type="number"
-			placeholder="Tamaño página"
-			min="1"
-			max="10"
-			class="w-1/4"
-			bind:value={$pageSize}
-		/>
-	</div>
-	<div class="rounded-md border">
-		<Table.Root {...$tableAttrs}>
-			<Table.Header>
-				{#each $headerRows as headerRow}
-					<Subscribe rowAttrs={headerRow.attrs()}>
-						<Table.Row>
-							{#each headerRow.cells as cell (cell.id)}
-								<Subscribe attrs={cell.attrs()} let:attrs props={cell.props()} let:props>
-									<Table.Head {...attrs}>
-										{#if cell.id === 'person'}
-											<Button variant="ghost" on:click={props.sort.toggle}>
-												<Render of={cell.render()} />
-												<ArrowUpDown class={'ml-2 h-4 w-4'} />
-											</Button>
-										{:else}
-											<Render of={cell.render()} />
-										{/if}
-									</Table.Head>
-								</Subscribe>
-							{/each}
-						</Table.Row>
-					</Subscribe>
-				{/each}
-			</Table.Header>
-			<Table.Body {...$tableBodyAttrs}>
-				{#each $pageRows as row (row.id)}
-					<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-						<Table.Row {...rowAttrs}>
-							{#each row.cells as cell (cell.id)}
-								<Subscribe attrs={cell.attrs()} let:attrs>
-									<Table.Cell {...attrs}>
-										<Render of={cell.render()} />
-									</Table.Cell>
-								</Subscribe>
-							{/each}
-						</Table.Row>
-					</Subscribe>
-				{/each}
-			</Table.Body>
-		</Table.Root>
-	</div>
-	<div class="flex items-center justify-end space-x-4 py-4">
-		<Button
-			variant="outline"
-			size="sm"
-			on:click={() => ($pageIndex = $pageIndex - 1)}
-			disabled={!$hasPreviousPage}>Anterior</Button
-		>
-		<Button
-			variant="outline"
-			size="sm"
-			disabled={!$hasNextPage}
-			on:click={() => ($pageIndex = $pageIndex + 1)}>Siguiente</Button
-		>
-	</div>
+	<Table {table} {columns} {filterFields} />
 </div>
