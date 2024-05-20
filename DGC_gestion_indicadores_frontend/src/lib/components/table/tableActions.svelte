206f11ca35e3eccbe8a3bc71b411b692387acaa6
Erick Carrasco <erickype@hotmail.com>
2024-05-20 19:46:47 +0000

    Create TableActions component

---
+++ DGC_gestion_indicadores_frontend/src/lib/components/table/tableActions.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import CircleX from 'lucide-svelte/icons/circle-x';
+	import Pencil from 'lucide-svelte/icons/pencil';
+
+	import Dialog from '$lib/components/alert/dialog.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import { createEventDispatcher } from 'svelte';
+
+	export let id: string;
+
+	let dialogOpen = false;
+
+	const dispatch = createEventDispatcher();
+
+	function sendDeleteConfirmation() {
+		dispatch('delete-confirmation', {
+			status: true,
+			id: parseInt(id)
+		});
+	}
+
+	function sendUpdateAction() {
+		dispatch('update-action', {
+			status: true,
+			id: parseInt(id)
+		});
+	}
+</script>
+
+<Dialog bind:dialogOpen on:dialog-continue={sendDeleteConfirmation}></Dialog>
+
+<div class="flex flex-auto">
+	<Button variant="ghost" size="icon" on:click={sendUpdateAction}>
+		<Pencil class="h-4 w-4" />
+	</Button>
+	<Button
+		variant="ghost"
+		size="icon"
+		on:click={() => {
+			dialogOpen = !dialogOpen;
+		}}
+	>
+		<CircleX class="stroke-primary h-4 w-4" />
+	</Button>
+</div>
