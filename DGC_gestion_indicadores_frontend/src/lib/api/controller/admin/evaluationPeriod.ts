cb42e412b72b950d1133b10d6c22292eff8ad985
Erick Carrasco <erickype@hotmail.com>
2024-05-22 19:26:54 +0000

    Add delete api client for EvaluationPeriod

--- DGC_gestion_indicadores_frontend/src/lib/api/controller/admin/evaluationPeriod.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/admin/evaluationPeriod.ts
@@ -1,5 +1,5 @@
 import type { PostEvaluationPeriodRequest } from "$lib/api/model/view/evaluationPeriod";
-import { postEvaluationPeriod } from "$lib/api/routes/admin/evaluationPeriod";
+import { deleteEvaluationPeriod, postEvaluationPeriod } from "$lib/api/routes/admin/evaluationPeriod";
 
 export async function PostEvaluationPeriod(period: PostEvaluationPeriodRequest, token: string) {
     return await fetch(postEvaluationPeriod, {
@@ -10,4 +10,14 @@ export async function PostEvaluationPeriod(period: PostEvaluationPeriodRequest, 
         },
         body: JSON.stringify(period)
     });
+}
+
+export async function DeleteEvaluationPeriod(id: string, token: string) {
+    return await fetch(deleteEvaluationPeriod + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token
+        }
+    })
 }
--- DGC_gestion_indicadores_frontend/src/lib/api/routes/admin/evaluationPeriod.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/routes/admin/evaluationPeriod.ts
@@ -2,3 +2,4 @@ import { adminBaseURL } from "./base";
 
 // Academic period
 export const postEvaluationPeriod = adminBaseURL + "/evaluationPeriod"
+export const deleteEvaluationPeriod = adminBaseURL + "/evaluationPeriod/"
