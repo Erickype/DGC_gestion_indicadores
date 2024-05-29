0fc2fa0cfe7116378aca145ce00dd2bb5330142d
Erick Carrasco <erickype@hotmail.com>
2024-05-29 17:24:42 +0000

    Add page to fetch academic periods with common error handling

--- DGC_gestion_indicadores_frontend/src/routes/(admin)/admin/academic-periods/+page.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/(admin)/admin/academic-periods/+page.svelte
@@ -1,3 +1,24 @@
+<script lang="ts">
+	import type { CommonError } from '$lib/api/model/errors';
+	import type { AcademicPeriod } from '$lib/api/model/view/academicPeriod';
+
+	import Alert from '$lib/components/alert/alert.svelte';
+
+	let academicPeriodsPromise: Promise<AcademicPeriod[]> = fetchAcademicPeriods();
+
+	async function fetchAcademicPeriods() {
+		const url = `/api/academicPeriod`;
+		const response = await fetch(url, {
+			method: 'GET'
+		});
+		if (!response.ok) {
+			const errorData = await response.json() as CommonError
+			throw errorData
+		}
+		return (academicPeriodsPromise = response.json());
+	}
+</script>
+
 <svelte:head>
 	<title>Periodos Académicos</title>
 </svelte:head>
@@ -5,3 +26,17 @@
 <div class="mx-auto flex w-full place-content-center justify-between px-8">
 	<h2 class="text-2xl font-bold">Periodos Académicos</h2>
 </div>
+
+{#await academicPeriodsPromise}
+	cargando...
+{:then periods}
+	{#if periods.length > 0}
+		{#each periods as period}
+			{period.name}
+		{/each}
+	{:else}
+		<Alert title="Sin registros" description={'Ups, no hay periodos académicos'} />
+	{/if}
+{:catch error}
+	<Alert variant="destructive" description={`${error.Detail}: ${error.Message}`} />
+{/await}
