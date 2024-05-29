653bef1f4dfcf2593c72ee55c3da9c678db6ef16
Erick Carrasco <erickype@hotmail.com>
2024-05-29 17:23:19 +0000

    Update feth academic periods for common error

--- DGC_gestion_indicadores_frontend/src/lib/api/controller/view/academicPeriod.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/view/academicPeriod.ts
@@ -1,23 +1,40 @@
+import { generateErrorFromCommonError, type CommonError } from "$lib/api/model/errors";
 import type { AcademicPeriod } from "$lib/api/model/view/academicPeriod";
 import { getAcademicPeriodsRoute } from "$lib/api/routes/view/academicPeriod";
 import type { Message } from "$lib/components/combobox/combobox";
 
-export async function GetAcademicPeriods() {
-    return await fetch(getAcademicPeriodsRoute, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
+export async function GetAcademicPeriods(): Promise<AcademicPeriod[] | CommonError> {
+    try {
+        const response = await fetch(getAcademicPeriodsRoute, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (!response.ok) {
+            const error: CommonError = await response.json()
+            return error
         }
-    });
+        const periods: AcademicPeriod[] = await response.json()
+        return periods
+    } catch (error) {
+        const errorMessage: CommonError = {
+            Status: "500",
+            StatusCode: 500,
+            Detail: "Error al solicitar datos",
+            Message: (error as Error).message
+        }
+        return errorMessage
+    }
 }
 
 
 export async function LoadAcademicPeriodsWithComboMessages() {
-    const res = await GetAcademicPeriods();
-    if (!res.ok) {
-        throw new Error('Fallo cargando periodos');
+    const response = await GetAcademicPeriods();
+    if ((response as CommonError).Status) {
+        throw generateErrorFromCommonError(response as CommonError)
     }
-    const periods: AcademicPeriod[] = await res.json();
+    const periods = response as AcademicPeriod[]
     let messages: Message[] = []
     messages = messages.concat(
         periods.map((period) => ({
