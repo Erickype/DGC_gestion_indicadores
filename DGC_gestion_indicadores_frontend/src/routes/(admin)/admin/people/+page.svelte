e386be8298b8d50877883f37358c68967611e13a
Erick Carrasco <erickype@hotmail.com>
2024-06-16 15:46:04 -0500

    Add People page

---
+++ DGC_gestion_indicadores_frontend/src/routes/(admin)/admin/people/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import AddModal from './AddModal.svelte';
+
+	export let data: PageData;
+	const addPersonFormData = data.addPersonForm;
+
+	async function fetchPeople() {
+		console.log('People data fetch');
+	}
+
+	function handleCreated(event: any) {
+		const data: { status: boolean } = event.detail;
+		if (data.status) {
+			fetchPeople();
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>Personas</title>
+</svelte:head>
+
+<div class="mx-auto flex w-full place-content-center justify-between px-8">
+	<h2 class="text-2xl font-bold">Personas</h2>
+	<AddModal formData={addPersonFormData} on:created={handleCreated} />
+</div>
