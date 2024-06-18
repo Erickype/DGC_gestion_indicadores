7b943a3eee299767d393ea5538273c2e7858ea04
Erick Carrasco <erickype@hotmail.com>
2024-06-18 14:00:58 -0500

    Add AddPerson server form action

--- DGC_gestion_indicadores_frontend/src/routes/(admin)/admin/people/+page.server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(admin)/admin/people/+page.server.ts
@@ -1,8 +1,10 @@
-import { superValidate } from "sveltekit-superforms";
+import { message, superValidate, type ErrorStatus } from "sveltekit-superforms";
 import type { PageServerLoad } from "./$types";
 import { redirect, type Actions } from "@sveltejs/kit";
 import { zod } from "sveltekit-superforms/adapters";
 import { addPersonSchema, updatePersonSchema } from "./schema";
+import type { PostPersonRequest } from "$lib/api/model/api/person";
+import { PostPerson } from "$lib/api/controller/admin/person";
 
 
 export const load: PageServerLoad = async ({ locals }) => {
@@ -14,4 +16,38 @@ export const load: PageServerLoad = async ({ locals }) => {
         addPersonForm: await superValidate(zod(addPersonSchema)),
         updatePersonForm: await superValidate(zod(updatePersonSchema)) 
     }
+};
+
+export const actions: Actions = {
+    addPerson: async (event) => {
+        const form = await superValidate(event, zod(addPersonSchema))
+
+        if (!form.valid) {
+            return message(form,
+                { success: false, error: "Invalid form" },
+                { status: 400 })
+        }
+
+        const token = event.cookies.get("AuthorizationToken")
+        const data = form.data
+        const person: PostPersonRequest = {
+            identity: data.identity,
+            name: data.name,
+            lastname: data.lastname,
+            email: data.email
+        }
+
+        const res = await PostPerson(person, token!)
+
+        if (!res.ok) {
+            const status = res.status as unknown as ErrorStatus
+            const data = await res.json()
+            return message(form,
+                { success: false, error: data },
+                { status: status })
+        }
+        return message(form, {
+            success: true
+        })
+    },
 };
