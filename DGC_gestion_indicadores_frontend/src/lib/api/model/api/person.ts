23d8f0ff72d3e9275a522f6a3bba852efa28d9bb
Erick Carrasco <erickype@hotmail.com>
2024-07-20 13:35:13 -0500

    Adds models put and delete enpoitns for person

--- DGC_gestion_indicadores_frontend/src/lib/api/model/api/person.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/model/api/person.ts
@@ -9,7 +9,15 @@ export interface Person {
     email: string;
 }
 
-export interface PostPersonRequest{
+export interface PostPersonRequest {
+    identity: string;
+    name: string;
+    lastname: string;
+    email: string;
+}
+
+export interface PutPersonRequest {
+    ID: number;
     identity: string;
     name: string;
     lastname: string;
--- DGC_gestion_indicadores_frontend/src/lib/api/routes/admin/person.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/routes/admin/person.ts
@@ -1,3 +1,5 @@
 import { adminBaseURL } from "./base";
 
 export const postPersonRoute = adminBaseURL + "/person"
+export const putPersonRoute = adminBaseURL + "/person/"
+export const deletePersonRoute = adminBaseURL + "/person/"
