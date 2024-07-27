4cbb99e8b3c3dae18295da474189f0d37f066b95
Erick Carrasco <erickype@hotmail.com>
2024-07-27 12:24:48 -0500

    Add server data schema AddFacultyForm

--- DGC_gestion_indicadores_frontend/src/routes/(institution)/institution/faculties/+page.server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(institution)/institution/faculties/+page.server.ts
@@ -2,7 +2,7 @@ import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
-import { updateFacultySchema } from "./schema";
+import { addFacultySchema, updateFacultySchema } from "./schema";
 
 export const load: PageServerLoad = async ({locals}) => {
     if(!locals.user){
@@ -10,6 +10,7 @@ export const load: PageServerLoad = async ({locals}) => {
     }
 
     return{
+        addFacultyForm: await superValidate(zod(addFacultySchema)),
         updateFacultyForm: await superValidate(zod(updateFacultySchema))
     }
 };
