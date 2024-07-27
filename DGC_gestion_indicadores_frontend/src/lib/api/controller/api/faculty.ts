39b32bd85d19fb33b8dc91765282559cc5334ca3
Erick Carrasco <erickype@hotmail.com>
2024-07-27 11:22:01 -0500

    Add common error to GetFaculties fetch

--- DGC_gestion_indicadores_frontend/src/lib/api/controller/api/faculty.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/api/faculty.ts
@@ -1,24 +1,42 @@
-import type { Faculty } from "$lib/api/model/api/faculty";
 import { getFacultiesRoute } from "$lib/api/routes/api/faculty";
 import type { Message } from "$lib/components/combobox/combobox";
+import type { Faculty } from "$lib/api/model/api/faculty";
+import { generateErrorFromCommonError, type CommonError } from "$lib/api/model/errors";
 
 export async function GetFaculties(token: string) {
-    return await fetch(getFacultiesRoute, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': token
+    try {
+        const response = await fetch(getFacultiesRoute, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            }
+        });
+
+        if (!response.ok) {
+            const error: CommonError = await response.json()
+            return error
         }
-    });
+        const faculties: Faculty[] = await response.json()
+        return faculties
+    } catch (error) {
+        const errorMessage: CommonError = {
+            status: "500",
+            status_code: 500,
+            detail: "Error al solicitar datos",
+            message: (error as Error).message
+        }
+        return errorMessage
+    }
 }
 
 
 export async function LoadFacultiesWithComboMessages(token: string) {
-    const res = await GetFaculties(token);
-    if (!res.ok) {
-        throw new Error('Fallo cargando facultades.');
+    const response = await GetFaculties(token);
+    if ((response as CommonError).status) {
+        throw generateErrorFromCommonError(response as CommonError)
     }
-    const faculties: Faculty[] = await res.json();
+    const faculties = response as Faculty[];
     let messages: Message[] = []
     messages = messages.concat(
         faculties.map((faculty) => ({
