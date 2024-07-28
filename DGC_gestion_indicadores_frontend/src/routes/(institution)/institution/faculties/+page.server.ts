b7e98cd5e2e0d8af482add6a4a145ff796de636c
Erick Carrasco <erickype@hotmail.com>
2024-07-28 18:00:16 -0500

    Add server action to create a faculty

--- DGC_gestion_indicadores_frontend/src/routes/(institution)/institution/faculties/+page.server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(institution)/institution/faculties/+page.server.ts
@@ -1,16 +1,44 @@
-import { redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
-import { superValidate } from "sveltekit-superforms";
+import { message, superValidate, type ErrorStatus } from "sveltekit-superforms";
+import { redirect, type Actions } from "@sveltejs/kit";
 import { zod } from "sveltekit-superforms/adapters";
+
 import { addFacultySchema, updateFacultySchema } from "./schema";
 
-export const load: PageServerLoad = async ({locals}) => {
-    if(!locals.user){
+import type { PageServerLoad } from "./$types";
+
+import type { PostFacultyRequest } from "$lib/api/model/admin/faculty";
+import { generateFormMessageFromHttpResponse, generateFormMessageFromInvalidForm } from "$lib/utils";
+import { PostFaculty } from "$lib/api/controller/admin/faculty";
+
+export const load: PageServerLoad = async ({ locals }) => {
+    if (!locals.user) {
         throw redirect(302, "/")
     }
 
-    return{
+    return {
         addFacultyForm: await superValidate(zod(addFacultySchema)),
         updateFacultyForm: await superValidate(zod(updateFacultySchema))
     }
+};
+
+export const actions: Actions = {
+    addFaculty: async (event) => {
+        const form = await superValidate(event, zod(addFacultySchema))
+
+        if (!form.valid) {
+            return generateFormMessageFromInvalidForm(form)
+        }
+
+        const token = event.cookies.get("AuthorizationToken")
+        const data = form.data
+        const person: PostFacultyRequest = {
+            name: data.name,
+            description: data.description,
+            abbreviation: data.abbreviation
+        }
+
+        const response = await PostFaculty(person, token!)
+
+        return generateFormMessageFromHttpResponse(form, response)
+    },
 };
