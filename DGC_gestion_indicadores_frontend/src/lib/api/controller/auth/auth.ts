d03e605ca7a3dfbf4feaac8b76fa62748214ba3d
Erick Carrasco <erickype@hotmail.com>
2024-07-29 16:26:54 -0500

    Add use of common error in the login server action

--- DGC_gestion_indicadores_frontend/src/lib/api/controller/auth/auth.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/auth/auth.ts
@@ -1,15 +1,27 @@
 import type { RegisterRequest } from "$lib/api/model/auth/register";
+import type { CommonError } from "$lib/api/model/errors";
 import { authRouteLogin, authRouteRegister } from "$lib/api/routes/auth/auth";
-import type { LoginRequest } from "../../model/auth/login";
+import { generateCommonErrorFromFetchError } from "$lib/utils";
+import type { LoginRequest, LoginResponse } from "../../model/auth/login";
 
-export async function Login(login: LoginRequest) {
-    return await fetch(authRouteLogin, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(login)
-    });
+export async function Login(login: LoginRequest): Promise<LoginResponse | CommonError> {
+    try {
+        const response = await fetch(authRouteLogin, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(login)
+        });
+        if (!response.ok) {
+            const error: CommonError = await response.json()
+            return error
+        }
+        const LoginResponse: LoginResponse = await response.json()
+        return LoginResponse;
+    } catch (error) {
+        return generateCommonErrorFromFetchError(error)
+    }
 }
 
 export async function Register(registerRequest: RegisterRequest) {
