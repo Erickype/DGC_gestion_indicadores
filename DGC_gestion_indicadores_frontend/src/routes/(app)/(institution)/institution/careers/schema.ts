1db27ac28669c7090ca5b0967102d753ade2a263
Erick Carrasco <erickype@hotmail.com>
2024-08-03 17:59:01 -0500

    Add schemas for career: add and update

---
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(institution)/institution/careers/schema.ts
@@ -0,0 +1,57 @@
+import { z } from "zod";
+
+export const addCarrerSchema = z.object({
+  facultyID: z.number({
+    required_error: "Facultad requerida"
+  }).gt(0, {
+    message: "Ingrese un valor válido"
+  }),
+  name: z.string({
+    required_error: "Nombre requerido",
+  }).min(5, {
+    message: "Nombre mínimo 5 caracteres."
+  }).max(200),
+
+  description: z.string({
+    required_error: "Descripción requerida"
+  }).min(10, {
+    message: "Descripción mínimo 10 caracteres."
+  }).max(50),
+
+  abbreviation: z.string({
+    required_error: "Abreviación requerida"
+  }).min(5, {
+    message: "Abreviación mínimo 5 caracteres."
+  }).max(50),
+});
+
+export const updateCarrerSchema = z.object({
+  ID: z.number().gt(0),
+
+  facultyID: z.number({
+    required_error: "Facultad requerida"
+  }).gt(0, {
+    message: "Ingrese un valor válido"
+  }),
+
+  name: z.string({
+    required_error: "Nombre requerido",
+  }).min(5, {
+    message: "Nombre mínimo 5 caracteres."
+  }).max(200),
+
+  description: z.string({
+    required_error: "Descripción requerida"
+  }).min(10, {
+    message: "Descripción mínimo 10 caracteres."
+  }).max(50),
+
+  abbreviation: z.string({
+    required_error: "Abreviación requerida"
+  }).min(5, {
+    message: "Abreviación mínimo 5 caracteres."
+  }).max(50),
+});
+
+export type AddCarrerSchema = typeof addCarrerSchema;
+export type UpdateCarrerSchema = typeof updateCarrerSchema;
