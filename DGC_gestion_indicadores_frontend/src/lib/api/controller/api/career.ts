d359f8536ab6187727c9bb85851ad28dd6f3a74a
Erick Carrasco <erickype@hotmail.com>
2024-08-03 10:05:38 -0500

    Add get careers api client with error management

--- DGC_gestion_indicadores_frontend/src/lib/api/controller/api/career.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/api/career.ts
@@ -1,24 +1,36 @@
 import type { Career } from "$lib/api/model/api/career";
+import { generateErrorFromCommonError, type CommonError } from "$lib/api/model/errors";
 import { getCareersRoute } from "$lib/api/routes/api/career";
 import type { Message } from "$lib/components/combobox/combobox";
+import { generateCommonErrorFromFetchError } from "$lib/utils";
 
 export async function GetCareers(token: string) {
-    return await fetch(getCareersRoute, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': token
+    try {
+        const response = await fetch(getCareersRoute, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            }
+        });
+        if (!response.ok) {
+            const error: CommonError = await response.json()
+            return error
         }
-    });
+        const careers: Career[] = await response.json()
+        return careers
+    } catch (error) {
+        return generateCommonErrorFromFetchError(error)
+    }
 }
 
 
 export async function LoadCareersWithComboMessages(token: string) {
-    const res = await GetCareers(token);
-    if (!res.ok) {
-        throw new Error('Fallo cargando carreras.');
+    const response = await GetCareers(token);
+    if ((response as CommonError).status) {
+        throw generateErrorFromCommonError(response as CommonError)
     }
-    const careers: Career[] = await res.json();
+    const careers: Career[] = response as Career[];
     let messages: Message[] = []
     messages = messages.concat(
         careers.map((career) => ({
