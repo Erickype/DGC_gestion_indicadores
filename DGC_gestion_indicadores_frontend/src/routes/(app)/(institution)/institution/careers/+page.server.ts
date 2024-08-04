5da6dfc8a5aefa78c7bd17f0af1a148333399ce2
Erick Carrasco <erickype@hotmail.com>
2024-08-04 10:09:11 -0500

    Add server action add career

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(institution)/institution/careers/+page.server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(institution)/institution/careers/+page.server.ts
@@ -1,11 +1,15 @@
-import type { PageServerLoad } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
 import { redirect } from "@sveltejs/kit";
 
 import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 
+import { generateFormMessageFromHttpResponse, generateFormMessageFromInvalidForm } from "$lib/utils";
 import { addCarrerSchema, updateCarrerSchema } from "./schema";
+
 import { LoadFacultiesWithComboMessages } from "$lib/api/controller/api/faculty";
+import type { PostCareerRequest } from "$lib/api/model/admin/career";
+import { PostCareer } from "$lib/api/controller/admin/career";
 
 export const load: PageServerLoad = async ({ locals, cookies }) => {
     if (!locals.user) {
@@ -19,4 +23,27 @@ export const load: PageServerLoad = async ({ locals, cookies }) => {
         addCarrerForm: await superValidate(zod(addCarrerSchema)),
         updateCarrerForm: await superValidate(zod(updateCarrerSchema))
     }
+};
+
+export const actions: Actions = {
+    addCareer: async (event) => {
+        const form = await superValidate(event, zod(addCarrerSchema))
+
+        if (!form.valid) {
+            return generateFormMessageFromInvalidForm(form)
+        }
+
+        const token = event.cookies.get("AuthorizationToken")
+        const data = form.data
+        const person: PostCareerRequest = {
+            faculty_id: data.facultyID,
+            name: data.name,
+            description: data.description,
+            abbreviation: data.abbreviation
+        }
+
+        const response = await PostCareer(person, token!)
+
+        return generateFormMessageFromHttpResponse(form, response)
+    },
 };
