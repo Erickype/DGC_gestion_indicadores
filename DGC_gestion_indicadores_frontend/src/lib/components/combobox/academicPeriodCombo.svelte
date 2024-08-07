fbfeeabde31e202e8a6fe5f5d0197ffa810ad515
Erick Carrasco <erickype@hotmail.com>
2024-08-07 12:57:46 -0500

    Remove of deleted property on combobox

--- DGC_gestion_indicadores_frontend/src/lib/components/combobox/academicPeriodCombo.svelte
+++ DGC_gestion_indicadores_frontend/src/lib/components/combobox/academicPeriodCombo.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
-	import Combobox from "./combobox.svelte";
-	import type { Message } from "./combobox";
+	import Combobox from './combobox.svelte';
+	import type { Message } from './combobox';
 
-    export let messages: Message[]
-    export let selectedValue: number
-    export let pixelsWidth: string = "300"
-    
+	export let messages: Message[];
+	export let selectedValue: number;
 </script>
 
 <Combobox
 	data={messages}
 	placeholder="Seleccionar periodo académico..."
 	bind:selectedValue
-	bind:pixelsWidth
 	emptyMessage="No se encontró un periodo"
 	on:message
 ></Combobox>
