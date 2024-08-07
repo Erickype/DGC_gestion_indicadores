e8656f194cc0f641a7775e3b225f02f884e24899
Erick Carrasco <erickype@hotmail.com>
2024-08-07 16:19:10 -0500

    Add contracType route and model

---
+++ DGC_gestion_indicadores_frontend/src/lib/api/model/api/contractType.ts
@@ -0,0 +1,8 @@
+export interface ContractType {
+    CreatedAt: string;
+    UpdatedAt: string;
+    ID: number;
+    name: string;
+    abbreviation: string;
+    description: string | null;
+}
---
+++ DGC_gestion_indicadores_frontend/src/lib/api/routes/api/contractType.ts
@@ -0,0 +1,3 @@
+import { apiBaseURL } from "./base";
+
+export const getContractTypesRoute = apiBaseURL + "/contractTypes"
