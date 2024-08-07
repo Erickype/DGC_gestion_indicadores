dd545386ca3c366a95f05f8902f18aaefc6d5f92
Erick Carrasco <erickype@hotmail.com>
2024-08-07 16:20:59 -0500

    Add error management to GetTeachersByAcademicPeriodID api

--- DGC_gestion_indicadores_frontend/src/routes/api/teacher/byAcademicPeriodID/[academicPeriodID]/+server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/api/teacher/byAcademicPeriodID/[academicPeriodID]/+server.ts
@@ -1,13 +1,14 @@
 import { GetTeachersByAcademicPeriodID } from "$lib/api/controller/api/teacher";
-import { error, json, type RequestHandler } from "@sveltejs/kit";
+import { generateErrorFromCommonError, type CommonError } from "$lib/api/model/errors";
+import { json, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({ params, cookies }) => {
     const academicPeriodID = params.academicPeriodID;
     const token = cookies.get("AuthorizationToken");
-    const res = await GetTeachersByAcademicPeriodID(token!, academicPeriodID!);
+    const response = await GetTeachersByAcademicPeriodID(token!, academicPeriodID!);
 
-    if (!res.ok) {
-        throw error(res.status);
+    if ((response as CommonError).status_code) {
+        return generateErrorFromCommonError(response as CommonError)
     }
-    return json(await res.json());
+    return json(response);
 }
