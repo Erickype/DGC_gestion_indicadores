3bb1a26f8323931cfed46fe017556d574200deb2
Erick Carrasco <erickype@hotmail.com>
2024-08-08 11:46:21 -0500

    Add "More" action to common actinos table

--- DGC_gestion_indicadores_frontend/src/lib/components/table/tableActions.svelte
+++ DGC_gestion_indicadores_frontend/src/lib/components/table/tableActions.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import ChevronRight from 'lucide-svelte/icons/chevron-right';
 	import CircleX from 'lucide-svelte/icons/circle-x';
 	import Pencil from 'lucide-svelte/icons/pencil';
 
@@ -7,6 +8,8 @@
 	import { createEventDispatcher } from 'svelte';
 
 	export let id: string;
+	export let moreAction = false;
+	export let moreActionURL: string | null = null;
 
 	let dialogOpen = false;
 
@@ -33,6 +36,11 @@
 	<Button variant="ghost" size="icon" on:click={sendUpdateAction}>
 		<Pencil class="h-4 w-4" />
 	</Button>
+	{#if moreAction && moreActionURL}
+		<Button variant="ghost" size="icon" href={moreActionURL + id}>
+			<ChevronRight class="h-4 w-4" />
+		</Button>
+	{/if}
 	<Button
 		variant="ghost"
 		size="icon"
