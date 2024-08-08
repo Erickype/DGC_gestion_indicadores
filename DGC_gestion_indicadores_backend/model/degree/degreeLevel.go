b23728ea22150aefcaa9e88b18be97ae999cec5d
Erick Carrasco <erickype@hotmail.com>
2024-08-08 17:09:58 -0500

    Add degree level model

---
+++ DGC_gestion_indicadores_backend/model/degree/degreeLevel.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+type DegreeLevel struct {
+	ID           uint      `gorm:"primary_key"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	Name         string    `gorm:"size:200;not null;unique" json:"name"`
+	Abbreviation string    `gorm:"size:50;not null;unique" json:"abbreviation"`
+	Description  string    `gorm:"size:200;" json:"description,omitempty"`
+}
