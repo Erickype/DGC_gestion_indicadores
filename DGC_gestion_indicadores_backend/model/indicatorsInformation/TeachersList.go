e1dd0d8940291e78c34059a499ad0eb02b1b8ed4
Erick Carrasco <erickype@hotmail.com>
2024-08-08 17:15:32 -0500

    Add TeachersList model wich will contain the information to calcuate the indicadors

---
+++ DGC_gestion_indicadores_backend/model/indicatorsInformation/TeachersList.go
@@ -0,0 +1,34 @@
+package model
+
+import (
+	"github.com/Erickype/DGC_gestion_indicadores_backend/model"
+	academicPeriod "github.com/Erickype/DGC_gestion_indicadores_backend/model/academicPeriod"
+	career "github.com/Erickype/DGC_gestion_indicadores_backend/model/career"
+	contractType "github.com/Erickype/DGC_gestion_indicadores_backend/model/contractType"
+	dedication "github.com/Erickype/DGC_gestion_indicadores_backend/model/dedication"
+	scaledGrade "github.com/Erickype/DGC_gestion_indicadores_backend/model/scaledGrade"
+	teacher "github.com/Erickype/DGC_gestion_indicadores_backend/model/teacher"
+	"time"
+)
+
+type TeachersList struct {
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
+	AcademicPeriodID uint      `gorm:"primary_key;" json:"academic_period_id"`
+	TeacherID        uint      `gorm:"primary_key;" json:"teacher_id"`
+	CareerID         uint      `gorm:"not null;" json:"career_id"`
+	DedicationID     uint      `gorm:"not null;" json:"dedication_id"`
+	ScaledGradeID    uint      `gorm:"not null;" json:"scaled_grade_id"`
+	ContractTypeID   uint      `gorm:"not null;" json:"contract_type_id"`
+
+	AcademicPeriod academicPeriod.AcademicPeriod `json:"-"`
+	Teacher        teacher.Teacher               `json:"-"`
+	Career         career.Career                 `json:"-"`
+	Dedication     dedication.Dedication         `json:"-"`
+	ScaledGrade    scaledGrade.ScaledGrade       `json:"-"`
+	ContractType   contractType.ContractType     `json:"-"`
+}
+
+func (tl *TeachersList) TableName() string {
+	return model.IndicatorsInformationSchema + ".teachers_lists"
+}
