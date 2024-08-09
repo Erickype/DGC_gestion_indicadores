903144b75d35980cbda0e5ee53ab617e5f22e4b3
Erick Carrasco <erickype@hotmail.com>
2024-08-09 10:43:01 -0500

    Add academic production list model

---
+++ DGC_gestion_indicadores_backend/model/indicatorsInformation/academicProductionList.go
@@ -0,0 +1,34 @@
+package model
+
+import (
+	"github.com/Erickype/DGC_gestion_indicadores_backend/model"
+	academicPeriod "github.com/Erickype/DGC_gestion_indicadores_backend/model/academicPeriod"
+	academicProduction "github.com/Erickype/DGC_gestion_indicadores_backend/model/academicProduction"
+	faculty "github.com/Erickype/DGC_gestion_indicadores_backend/model/faculty"
+	"gorm.io/datatypes"
+	"time"
+)
+
+type AcademicProductionList struct {
+	DOI                    string         `gorm:"primary_key;size:1000" json:"DOI"`
+	CreatedAt              time.Time      `json:"created_at"`
+	UpdatedAt              time.Time      `json:"updated_at"`
+	AcademicPeriodID       uint           `json:"academic_period_id"`
+	PublicationDate        datatypes.Date `gorm:"not null" json:"publication_date"`
+	PublicationName        string         `gorm:"size:1000;not null" json:"publication_name"`
+	PublicationTypeID      uint16         `json:"publication_type_id"`
+	FacultyID              uint           `json:"faculty_id"`
+	ScienceMagazineID      uint           `json:"science_magazine_id"`
+	ImpactCoefficientID    uint           `json:"impact_coefficient_id"`
+	InterculturalComponent bool           `json:"intercultural_component"`
+
+	PublicationType   academicProduction.PublicationType   `json:"-"`
+	AcademicPeriod    academicPeriod.AcademicPeriod        `json:"-"`
+	Faculty           faculty.Faculty                      `json:"-"`
+	ScienceMagazine   academicProduction.ScienceMagazine   `json:"-"`
+	ImpactCoefficient academicProduction.ImpactCoefficient `json:"-"`
+}
+
+func (apl *AcademicProductionList) TableName() string {
+	return model.IndicatorsInformationSchema + ".academic_production_lists"
+}
