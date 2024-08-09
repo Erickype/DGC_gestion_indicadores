e445cc1f5a82f353824ef1da9d3140f289aa6f63
Erick Carrasco <erickype@hotmail.com>
2024-08-09 10:42:18 -0500

    Rename off teachersListDegree and pass to indicatorsInformation shema

--- DGC_gestion_indicadores_backend/model/indicatorsInformation/teachersListsDegree.go
+++ DGC_gestion_indicadores_backend/model/indicatorsInformation/teachersListsDegree.go
@@ -1,16 +1,22 @@
 package model
 
 import (
+	"github.com/Erickype/DGC_gestion_indicadores_backend/model"
 	academicPeriod "github.com/Erickype/DGC_gestion_indicadores_backend/model/academicPeriod"
+	degree "github.com/Erickype/DGC_gestion_indicadores_backend/model/degree"
 	"time"
 )
 
-type TeachersDegreesAcademicPeriod struct {
+type TeachersListsDegree struct {
 	CreatedAt        time.Time `json:"created_at"`
 	UpdatedAt        time.Time `json:"updated_at"`
 	AcademicPeriodID uint      `gorm:"primary_key" json:"academic_period_id"`
 	TeachersDegreeID uint      `gorm:"primary_key" json:"teachers_degree_id"`
 
 	AcademicPeriod academicPeriod.AcademicPeriod `json:"-"`
-	TeachersDegree TeachersDegree                `json:"-"`
+	TeachersDegree degree.TeachersDegree         `json:"-"`
+}
+
+func (tl *TeachersListsDegree) TableName() string {
+	return model.IndicatorsInformationSchema + ".teachers_lists_degrees"
 }
