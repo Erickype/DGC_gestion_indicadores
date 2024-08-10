c4cd2e80b0f9cfe23b5c832cda576174c4d8d623
Erick Carrasco <erickype@hotmail.com>
2024-08-10 10:34:31 -0500

    Add BookOrChaptersProductionListAuthor model

---
+++ DGC_gestion_indicadores_backend/model/indicatorsInformation/books_or_chapter_production_lists_author.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"github.com/Erickype/DGC_gestion_indicadores_backend/model"
+	academicProduction "github.com/Erickype/DGC_gestion_indicadores_backend/model/academicProduction"
+	"time"
+)
+
+type BooksOrChaptersProductionListAuthor struct {
+	CreatedAt                        time.Time `json:"created_at"`
+	UpdatedAt                        time.Time `json:"updated_at"`
+	BooksOrChaptersProductionListDOI uint      `gorm:"primary_key" json:"books_or_chapters_production_list_doi;"`
+	AuthorID                         uint      `gorm:"primary_key" json:"author_id"`
+
+	BooksOrChaptersProductionList BooksOrChaptersProductionList `json:"-"`
+	Author                        academicProduction.Author     `json:"-"`
+}
+
+func (bc BooksOrChaptersProductionListAuthor) TableName() string {
+	return model.IndicatorsInformationSchema + ".books_or_chapter_production_lists_authors"
+}
