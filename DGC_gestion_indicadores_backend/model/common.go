60dbd982f51ffdbcc5a199c47aa24a5397e15b75
Erick Carrasco <erickype@hotmail.com>
2024-08-11 13:04:54 -0500

    Add gorm scope for pagination

--- DGC_gestion_indicadores_backend/model/common.go
+++ DGC_gestion_indicadores_backend/model/common.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"gorm.io/gorm"
+)
+
 type CommonDeleteResponse struct {
 	Deleted    bool   `json:"deleted"`
 	EntityName string `json:"entity_name"`
@@ -11,5 +15,22 @@ func CreateCommonDeleteResponse(EntityName string, IDDeleted int) *CommonDeleteR
 		Deleted:    true,
 		EntityName: EntityName,
 		IDDeleted:  IDDeleted,
+	}
+}
+
+func Paginate(pageSize, page int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if page <= 0 {
+			page = 1
+		}
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
 	}
 }
