b5f4cc2107a73f83116377d455fdb68e4446bd8e
Erick Carrasco <erickype@hotmail.com>
2024-08-24 14:12:51 -0500

    Add scrollable to static table

--- DGC_gestion_indicadores_frontend/src/lib/components/table/table.svelte
+++ DGC_gestion_indicadores_frontend/src/lib/components/table/table.svelte
@@ -4,6 +4,7 @@
 
 	import ArrowUpDown from 'lucide-svelte/icons/arrow-up-down';
 
+	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Button } from '$lib/components/ui/button';
 	import * as Table from '$lib/components/ui/table';
@@ -11,7 +12,8 @@
 	export let table: HeadlessTable<any, any>;
 	export let columns: Column<any, any>[];
 	export let filterFields: string[];
-	export let tableHeightClass = "h-[60vh]"
+	export let tableHeightClass = 'h-[60vh]';
+	export let itemCount: number;
 
 	const { headerRows, pageRows, tableAttrs, tableBodyAttrs, pluginStates } =
 		table.createViewModel(columns);
@@ -33,47 +35,52 @@
 	/>
 </div>
 <div class="flex rounded-md border {tableHeightClass} overflow-auto">
-	<Table.Root {...$tableAttrs}>
-		<Table.Header>
-			{#each $headerRows as headerRow}
-				<Subscribe rowAttrs={headerRow.attrs()}>
-					<Table.Row>
-						{#each headerRow.cells as cell (cell.id)}
-							<Subscribe attrs={cell.attrs()} let:attrs props={cell.props()} let:props>
-								<Table.Head {...attrs}>
-									{#if filterFields.find((field) => cell.id === field)}
-										<Button class="p-0" variant="ghost" on:click={props.sort.toggle}>
+	<ScrollArea class="w-full rounded-md border">
+		<Table.Root {...$tableAttrs}>
+			<Table.Header>
+				{#each $headerRows as headerRow}
+					<Subscribe rowAttrs={headerRow.attrs()}>
+						<Table.Row>
+							{#each headerRow.cells as cell (cell.id)}
+								<Subscribe attrs={cell.attrs()} let:attrs props={cell.props()} let:props>
+									<Table.Head {...attrs}>
+										{#if filterFields.find((field) => cell.id === field)}
+											<Button variant="ghost" on:click={props.sort.toggle}>
+												<Render of={cell.render()} />
+												<ArrowUpDown class={'stroke-primary ml-2 h-4 w-4'} />
+											</Button>
+										{:else}
 											<Render of={cell.render()} />
-											<ArrowUpDown class={'ml-2 h-4 w-4'} />
-										</Button>
-									{:else}
+										{/if}
+									</Table.Head>
+								</Subscribe>
+							{/each}
+						</Table.Row>
+					</Subscribe>
+				{/each}
+			</Table.Header>
+			<Table.Body {...$tableBodyAttrs}>
+				{#each $pageRows as row (row.id)}
+					<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
+						<Table.Row {...rowAttrs}>
+							{#each row.cells as cell (cell.id)}
+								<Subscribe attrs={cell.attrs()} let:attrs>
+									<Table.Cell {...attrs}>
 										<Render of={cell.render()} />
-									{/if}
-								</Table.Head>
-							</Subscribe>
-						{/each}
-					</Table.Row>
-				</Subscribe>
-			{/each}
-		</Table.Header>
-		<Table.Body {...$tableBodyAttrs}>
-			{#each $pageRows as row (row.id)}
-				<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-					<Table.Row {...rowAttrs}>
-						{#each row.cells as cell (cell.id)}
-							<Subscribe attrs={cell.attrs()} let:attrs>
-								<Table.Cell {...attrs}>
-									<Render of={cell.render()} />
-								</Table.Cell>
-							</Subscribe>
-						{/each}
-					</Table.Row>
-				</Subscribe>
-			{/each}
-		</Table.Body>
-	</Table.Root>
+									</Table.Cell>
+								</Subscribe>
+							{/each}
+						</Table.Row>
+					</Subscribe>
+				{/each}
+			</Table.Body>
+		</Table.Root>
+	</ScrollArea>
 </div>
 <div class="flex items-center justify-end space-x-4 py-4">
+	<dir class="m-0 p-0">
+		{`${$pageIndex * $pageSize + $pageRows.length}/${itemCount}`}
+	</dir>
 	<Button
 		variant="outline"
 		size="sm"
