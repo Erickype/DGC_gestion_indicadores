24ebed833d39ff8b3a0711754e6851e179be959a
Erick Carrasco <erickype@hotmail.com>
2024-08-26 12:01:30 -0500

    Modify popoverFilter to prevent submit on enter

--- DGC_gestion_indicadores_frontend/src/lib/components/table/popoverFilter.svelte
+++ DGC_gestion_indicadores_frontend/src/lib/components/table/popoverFilter.svelte
@@ -3,7 +3,6 @@
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
 	import * as Popover from '$lib/components/ui/popover/index.js';
-
 	import Filter from 'lucide-svelte/icons/filter';
 	import type { PopoverFilterDataMap } from './types';
 	import { createEventDispatcher } from 'svelte';
@@ -13,14 +12,21 @@
 	const dispatch = createEventDispatcher();
 
 	function handleOnClickDetailedFilter() {
-		return dispatch('detailedFilter');
+		dispatch('detailedFilter');
+	}
+
+	function handleKeyDown(event: KeyboardEvent) {
+		if (event.key === 'Enter') {
+			event.preventDefault();
+			handleOnClickDetailedFilter();
+		}
 	}
 </script>
 
 <Popover.Root portal={null}>
 	<Popover.Trigger asChild let:builder>
 		<Button builders={[builder]} variant="outline">
-			<Filter class={'h-4 w-4'} />
+			<Filter class="h-4 w-4" />
 		</Button>
 	</Popover.Trigger>
 	<Popover.Content class="w-80">
@@ -29,20 +35,25 @@
 				<h4 class="font-medium leading-none">Filtro detallado</h4>
 				<p class="text-muted-foreground text-sm">Ingrese los criterios a buscar</p>
 			</div>
-			<form class="flex flex-col gap-1" on:submit={handleOnClickDetailedFilter}>
+			<div class="flex flex-col gap-1">
 				<div class="grid gap-2">
 					{#each popoverFilterDataMap.entries() as [key, popoverFilterData]}
 						<div class="grid grid-cols-3 items-center gap-4">
 							<Label for={key}>{popoverFilterData.label}</Label>
-							<Input id={key} bind:value={popoverFilterData.value} class="col-span-2 h-8" />
+							<Input
+								id={key}
+								bind:value={popoverFilterData.value}
+								class="col-span-2 h-8"
+								on:keydown={handleKeyDown}
+							/>
 						</div>
 					{/each}
 				</div>
-				<Button type="submit" variant="secondary" size="sm" on:click={handleOnClickDetailedFilter}>
+				<Button type="button" variant="secondary" size="sm" on:click={handleOnClickDetailedFilter}>
 					Filtrar
-					<Filter class={'ml-2 h-4 w-4'} />
+					<Filter class="ml-2 h-4 w-4" />
 				</Button>
-			</form>
+			</div>
 		</div>
 	</Popover.Content>
 </Popover.Root>
