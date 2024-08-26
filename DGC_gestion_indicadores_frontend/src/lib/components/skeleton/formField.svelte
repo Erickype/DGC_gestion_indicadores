3d8ccb8a29393a79a9bb91ae0d335ae8b0b57cd7
Erick Carrasco <erickype@hotmail.com>
2024-08-26 11:56:15 -0500

    Add form field skeleton component

---
+++ DGC_gestion_indicadores_frontend/src/lib/components/skeleton/formField.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Skeleton } from '../ui/skeleton';
+</script>
+
+<div class="w-full p-0">
+	<Skeleton class="mb-2 h-4 w-14" />
+	<div class="flex w-full items-center rounded-sm">
+		<Skeleton class="h-10 w-full" />
+	</div>
+</div>
