536ce64583e025bec5b6ad0cfc68e93d71ade293
Erick Carrasco <erickype@hotmail.com>
2024-08-31 16:26:28 -0500

    Modify update career form to use common FormSelect

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(institution)/institution/careers/UpdateForm.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(institution)/institution/careers/UpdateForm.svelte
@@ -1,33 +1,30 @@
 <script lang="ts">
 	import SuperDebug, { superForm, type Infer, type SuperValidated } from 'sveltekit-superforms';
+	import { updateCarrerSchema, type UpdateCarrerSchema } from './schema';
 	import { zodClient } from 'sveltekit-superforms/adapters';
 	import { browser } from '$app/environment';
 
-	import { cn } from '$lib/utils.js';
+	import { createEventDispatcher, onMount } from 'svelte';
+
 	import * as Form from '$lib/components/ui/form';
-	import * as Popover from '$lib/components/ui/popover/index.js';
-	import * as Command from '$lib/components/ui/command/index.js';
-	import { buttonVariants } from '$lib/components/ui/button';
-
-	import Check from 'lucide-svelte/icons/check';
-	import ChevronsUpDown from 'lucide-svelte/icons/chevrons-up-down';
-
-	import { createEventDispatcher, tick } from 'svelte';
-
 	import { Input } from '$lib/components/ui/input';
 	import { toast } from 'svelte-sonner';
 
-	import { updateCarrerSchema, type UpdateCarrerSchema } from './schema';
-	import type { Career } from '$lib/api/model/api/career';
-	import type { Message } from '$lib/components/combobox/combobox';
 	import { manageToastFromErrorMessageOnAddForm, manageToastFromInvalidAddForm } from '$lib/utils';
+	import FormFieldSkeleton from '$lib/components/skeleton/formField.svelte';
+	import FormSelect from '$lib/components/combobox/formSelect.svelte';
+	import type { Faculty } from '$lib/api/model/api/faculty';
+	import type { Career } from '$lib/api/model/api/career';
+	import {
+		fetchFaculties,
+		GenerateComboMessagesFromFaculties
+	} from '$lib/components/filters/faculties';
+	import { writable } from 'svelte/store';
 
 	export let data: SuperValidated<Infer<UpdateCarrerSchema>, App.Superforms.Message>;
-	export let comboMessages: [Message[]];
 	export let updateEntity: Career;
 
-	const facultiesComboData = comboMessages.at(0) as Message[];
-	let openFaculty = false;
+	const facultiesPromise: Promise<Faculty[]> = fetchFaculties();
 
 	const dispatch = createEventDispatcher();
 
@@ -54,6 +51,7 @@
 			return manageToastFromErrorMessageOnAddForm(message);
 		}
 	});
+	const { form: formData, enhance } = form;
 
 	function fillForm() {
 		$formData.ID = updateEntity!.ID;
@@ -61,24 +59,15 @@
 		$formData.name = updateEntity!.name;
 		$formData.abbreviation = updateEntity!.abbreviation;
 		$formData.description = updateEntity!.description!;
+		$formDataFacultyID = $formData.facultyID;
 	}
 
-	const { form: formData, enhance } = form;
+	let formDataFacultyID = writable($formData.facultyID);
+	formDataFacultyID.subscribe((value) => ($formData.facultyID = value));
 
-	$: {		
-		$formData.ID = updateEntity!.ID;
-		$formData.facultyID = updateEntity!.faculty_id;
-		$formData.name = updateEntity!.name;
-		$formData.abbreviation = updateEntity!.abbreviation;
-		$formData.description = updateEntity!.description!;
-	}
-
-	function closeAndFocusTriggerFaculty(triggerId: string) {
-		openFaculty = false;
-		tick().then(() => {
-			document.getElementById(triggerId)?.focus();
-		});
-	}
+	onMount(async () => {
+		fillForm();
+	});
 </script>
 
 <form action="?/updateCareer" use:enhance>
@@ -89,50 +78,15 @@
 			</Form.Control>
 		</Form.Field>
 		<Form.Field class="flex flex-col" {form} name="facultyID">
-			<Popover.Root bind:open={openFaculty} let:ids>
-				<Form.Control let:attrs>
-					<Form.Label>Facultad asignada</Form.Label>
-					<Popover.Trigger
-						class={cn(
-							buttonVariants({ variant: 'outline' }),
-							'w-full justify-between',
-							!$formData.facultyID && 'text-muted-foreground'
-						)}
-						role="combobox"
-						{...attrs}
-					>
-						{facultiesComboData.find((f) => f.value === $formData.facultyID)?.label ??
-							'Seleccionar facultad'}
-						<ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
-					</Popover.Trigger>
-					<input hidden value={$formData.facultyID} name={attrs.name} />
-				</Form.Control>
-				<Popover.Content class="w-[90%] p-0">
-					<Command.Root>
-						<Command.Input autofocus placeholder="Buscar facultad..." class="h-9" />
-						<Command.Empty>Sin coincidencias!</Command.Empty>
-						<Command.Group>
-							{#each facultiesComboData as faculty}
-								<Command.Item
-									value={faculty.label}
-									onSelect={() => {
-										$formData.facultyID = faculty.value;
-										closeAndFocusTriggerFaculty(ids.trigger);
-									}}
-								>
-									{faculty.label}
-									<Check
-										class={cn(
-											'ml-auto h-4 w-4',
-											faculty.value !== $formData.facultyID && 'text-transparent'
-										)}
-									/>
-								</Command.Item>
-							{/each}
-						</Command.Group>
-					</Command.Root>
-				</Popover.Content>
-			</Popover.Root>
+			{#await facultiesPromise}
+				<FormFieldSkeleton />
+			{:then faculties}
+				<FormSelect
+					formLabel="Facultad"
+					comboData={GenerateComboMessagesFromFaculties(faculties)}
+					bind:formDataID={formDataFacultyID}
+				/>
+			{/await}
 			<Form.FieldErrors />
 		</Form.Field>
 		<Form.Field {form} name="name">
