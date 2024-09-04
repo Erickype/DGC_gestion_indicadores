41d9e38fc818a9da43167858d91f9b44ac7ffcd7
Erick Carrasco <erickype@hotmail.com>
2024-09-03 22:44:08 -0500

    Add client method to fetch filterTeachersList api endpoint

--- DGC_gestion_indicadores_frontend/src/lib/api/controller/api/indicatorsInformation/teachersList.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/api/indicatorsInformation/teachersList.ts
@@ -1,6 +1,6 @@
-import type { CreateTeachersListsRequest, TeachersList } from "$lib/api/model/api/indicatorsInformation/teachersLists";
+import type { CreateTeachersListsRequest, FilterTeachersListsByAcademicPeriodRequest, FilterTeachersListsByAcademicPeriodResponse, TeachersList } from "$lib/api/model/api/indicatorsInformation/teachersLists";
 import type { CommonError } from "$lib/api/model/errors";
-import { postTeachersListRoute } from "$lib/api/routes/api/indicatorsInformation/teachersLists";
+import { postFilterTeachersListByAcademicPeriodIDRoute, postTeachersListRoute } from "$lib/api/routes/api/indicatorsInformation/teachersLists";
 import { generateCommonErrorFromFetchError } from "$lib/utils";
 
 export async function CreateTeachersList(token: string, request: CreateTeachersListsRequest) {
@@ -19,6 +19,28 @@ export async function CreateTeachersList(token: string, request: CreateTeachersL
         }
         const teacherCreated: TeachersList = await response.json()
         return teacherCreated;
+    } catch (error) {
+        return generateCommonErrorFromFetchError(error)
+    }
+}
+
+export async function PostFilterTeachersListsByAcademicPeriodID(token: string, request: FilterTeachersListsByAcademicPeriodRequest) {
+    try {
+        const response = await fetch(postFilterTeachersListByAcademicPeriodIDRoute, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            },
+            body: JSON.stringify(request)
+        });
+
+        if (!response.ok) {
+            const error: CommonError = await response.json()
+            return error
+        }
+        const filterTeachersListsByAcademicPeriodResponse: FilterTeachersListsByAcademicPeriodResponse[] = await response.json()
+        return filterTeachersListsByAcademicPeriodResponse
     } catch (error) {
         return generateCommonErrorFromFetchError(error)
     }
