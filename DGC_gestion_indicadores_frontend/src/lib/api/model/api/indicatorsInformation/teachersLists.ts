79e0dc2e37a6886f7ed21b76317bc268ad65350b
Erick Carrasco <erickype@hotmail.com>
2024-09-03 22:43:35 -0500

    Add client models and route to filterTeachersList

--- DGC_gestion_indicadores_frontend/src/lib/api/model/api/indicatorsInformation/teachersLists.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/model/api/indicatorsInformation/teachersLists.ts
@@ -1,4 +1,4 @@
-export interface TeachersList{
+export interface TeachersList {
     created_at: string;
     updated_at: string;
     academic_period_id: number;
@@ -16,4 +16,38 @@ export interface CreateTeachersListsRequest {
     dedication_id: number;
     scaled_grade_id: number;
     contract_type_id: number;
+}
+
+export interface FilterTeachersListsByAcademicPeriodRequest {
+    academic_period_id?: number,
+    teacher_identity?: string,
+    teacher_name?: string,
+    teacher_lastname?: string,
+    career?: string,
+    dedication?: string,
+    scaled_grade?: string,
+    contract_type?: string,
+    page_size: number,
+    page: number
+}
+
+export interface TeachersListByAcademicPeriodJoined {
+    teacher_id: number,
+    teacher_identity: string,
+    teacher: string,
+    career_id: number,
+    career: string,
+    dedication_id: number,
+    dedication: string,
+    scaled_grade_id: number,
+    scaled_grade: string,
+    contract_type_id: number,
+    contract_type: string
+}
+
+export interface FilterTeachersListsByAcademicPeriodResponse {
+    count: number,
+    page_size: number,
+    page: number,
+    teachers_lists: TeachersListByAcademicPeriodJoined[]
 }
--- DGC_gestion_indicadores_frontend/src/lib/api/routes/api/indicatorsInformation/teachersLists.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/routes/api/indicatorsInformation/teachersLists.ts
@@ -1,3 +1,4 @@
 import { indicatorsInformationBaseRoute } from "./base";
 
 export const postTeachersListRoute = indicatorsInformationBaseRoute + "/teachersList"
+export const postFilterTeachersListByAcademicPeriodIDRoute = indicatorsInformationBaseRoute + "/teachersList/filter"
