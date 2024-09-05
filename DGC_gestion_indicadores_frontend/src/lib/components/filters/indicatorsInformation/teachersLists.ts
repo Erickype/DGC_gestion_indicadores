d5ce0d8a5db41561d857bcb656310b8b4e1eaa8d
Erick Carrasco <erickype@hotmail.com>
2024-09-05 08:55:40 -0500

    Add functions to filter in teachers lists

--- DGC_gestion_indicadores_frontend/src/lib/components/filters/indicatorsInformation/teachersLists.ts
+++ DGC_gestion_indicadores_frontend/src/lib/components/filters/indicatorsInformation/teachersLists.ts
@@ -33,7 +33,7 @@ export function newPopoverFilterDataMap(): PopoverFilterDataMap {
     return popoverFilterDataMap
 }
 
-export async function fetchFiterTeachersLists(filterTeachersListsByAcademicPeriodRequest: FilterTeachersListsByAcademicPeriodRequest):
+export async function fetchFilterTeachersLists(filterTeachersListsByAcademicPeriodRequest: FilterTeachersListsByAcademicPeriodRequest):
     Promise<FilterTeachersListsByAcademicPeriodResponse> {
     const url = `/api/indicatorsInformation/teachersLists/filter`;
     const response = await fetch(url, {
@@ -48,4 +48,84 @@ export async function fetchFiterTeachersLists(filterTeachersListsByAcademicPerio
         throw errorData;
     }
     return response.json();
+}
+
+export function generateInitialFilterValue(filterTeachersListsRequest: FilterTeachersListsByAcademicPeriodRequest): string | undefined {
+    let values = [
+        filterTeachersListsRequest.teacher_identity,
+        filterTeachersListsRequest.teacher_name,
+        filterTeachersListsRequest.teacher_lastname,
+        filterTeachersListsRequest.career,
+        filterTeachersListsRequest.dedication,
+        filterTeachersListsRequest.scaled_grade,
+        filterTeachersListsRequest.contract_type,
+    ];
+
+    let uniqueValues = [...new Set(values.filter((value) => value !== ''))];
+
+    let initialFilterValue: string | undefined =
+        uniqueValues.length === 1 ? uniqueValues[0] : uniqueValues.join(' ');
+
+    return initialFilterValue
+}
+
+export async function fetchOnFilterChanged(filter: string, filterTeachersListsByAcademicPeriodRequest: FilterTeachersListsByAcademicPeriodRequest, popoverFilterDataMap: PopoverFilterDataMap): Promise<FilterTeachersListsByAcademicPeriodResponse> {
+    popoverFilterDataMap.forEach((_, key) => {
+        (filterTeachersListsByAcademicPeriodRequest as any)[key] = filter;
+    });
+
+    return fetchFilterTeachersLists(filterTeachersListsByAcademicPeriodRequest).then(
+        (response: FilterTeachersListsByAcademicPeriodResponse) => {
+            if (response.count === 0) {
+                toast.warning(`No hay datos para el filtro: ${filter}`);
+                popoverFilterDataMap.forEach((_, key) => {
+                    (filterTeachersListsByAcademicPeriodRequest as any)[key] = '';
+                });
+                return fetchFilterTeachersLists(filterTeachersListsByAcademicPeriodRequest);
+            }
+            return response;
+        }
+    );
+}
+
+export async function fetchOnDetailedFilter(
+    filterTeachersRequest: FilterTeachersListsByAcademicPeriodRequest,
+    popoverFilterDataMap: PopoverFilterDataMap
+): Promise<{ request: FilterTeachersListsByAcademicPeriodRequest, response: FilterTeachersListsByAcademicPeriodResponse }> {
+
+    let request: FilterTeachersListsByAcademicPeriodRequest = {
+        academic_period_id: filterTeachersRequest.academic_period_id,
+        page: filterTeachersRequest.page,
+        page_size: filterTeachersRequest.page_size
+    };
+
+    popoverFilterDataMap.forEach((item, key) => {
+        if (item.value !== '') {
+            (request as any)[key] = item.value;
+        }
+    });
+
+    filterTeachersRequest = request;
+
+    const response = await fetchFilterTeachersLists(filterTeachersRequest);
+
+    if (response.count === 0) {
+        let message = 'No hay datos para el filtro\n';
+        popoverFilterDataMap.forEach((item, _) => {
+            if (item.value !== '') {
+                message += `${item.label}: ${item.value}; `;
+            }
+        });
+        message = message.slice(0, message.length - 2);
+        toast.warning(message);
+
+        popoverFilterDataMap.forEach((_, key) => {
+            (filterTeachersRequest as any)[key] = '';
+        });
+
+        const newResponse = await fetchFilterTeachersLists(filterTeachersRequest);
+        return { request: filterTeachersRequest, response: newResponse };
+    }
+
+    return { request: filterTeachersRequest, response };
 }
