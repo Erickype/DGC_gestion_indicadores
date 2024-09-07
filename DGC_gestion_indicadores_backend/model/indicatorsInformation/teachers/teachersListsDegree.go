c873ecb0112136c7927baee6a0d16aa065e4adbd
Erick Carrasco <erickype@hotmail.com>
2024-09-07 18:57:32 -0500

    Add TeacherID reference to teachers lists degree

--- DGC_gestion_indicadores_backend/model/indicatorsInformation/teachers/teachersListsDegree.go
+++ DGC_gestion_indicadores_backend/model/indicatorsInformation/teachers/teachersListsDegree.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Erickype/DGC_gestion_indicadores_backend/model"
 	academicPeriod "github.com/Erickype/DGC_gestion_indicadores_backend/model/academicPeriod"
 	degree "github.com/Erickype/DGC_gestion_indicadores_backend/model/degree"
+	teacher "github.com/Erickype/DGC_gestion_indicadores_backend/model/teacher"
 	"time"
 )
 
@@ -11,9 +12,11 @@ type TeachersListsDegree struct {
 	CreatedAt        time.Time `json:"created_at"`
 	UpdatedAt        time.Time `json:"updated_at"`
 	AcademicPeriodID uint      `gorm:"primary_key" json:"academic_period_id"`
+	TeacherID        uint      `gorm:"primary_key" json:"teacher_id"`
 	TeachersDegreeID uint      `gorm:"primary_key" json:"teachers_degree_id"`
 
 	AcademicPeriod academicPeriod.AcademicPeriod `json:"-"`
+	Teacher        teacher.Teacher               `json:"-"`
 	TeachersDegree degree.TeachersDegree         `json:"-"`
 }
 
