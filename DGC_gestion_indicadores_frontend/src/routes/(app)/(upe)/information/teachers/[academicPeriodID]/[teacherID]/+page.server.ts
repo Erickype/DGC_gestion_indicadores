d5f26492f14b1d04be136839e8ca3187236951ba
Erick Carrasco <erickype@hotmail.com>
2024-09-08 11:47:02 -0500

    Add addTeachersListsDegreeSchema and path params

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/teachers/[academicPeriodID]/[teacherID]/+page.server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/teachers/[academicPeriodID]/[teacherID]/+page.server.ts
@@ -1,11 +1,17 @@
 import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
+import { superValidate } from "sveltekit-superforms";
+import { zod } from "sveltekit-superforms/adapters";
+import { addTeachersListsDegreeSchema } from "./schema";
 
 export const load: PageServerLoad = async ({ params }) => {
-    const param = parseInt(params.teacherID)
-    if(param > 0){
-        return{
-            data: "hi "+param
+    const academicPeriodID = parseInt(params.teacherID)
+    const teacherID = parseInt(params.teacherID)
+    if (academicPeriodID > 0 && teacherID > 0) {
+        return {
+            academicPeriodID: academicPeriodID,
+            teacherID: teacherID,
+            addTeachersListsDegreeForm: await superValidate(zod(addTeachersListsDegreeSchema))
         }
     }
 
