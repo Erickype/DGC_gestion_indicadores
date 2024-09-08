0522bf5dfc849fae03be47ef3d1933f32656437f
Erick Carrasco <erickype@hotmail.com>
2024-09-08 13:44:12 -0500

    Add AddDegreeAndTeachersListsDegree server action to post form

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/teachers/[academicPeriodID]/[teacherID]/+page.server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/teachers/[academicPeriodID]/[teacherID]/+page.server.ts
@@ -1,10 +1,13 @@
-import { error, redirect } from "@sveltejs/kit";
+import { error, redirect, type Actions } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
-import { addTeachersListsDegreeSchema } from "./schema";
+import { addDegreeAndTeachersListsDegreeSchema } from "./schema";
 import { LoadDegreeLevelsWithComboMessages } from "$lib/api/controller/api/degreeLevels";
 import { mainDashboarRoute } from "$lib/api/util/paths";
+import { generateFormMessageFromHttpResponse, generateFormMessageFromInvalidForm } from "$lib/utils";
+import type { AddDegreeAndTeachersListsDegreeRequest } from "$lib/api/model/api/indicatorsInformation/teachersListsDegree";
+import { AddDegreeAndTeachersListsDegree } from "$lib/api/controller/api/indicatorsInformation/teachersListsDegree";
 
 export const load: PageServerLoad = async ({ params, locals, cookies }) => {
     const token = cookies.get("AuthorizationToken")
@@ -17,16 +20,39 @@ export const load: PageServerLoad = async ({ params, locals, cookies }) => {
         throw redirect(302, mainDashboarRoute)
     }
 
-    const academicPeriodID = parseInt(params.teacherID)
+    const academicPeriodID = parseInt(params.academicPeriodID)
     const teacherID = parseInt(params.teacherID)
     if (academicPeriodID > 0 && teacherID > 0) {
         return {
             academicPeriodID: academicPeriodID,
             teacherID: teacherID,
-            addTeachersListsDegreeForm: await superValidate(zod(addTeachersListsDegreeSchema)),
+            addDegreeAndTeachersListsDegreeForm: await superValidate(zod(addDegreeAndTeachersListsDegreeSchema)),
             degreeLevelsData: await LoadDegreeLevelsWithComboMessages(token!)
         }
     }
 
     error(404, 'Not found');
+};
+
+export const actions: Actions = {
+    addDegreeAndTeachersListsDegree: async (event) => {
+        const form = await superValidate(event, zod(addDegreeAndTeachersListsDegreeSchema))
+
+        if (!form.valid) {
+            return generateFormMessageFromInvalidForm(form)
+        }
+
+        const token = event.cookies.get("AuthorizationToken")
+        const data = form.data
+        const addDegreeAndTeachersListsDegreeRequest: AddDegreeAndTeachersListsDegreeRequest = {
+            academic_period_id: data.academicPeriodID,
+            teacher_id: data.teacherID,
+            degree_level_id: data.degreeLevelID,
+            name: data.name
+        }
+
+        const response = await AddDegreeAndTeachersListsDegree(token!, addDegreeAndTeachersListsDegreeRequest)
+
+        return generateFormMessageFromHttpResponse(form, response)
+    },
 };
