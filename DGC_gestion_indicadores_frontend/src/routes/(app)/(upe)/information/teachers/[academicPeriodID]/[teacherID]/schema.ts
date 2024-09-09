0f36ceb92c54534d65ad021d765a528698cd92db
Erick Carrasco <erickype@hotmail.com>
2024-09-09 11:14:32 -0500

    Add updateDegreeAndTeachersListsDegreeSchema schema

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/teachers/[academicPeriodID]/[teacherID]/schema.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/teachers/[academicPeriodID]/[teacherID]/schema.ts
@@ -18,4 +18,21 @@ export const addDegreeAndTeachersListsDegreeSchema = z.object({
     }),
 });
 
+export const updateDegreeAndTeachersListsDegreeSchema = z.object({
+    teachersDegreeID: z.number().gt(0),
+
+    degreeLevelID: z.number({
+        required_error: "Nivel requerido"
+    }).gt(0),
+
+    name: z.string({
+        required_error: "Nombre requerida"
+    }).min(10, {
+        message: "Nombre mínimo 10 caracteres."
+    }).max(200, {
+        message: "Nombre máximo 200 caracteres."
+    }),
+});
+
 export type AddDegreeAndTeachersListsDegreeSchema = typeof addDegreeAndTeachersListsDegreeSchema;
+export type UpdateDegreeAndTeachersListsDegreeSchema = typeof updateDegreeAndTeachersListsDegreeSchema;
