c88642a982e66a4ea4880a54ece0694dd1ee8210
Erick Carrasco <erickype@hotmail.com>
2024-09-10 16:31:19 -0500

    Add form to agregate not assigned degree

---
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/teachers/[academicPeriodID]/[teacherID]/AddNotAssignedDegreeForm.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import SuperDebug, { superForm, type Infer, type SuperValidated } from 'sveltekit-superforms';
+	import { addDegreeNotAssignedSchema, type AddDegreeNotAssignedSchema } from './schema';
+	import { zodClient } from 'sveltekit-superforms/adapters';
+
+	import { createEventDispatcher } from 'svelte';
+	import { browser } from '$app/environment';
+	import { writable } from 'svelte/store';
+
+	import * as Form from '$lib/components/ui/form';
+	import { toast } from 'svelte-sonner';
+
+	import { manageToastFromErrorMessageOnAddForm, manageToastFromInvalidAddForm } from '$lib/utils';
+	import FormSelect from '$lib/components/combobox/formSelect.svelte';
+	import type { Message } from '$lib/components/combobox/combobox';
+
+	export let data: SuperValidated<Infer<AddDegreeNotAssignedSchema>, App.Superforms.Message>;
+	export let comboMessages: Message[][];
+	const notAssignedDegrees = comboMessages.at(0)!;
+
+	const dispatch = createEventDispatcher();
+
+	function DegreeNotAssignedAssigned() {
+		dispatch('message', {
+			created: true
+		});
+	}
+
+	const form = superForm(data, {
+		validators: zodClient(addDegreeNotAssignedSchema),
+		taintedMessage: null,
+		onUpdated: ({ form: f }) => {
+			const message = f.message;
+			if (!message) {
+				return manageToastFromInvalidAddForm();
+			}
+			if (message.success) {
+				DegreeNotAssignedAssigned();
+				return toast.success(`Título asignado.`);
+			}
+			return manageToastFromErrorMessageOnAddForm(message);
+		}
+	});
+
+	const { form: formData, enhance } = form;
+
+	console.log(notAssignedDegrees);
+	
+	let formDataTeachersDegreeID = writable($formData.teachersDegreeID);
+	formDataTeachersDegreeID.subscribe((value) => ($formData.teachersDegreeID = value));
+</script>
+
+<form action="?/addDegreeAndTeachersListsDegree" use:enhance>
+	<div class="flex flex-col gap-2">
+		<Form.Field {form} name="academicPeriodID">
+			<Form.Control let:attrs>
+				<input hidden value={$formData.academicPeriodID} name={attrs.name} />
+			</Form.Control>
+		</Form.Field>
+		<Form.Field {form} name="teacherID">
+			<Form.Control let:attrs>
+				<input hidden value={$formData.teacherID} name={attrs.name} />
+			</Form.Control>
+		</Form.Field>
+		<Form.Field {form} name="teachersDegreeID" class="flex flex-col">
+			<FormSelect
+				formLabel="Título"
+				comboData={notAssignedDegrees}
+				bind:formDataID={formDataTeachersDegreeID}
+			/>
+		</Form.Field>
+	</div>
+	<Form.Button class="my-2 w-full">Agregar</Form.Button>
+	<!-- {#if browser}
+		<SuperDebug data={$formData} />
+	{/if} -->
+</form>
