717b30f0a12f057784a918a07c5aa5394c7ec9b6
Erick Carrasco <erickype@hotmail.com>
2024-09-15 12:02:42 -0500

    Add top layout to fetch academic period in academic production

---
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/academic-publications/+page.server.ts
@@ -0,0 +1,20 @@
+import type { Actions, PageServerLoad } from "./$types";
+import { mainDashboarRoute } from "$lib/api/util/paths";
+import { redirect } from "@sveltejs/kit";
+
+import { LoadAcademicPeriodsWithComboMessages } from "$lib/api/controller/view/academicPeriod";
+
+export const load: PageServerLoad = async ({ locals, cookies }) => {
+    const token = cookies.get("AuthorizationToken")
+
+    if (!locals.user) {
+        throw redirect(302, "/login")
+    }
+
+    if (locals.user.role === 3) {
+        throw redirect(302, mainDashboarRoute)
+    }
+    return {
+        academicPeriodsData: await LoadAcademicPeriodsWithComboMessages(),
+    }
+};
--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/academic-publications/+page.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/academic-publications/+page.svelte
@@ -1,1 +1,37 @@
-hi
+<script lang="ts">
+	import AcademicPeriodCombo from '$lib/components/combobox/academicPeriodCombo.svelte';
+
+	import Icon from 'lucide-svelte/icons/book-marked';
+	import type { PageServerData } from './$types';
+
+	export let data: PageServerData;
+
+	const academicPeriodsData = data.academicPeriodsData;
+	let selectedAcademicPeriod: number = academicPeriodsData.periods.at(0)!.ID;
+
+	function fetchOnAcademicPeriodChange() {
+		/* filterTeachersListsByAcademicPeriodRequest.academic_period_id = selectedAcademicPeriod;
+		filterTeachersListsByAcademicPeriodPromise = fetchFilterTeachersLists(
+			filterTeachersListsByAcademicPeriodRequest
+		); */
+	}
+</script>
+
+<svelte:head>
+	<title>Publicaciones académicas</title>
+</svelte:head>
+
+<div class="mx-auto flex w-full place-content-center justify-between px-8">
+	<div class="flex items-center gap-1">
+		<Icon class="h-8 w-8" />
+		<h2 class="text-2xl font-bold">Publicaciones académicas</h2>
+	</div>
+</div>
+
+<div class="mx-auto flex w-full place-content-center justify-between px-8">
+	<AcademicPeriodCombo
+		messages={academicPeriodsData.messages}
+		bind:selectedValue={selectedAcademicPeriod}
+		on:message={fetchOnAcademicPeriodChange}
+	></AcademicPeriodCombo>
+</div>
