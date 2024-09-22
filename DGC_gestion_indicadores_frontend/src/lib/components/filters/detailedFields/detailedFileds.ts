e2858504f09155684d75d6248d466edf1a52dd41
Erick Carrasco <erickype@hotmail.com>
2024-09-22 12:20:24 -0500

    Add initial filter functions for detailed field

---
+++ DGC_gestion_indicadores_frontend/src/lib/components/filters/detailedFields/detailedFileds.ts
@@ -0,0 +1,55 @@
+import type { FilterDetailedFieldRequest, FilterDetailedFieldResponse } from "$lib/api/model/api/knowledgeFields/detailedFields";
+import type { PopoverFilterDataMap } from "$lib/components/table/types";
+
+import type { CommonError } from "$lib/api/model/errors";
+import { goto } from "$app/navigation";
+
+export function newFilterDetailedFieldsRequest(page_size: number, page: number): FilterDetailedFieldRequest {
+    let filterDetailedFieldRequest: FilterDetailedFieldRequest = {
+        wide_field: '',
+        specific_field: '',
+        detailed_field: '',
+        page_size: page_size,
+        page: page
+    };
+    return filterDetailedFieldRequest
+}
+
+export function newPopoverFilterDataMap(): PopoverFilterDataMap {
+    let popoverFilterDataMap: PopoverFilterDataMap = new Map();
+    popoverFilterDataMap.set('wide_field', { label: 'Campo amplio', value: '' });
+    popoverFilterDataMap.set('specific_field', { label: 'Campo específico', value: '' });
+    popoverFilterDataMap.set('detailed_field', { label: 'Campo detallado', value: '' });
+    return popoverFilterDataMap
+}
+
+export function generateInitialFilterValue(filterDetailedFieldRequest: FilterDetailedFieldRequest): string | undefined {
+    let values = [
+        filterDetailedFieldRequest.wide_field,
+        filterDetailedFieldRequest.specific_field,
+        filterDetailedFieldRequest.detailed_field,
+    ];
+
+    let uniqueValues = [...new Set(values.filter((value) => value !== ''))];
+
+    let initialFilterValue: string | undefined =
+        uniqueValues.length === 1 ? uniqueValues[0] : uniqueValues.join(' ');
+
+    return initialFilterValue
+}
+
+export async function fetchFilterDetailedFields(filterDetailedFieldRequest: FilterDetailedFieldRequest): Promise<FilterDetailedFieldResponse> {
+    const url = `/api/knowledgeFields/detailedFields/filter`;
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(filterDetailedFieldRequest)
+    });
+    if (!response.ok) {
+        const errorData = (await response.json()) as CommonError;
+        if (response.status === 401) {
+            throw goto('/');
+        }
+        throw errorData;
+    }
+    return response.json();
+}
