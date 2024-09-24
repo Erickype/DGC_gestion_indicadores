25cba46ed9aa6c7aa74a3e6edf826534050e4337
Erick Carrasco <erickype@hotmail.com>
2024-09-24 17:36:04 -0500

    Add GetAcademicProductionListAuthorPreviousCareers to load if exists author previous information on publication creation

--- DGC_gestion_indicadores_backend/model/indicatorsInformation/academicProduction/academicProductionListsAuthor.go
+++ DGC_gestion_indicadores_backend/model/indicatorsInformation/academicProduction/academicProductionListsAuthor.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"github.com/Erickype/DGC_gestion_indicadores_backend/database"
 	"github.com/Erickype/DGC_gestion_indicadores_backend/model"
 	academicProduction "github.com/Erickype/DGC_gestion_indicadores_backend/model/academicProduction/author"
 	career "github.com/Erickype/DGC_gestion_indicadores_backend/model/career"
@@ -21,4 +22,28 @@ type AcademicProductionListsAuthor struct {
 
 func (apl *AcademicProductionListsAuthor) TableName() string {
 	return model.IndicatorsInformationSchema + ".academic_production_lists_authors"
+}
+
+func GetAcademicProductionListAuthorPreviousCareers(authorID int, previousCareers *[]career.Career) (err error) {
+	var academicPeriodsIDs []uint
+	database.DB.Table("academic_periods ap").
+		Order("ap.start_date desc").
+		Select("ap.id").
+		Limit(2).Scan(&academicPeriodsIDs)
+
+	var authorPreviousCareers []uint
+	database.DB.Table("indicators_information.academic_production_lists_authors apla").
+		Distinct("apla.career_id").
+		Joins("join indicators_information.academic_production_lists apl on apla.academic_production_list_id = apl.id").
+		Where("apl.academic_period_id in (?)", academicPeriodsIDs).
+		Where("apla.author_id = ?", authorID).Scan(&authorPreviousCareers)
+
+	err = database.DB.Table("careers c").
+		Where("c.id in (?)", authorPreviousCareers).
+		Scan(&previousCareers).Error
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
