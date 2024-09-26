0ae868e6d18df5729be1b3ebc6e92bae5f7bd290
Erick Carrasco <erickype@hotmail.com>
2024-09-26 09:34:03 -0500

    Add fetch method to load table data

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/academic-publications/[academicProductionID]/+page.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/academic-publications/[academicProductionID]/+page.svelte
@@ -1,17 +1,38 @@
 <script lang="ts">
 	import type { PageServerData } from './$types';
 	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
 
 	import { Button } from '$lib/components/ui/button/index';
 
 	import MoveLeft from 'lucide-svelte/icons/move-left';
 
+	import type { AcademicProductionListsAuthorsCareersJoined } from '$lib/api/model/api/indicatorsInformation/academicProductionListsAuthor';
 	import AddModal from '$lib/components/modal/AddModal.svelte';
+	import type { CommonError } from '$lib/api/model/errors';
+	import Alert from '$lib/components/alert/alert.svelte';
+	import AuthorsCareersTable from './Table.svelte';
 
 	export let data: PageServerData;
+	let authorsCareersPromise: Promise<AcademicProductionListsAuthorsCareersJoined[]> =
+		fetchGetAcademicProductionListsAuthorsCareersJoined();
 
-	function fetchOnSuccess(event: CustomEvent) {
+	async function fetchGetAcademicProductionListsAuthorsCareersJoined() {
+		const url = `/api/indicatorsInformation/academicProductionListsAuthors/JoinedByAcademicProductionListID/${data.academicProductionID}`;
+		const response = await fetch(url, {
+			method: 'GET'
+		});
+		if (!response.ok) {
+			const errorData = (await response.json()) as CommonError;
+			if (response.status === 401) {
+				throw goto('/');
+			}
+			throw errorData;
+		}
+		return (authorsCareersPromise = response.json());
 	}
+
+	function fetchOnSuccess(event: CustomEvent) {}
 
 	function returnToAcademicProduction() {
 		if (browser) {
@@ -37,4 +58,19 @@
 		formData={addDegreeAndTeachersListsDegreeForm}
 		on:created={fetchOnSuccess}
 	/> -->
+</div>
+
+<div class="mx-auto flex w-full place-content-center px-8">
+	{#await authorsCareersPromise}
+		cargando...
+	{:then authorsCareers}
+		{#if authorsCareers.length > 0}
+			<AuthorsCareersTable {authorsCareers} on:updated={fetchOnSuccess} on:deleted={fetchOnSuccess}
+			></AuthorsCareersTable>
+		{:else}
+			<Alert title="Sin registros" description={'Ups, no hay autores registrados.'} />
+		{/if}
+	{:catch error}
+		<Alert variant="destructive" description={error.message} />
+	{/await}
 </div>
