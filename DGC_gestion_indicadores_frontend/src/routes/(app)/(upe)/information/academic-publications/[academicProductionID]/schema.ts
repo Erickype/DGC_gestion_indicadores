ead6e98cf03c4eb42643f099a2d9ba27ad44d53e
Erick Carrasco <erickype@hotmail.com>
2024-09-27 21:26:03 -0500

    Add career schema auxiliar field

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/academic-publications/[academicProductionID]/schema.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/academic-publications/[academicProductionID]/schema.ts
@@ -1,15 +1,15 @@
 import { z } from "zod";
 
 export const addAcademicProductionListsAuthorSchema = z.object({
-    academicProductionID: z.number({
-        required_error: "DOI requerido"
-    }).gt(0),
+    academicProductionID: z.number().gt(0),
 
     authorID: z.number({
         required_error: "Autor requerido"
     }).gt(0, {
         message: "Seleccione un autor válido"
     }),
+
+    career: z.number(),
 
     careers: z.array(z.number()).refine((value) => value.some((item) => item), {
         message: "Seleccionar al menos una carrera"
