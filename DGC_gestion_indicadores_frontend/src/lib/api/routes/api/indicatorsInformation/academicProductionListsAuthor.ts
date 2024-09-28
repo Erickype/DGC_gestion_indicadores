15fb40b40ecfe18b7db9a64475db30f10f52df6b
Erick Carrasco <erickype@hotmail.com>
2024-09-28 14:03:51 -0500

    Add GetAcademicProductionListAuthorPreviousCareers api client

--- DGC_gestion_indicadores_frontend/src/lib/api/routes/api/indicatorsInformation/academicProductionListsAuthor.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/routes/api/indicatorsInformation/academicProductionListsAuthor.ts
@@ -1,3 +1,4 @@
 import { indicatorsInformationBaseRoute } from "./base";
 
 export const getAcademicProductionListsAuthorsJoinedByAcademicProductionListID = indicatorsInformationBaseRoute + "/academicProductionListsAuthors/JoinedByAcademicProductionListID/"
+export const getAcademicProductionListsAuthorPreviousCareersByAuthorID = indicatorsInformationBaseRoute + "/academicProductionListsAuthor/previousCareers/"
