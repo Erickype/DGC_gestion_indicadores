63f6a323fe09419a8c335649ba38ff9292a7488a
Erick Carrasco <erickype@hotmail.com>
2024-09-30 10:38:07 -0500

    Add patchAcademicProductionList server action

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/academic-publications/+page.server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/academic-publications/+page.server.ts
@@ -8,7 +8,7 @@ import { zod } from "sveltekit-superforms/adapters";
 
 import { LoadGetImpactCoefficientssWithComboMessages } from "$lib/api/controller/api/academicProduction/impactCoefficients/impactCoefficient";
 import { LoadGetScienceMagazinesWithComboMessages } from "$lib/api/controller/api/academicProduction/scienceMagazines/scienceMagazine";
-import { PostAcademicProductionList } from "$lib/api/controller/api/indicatorsInformation/academicProductionLists";
+import { PatchAcademicProductionList, PostAcademicProductionList } from "$lib/api/controller/api/indicatorsInformation/academicProductionLists";
 import type { AcademicProductionList } from "$lib/api/model/api/indicatorsInformation/academicProductionLists";
 import { generateFormMessageFromHttpResponse, generateFormMessageFromInvalidForm } from "$lib/utils";
 import { LoadAcademicPeriodsWithComboMessages } from "$lib/api/controller/view/academicPeriod";
@@ -57,4 +57,29 @@ export const actions: Actions = {
 
         return generateFormMessageFromHttpResponse(form, response)
     },
+
+    patchAcademicProductionList: async (event) => {
+        const form = await superValidate(event, zod(updateAcademicProductionSchema))
+        const token = event.cookies.get("AuthorizationToken")
+
+        if (!form.valid) {
+            return generateFormMessageFromInvalidForm(form)
+        }
+
+        const data = form.data        
+        const academicProductionList: AcademicProductionList = {
+            ID: data.ID,
+            DOI: data.doi,
+            publication_date: new Date(data.publication_date).toISOString(),
+            publication_name: data.publication_name,
+            academic_period_id: data.academicPeriod,
+            detailed_field_id: data.detailed_field_id,
+            science_magazine_id: data.science_magazine_id,
+            impact_coefficient_id: data.impact_coefficient_id,
+            intercultural_component: data.intercultural_component
+        }
+        const response = await PatchAcademicProductionList(token!, academicProductionList)
+
+        return generateFormMessageFromHttpResponse(form, response)
+    }
 };
