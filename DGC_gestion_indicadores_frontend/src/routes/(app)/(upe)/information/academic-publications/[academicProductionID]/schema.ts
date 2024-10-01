68d3ff28d9550fc56dc0a7580e52145846f91c2a
Erick Carrasco <erickype@hotmail.com>
2024-09-30 22:45:47 -0500

    Add UpdateAcademicProductionListsAuthorCareersSchema

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/academic-publications/[academicProductionID]/schema.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/academic-publications/[academicProductionID]/schema.ts
@@ -16,4 +16,21 @@ export const addAcademicProductionListsAuthorSchema = z.object({
     }),
 });
 
+export const updateAcademicProductionListsAuthorCareersSchema = z.object({
+    academicProductionID: z.number().gt(0),
+
+    authorID: z.number({
+        required_error: "Autor requerido"
+    }).gt(0, {
+        message: "Seleccione un autor válido"
+    }),
+
+    career: z.number(),
+
+    careers: z.array(z.number()).refine((value) => value.some((item) => item), {
+        message: "Seleccionar al menos una carrera"
+    }),
+});
+
 export type AddAcademicProductionListsAuthorSchema = typeof addAcademicProductionListsAuthorSchema;
+export type UpdateAcademicProductionListsAuthorCareersSchema = typeof updateAcademicProductionListsAuthorCareersSchema;
