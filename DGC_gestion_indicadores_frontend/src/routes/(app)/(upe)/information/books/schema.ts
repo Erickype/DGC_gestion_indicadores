1cf769b0cb941a2f01548c917d2bd09328289576
Erick Carrasco <erickype@hotmail.com>
2024-10-01 10:18:51 -0500

    Add AddBookOrChaptersProductionListSchema

---
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/books/schema.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+
+export const addBookOrChaptersProductionListSchema = z.object({
+    doi: z.string({
+        required_error: "DOI requerido"
+    }).url({
+        message: "URL inválida"
+    }).max(1000),
+
+    is_chapter: z.boolean({
+        required_error: "Tipo de publicación requerido"
+    }).default(false),
+
+    book_title: z.string({
+        required_error: "Nombre libro requerido"
+    }).max(1000).min(5, {
+        message: "Ingrese un nombre válido"
+    }),
+
+    chapter_title: z.string({
+        required_error: "Nombre capítulo requerido"
+    }).max(1000).min(5, {
+        message: "Ingrese un nombre válido"
+    }),
+
+    publication_date: z.string({
+        required_error: "Fecha publicación requerida"
+    }).refine((v) => v, { message: "Fecha publicación requerida" }),
+
+    peer_reviewed: z.boolean({
+        required_error: "Tipo de publicación requerido"
+    }).default(false),
+
+    academic_period_id: z.number().gt(0),
+
+    detailed_field_id: z.number({
+        required_error: "Campo detallado requerido"
+    }).gt(0, {
+        message: "Ingrese un campo detallado válido"
+    }),
+});
+
+export type AddBookOrChaptersProductionListSchema = typeof addBookOrChaptersProductionListSchema;
