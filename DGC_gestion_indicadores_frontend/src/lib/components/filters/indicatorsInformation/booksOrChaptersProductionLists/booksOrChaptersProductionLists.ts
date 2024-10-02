25b7322656fe71a1d5aab449a3bd4d827e78ae00
Erick Carrasco <erickype@hotmail.com>
2024-10-02 10:30:16 -0500

    Add initial filter functions for books or chapters production filter

---
+++ DGC_gestion_indicadores_frontend/src/lib/components/filters/indicatorsInformation/booksOrChaptersProductionLists/booksOrChaptersProductionLists.ts
@@ -0,0 +1,47 @@
+import type { FilterBooksOrChaptersProductionListsByAcademicPeriodRequest, FilterBooksOrChaptersProductionListsByAcademicPeriodResponse } from "$lib/api/model/api/indicatorsInformation/booksOrChaptersProductionLists";
+import type { PopoverFilterDataMap } from "$lib/components/table/types";
+import type { CommonError } from "$lib/api/model/errors";
+
+import { goto } from "$app/navigation";
+import { toast } from "svelte-sonner";
+
+export function newFilterAcademicProductionListsByAcademiPeriodRequest(page_size: number, page: number, academicPeriodID: number): FilterBooksOrChaptersProductionListsByAcademicPeriodRequest {
+    let filterAcademicProductionRequest: FilterBooksOrChaptersProductionListsByAcademicPeriodRequest = {
+        DOI: '',
+        title: '',
+        publication_date: '',
+        detailed_field: '',
+        academic_period_id: academicPeriodID,
+        page_size: page_size,
+        page: page
+    };
+    return filterAcademicProductionRequest
+}
+
+export async function fetchFilterAcademicProductionLists(filterAcademicProductionRequest: FilterBooksOrChaptersProductionListsByAcademicPeriodRequest):
+    Promise<FilterBooksOrChaptersProductionListsByAcademicPeriodResponse> {
+    const url = `/api/indicatorsInformation/booksOrChaptersProductionLists/filter`;
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(filterAcademicProductionRequest)
+    });
+    if (!response.ok) {
+        const errorData = (await response.json()) as CommonError;
+        if (response.status === 401) {
+            throw goto('/');
+        }
+        throw errorData;
+    }
+    return response.json();
+}
+
+export function newPopoverFilterDataMap(): PopoverFilterDataMap {
+    let popoverFilterDataMap: PopoverFilterDataMap = new Map();
+    popoverFilterDataMap.set('DOI', { label: 'DOI', value: '' });
+    /* popoverFilterDataMap.set('is_chapter', { label: 'Fecha', value: '' }); */
+    popoverFilterDataMap.set('title', { label: 'Título', value: '' });
+    popoverFilterDataMap.set('publication_date', { label: 'Fecha', value: '' });
+    /* popoverFilterDataMap.set('peer_reviewed', { label: 'Revisado pares', value: '' }); */
+    popoverFilterDataMap.set('detailed_field', { label: 'Campo detallado', value: '' });
+    return popoverFilterDataMap
+}
