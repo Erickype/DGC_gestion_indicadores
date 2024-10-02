44e675b18c7300f12b3d1691efb5e7d7571bc46c
Erick Carrasco <erickype@hotmail.com>
2024-10-02 10:14:31 -0500

    Add PostFilterBooksOrChaptersProductionListsByAcademicPeriodID frontend api

---
+++ DGC_gestion_indicadores_frontend/src/routes/api/indicatorsInformation/booksOrChaptersProductionLists/filter/+server.ts
@@ -0,0 +1,13 @@
+import { PostFilterBooksOrChaptersProductionListsByAcademicPeriodID } from "$lib/api/controller/api/indicatorsInformation/booksOrChaptersProductionLists";
+import { generateErrorFromCommonError, type CommonError } from "$lib/api/model/errors";
+import { json, type RequestHandler } from "@sveltejs/kit";
+
+export const POST: RequestHandler = async ({ cookies, request }) => {
+    const token = cookies.get("AuthorizationToken")
+    const response = await PostFilterBooksOrChaptersProductionListsByAcademicPeriodID(token!, await request.json())
+
+    if ((response as CommonError).status_code) {
+        return generateErrorFromCommonError(response as CommonError)
+    }
+    return json(response);
+};
