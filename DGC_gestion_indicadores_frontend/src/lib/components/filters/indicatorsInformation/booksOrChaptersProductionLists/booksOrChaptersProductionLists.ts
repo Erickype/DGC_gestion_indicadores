a1f6cc91d63aec11acff54d7bb300b992f87d395
Erick Carrasco <erickype@hotmail.com>
2024-10-02 18:26:01 -0500

    Add filter functions of booksOrChaptersListsProduction

--- DGC_gestion_indicadores_frontend/src/lib/components/filters/indicatorsInformation/booksOrChaptersProductionLists/booksOrChaptersProductionLists.ts
+++ DGC_gestion_indicadores_frontend/src/lib/components/filters/indicatorsInformation/booksOrChaptersProductionLists/booksOrChaptersProductionLists.ts
@@ -44,3 +44,79 @@ export function newPopoverFilterDataMap(): PopoverFilterDataMap {
     popoverFilterDataMap.set('detailed_field', { label: 'Campo detallado', value: '' });
     return popoverFilterDataMap
 }
+
+export function generateInitialFilterValue(request: FilterBooksOrChaptersProductionListsByAcademicPeriodRequest): string | undefined {
+    let values = [
+        request.DOI,
+        request.title,
+        request.publication_date,
+        request.detailed_field
+    ];
+    let uniqueValues = [...new Set(values.filter((value) => value !== ''))];
+
+    let initialFilterValue: string | undefined =
+        uniqueValues.length === 1 ? uniqueValues[0] : uniqueValues.join(' ');
+
+    return initialFilterValue
+}
+
+export async function fetchOnFilterChanged(filter: string, filterBooksOrChaptersProductionListsByAcademicPeriodRequest: FilterBooksOrChaptersProductionListsByAcademicPeriodRequest, popoverFilterDataMap: PopoverFilterDataMap): Promise<FilterBooksOrChaptersProductionListsByAcademicPeriodResponse> {
+    popoverFilterDataMap.forEach((_, key) => {
+        (filterBooksOrChaptersProductionListsByAcademicPeriodRequest as any)[key] = filter;
+    });
+
+    return fetchFilterBooksOrChaptersProductionLists(filterBooksOrChaptersProductionListsByAcademicPeriodRequest).then(
+        (response: FilterBooksOrChaptersProductionListsByAcademicPeriodResponse) => {
+            if (response.count === 0) {
+                toast.warning(`No hay datos para el filtro: ${filter}`);
+                popoverFilterDataMap.forEach((_, key) => {
+                    (filterBooksOrChaptersProductionListsByAcademicPeriodRequest as any)[key] = '';
+                });
+                return fetchFilterBooksOrChaptersProductionLists(filterBooksOrChaptersProductionListsByAcademicPeriodRequest);
+            }
+            return response;
+        }
+    );
+}
+
+export async function fetchOnDetailedFilter(
+    filterBooksOrChaptersProductionListsByAcademicPeriodRequest: FilterBooksOrChaptersProductionListsByAcademicPeriodRequest,
+    popoverFilterDataMap: PopoverFilterDataMap
+): Promise<{ request: FilterBooksOrChaptersProductionListsByAcademicPeriodRequest, response: FilterBooksOrChaptersProductionListsByAcademicPeriodResponse }> {
+
+    let request: FilterBooksOrChaptersProductionListsByAcademicPeriodRequest = {
+        academic_period_id: filterBooksOrChaptersProductionListsByAcademicPeriodRequest.academic_period_id,
+        page: filterBooksOrChaptersProductionListsByAcademicPeriodRequest.page,
+        page_size: filterBooksOrChaptersProductionListsByAcademicPeriodRequest.page_size
+    };
+
+    popoverFilterDataMap.forEach((item, key) => {
+        if (item.value !== '') {
+            (request as any)[key] = item.value;
+        }
+    });
+
+    filterBooksOrChaptersProductionListsByAcademicPeriodRequest = request;
+
+    const response = await fetchFilterBooksOrChaptersProductionLists(filterBooksOrChaptersProductionListsByAcademicPeriodRequest);
+
+    if (response.count === 0) {
+        let message = 'No hay datos para el filtro\n';
+        popoverFilterDataMap.forEach((item, _) => {
+            if (item.value !== '') {
+                message += `${item.label}: ${item.value}; `;
+            }
+        });
+        message = message.slice(0, message.length - 2);
+        toast.warning(message);
+
+        popoverFilterDataMap.forEach((_, key) => {
+            (filterBooksOrChaptersProductionListsByAcademicPeriodRequest as any)[key] = '';
+        });
+
+        const newResponse = await fetchFilterBooksOrChaptersProductionLists(filterBooksOrChaptersProductionListsByAcademicPeriodRequest);
+        return { request: filterBooksOrChaptersProductionListsByAcademicPeriodRequest, response: newResponse };
+    }
+
+    return { request: filterBooksOrChaptersProductionListsByAcademicPeriodRequest, response };
+}
