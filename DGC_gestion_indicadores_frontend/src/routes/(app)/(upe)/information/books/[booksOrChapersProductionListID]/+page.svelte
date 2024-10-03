acfe25a3efa85cecbfa324baacb0ab7b2829c045
Erick Carrasco <erickype@hotmail.com>
2024-10-03 09:30:00 -0500

    Add initial page book or chapters authors detail

---
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/books/[booksOrChapersProductionListID]/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import type { PageServerData } from './$types';
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+
+	import { Button } from '$lib/components/ui/button/index';
+
+	import MoveLeft from 'lucide-svelte/icons/move-left';
+
+	import type { Message } from '$lib/components/combobox/combobox';
+	import AddModal from '$lib/components/modal/AddModal.svelte';
+
+	import AddForm from './AddForm.svelte';
+
+	export let data: PageServerData;
+
+	const comboMessages: Message[][] = [data.careersData.messages];
+
+	let addBooksOrChaptersProductionListsAuthorCareersForm =
+		data.addBooksOrChaptersProductionListsAuthorCareersForm;
+
+	function fetchOnSuccess(event: CustomEvent) {
+		const detail: { status: boolean } = event.detail;
+		if (detail.status) {
+			/* fetchGetAcademicProductionListsAuthorsCareersJoined(); */
+		}
+	}
+
+	function returnToBooksOrChaptersProductionLists() {
+		if (browser) {
+			window.history.back();
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>Libros o capítulos autores</title>
+</svelte:head>
+
+<div class="mx-auto flex w-full place-content-center justify-between px-8">
+	<div class="flex items-center gap-1">
+		<Button variant="ghost" on:click={returnToBooksOrChaptersProductionLists}>
+			<MoveLeft class="h-4 w-4" />
+		</Button>
+		<h2 class="text-2xl font-bold">Libros o capítulos Autores</h2>
+	</div>
+	<AddModal
+		formComponent={AddForm}
+		modalTitle="Añadir autor"
+		formData={addBooksOrChaptersProductionListsAuthorCareersForm}
+		{comboMessages}
+		on:created={fetchOnSuccess}
+	/>
+</div>
