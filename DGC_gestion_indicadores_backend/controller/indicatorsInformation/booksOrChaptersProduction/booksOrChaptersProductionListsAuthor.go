5c88e5f37b08484d053c07d31f57bdf89fae9991
Erick Carrasco <erickype@hotmail.com>
2024-10-03 11:24:20 -0500

    Cambio del modelo por booksOrChapterProduction

--- DGC_gestion_indicadores_backend/controller/indicatorsInformation/booksOrChaptersProduction/booksOrChaptersProductionListsAuthor.go
+++ DGC_gestion_indicadores_backend/controller/indicatorsInformation/booksOrChaptersProduction/booksOrChaptersProductionListsAuthor.go
@@ -2,22 +2,22 @@ package controller
 
 import (
 	errors "github.com/Erickype/DGC_gestion_indicadores_backend/model"
-	model "github.com/Erickype/DGC_gestion_indicadores_backend/model/indicatorsInformation/academicProduction"
+	model "github.com/Erickype/DGC_gestion_indicadores_backend/model/indicatorsInformation/booksOrChaptersProduction"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func PostBooksOrChaptersProductionListsAuthorCareers(c *gin.Context) {
-	var postAcademicProductionListsAuthorCareersRequest model.PostAcademicProductionListsAuthorCareersRequest
-	err := c.BindJSON(&postAcademicProductionListsAuthorCareersRequest)
+	var postBooksOrChaptersProductionListsAuthorCareersRequest model.PostBooksOrChaptersProductionListsAuthorCareersRequest
+	err := c.BindJSON(&postBooksOrChaptersProductionListsAuthorCareersRequest)
 	if err != nil {
 		errors.BadRequestResponse(c, err)
 		return
 	}
-	err = model.PostAcademicProductionListsAuthorCareers(&postAcademicProductionListsAuthorCareersRequest)
+	err = model.PostBooksOrChaptersProductionListsAuthorCareers(&postBooksOrChaptersProductionListsAuthorCareersRequest)
 	if err != nil {
 		errors.InternalServerErrorResponse(c, "Error añadiendo autor y sus carreras", err)
 		return
 	}
-	c.JSON(http.StatusCreated, postAcademicProductionListsAuthorCareersRequest)
+	c.JSON(http.StatusCreated, postBooksOrChaptersProductionListsAuthorCareersRequest)
 }
