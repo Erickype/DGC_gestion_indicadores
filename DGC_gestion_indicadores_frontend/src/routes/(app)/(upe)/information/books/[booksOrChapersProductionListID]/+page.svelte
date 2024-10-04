3f953d74501798fcacba5f3a66c35a7d85e492d1
Erick Carrasco <erickype@hotmail.com>
2024-10-03 21:27:53 -0500

    Add fetchGetBooksOrChaptersProductionListsAuthorsJoinedByBooksOrChaptersProductionListID and table

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/books/[booksOrChapersProductionListID]/+page.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/books/[booksOrChapersProductionListID]/+page.svelte
@@ -7,9 +7,14 @@
 
 	import MoveLeft from 'lucide-svelte/icons/move-left';
 
+	import TableSkeleton from '$lib/components/skeleton/table.svelte';
 	import type { Message } from '$lib/components/combobox/combobox';
 	import AddModal from '$lib/components/modal/AddModal.svelte';
+	import type { CommonError } from '$lib/api/model/errors';
+	import Alert from '$lib/components/alert/alert.svelte';
 
+	import type { BooksOrChaptersProductionListsAuthorsCareersJoined } from '$lib/api/model/api/indicatorsInformation/booksOrChaptersProductionListsAuthor';
+	import AuthorsCareersTable from './Table.svelte';
 	import AddForm from './AddForm.svelte';
 
 	export let data: PageServerData;
@@ -18,14 +23,34 @@
 
 	let addBooksOrChaptersProductionListsAuthorCareersForm =
 		data.addBooksOrChaptersProductionListsAuthorCareersForm;
+	let updateBooksOrChaptersProductionListsAuthorCareersForm =
+		data.updateBooksOrChaptersProductionListsAuthorCareersForm;
+
+	let authorsCareersPromise: Promise<BooksOrChaptersProductionListsAuthorsCareersJoined[]> =
+		fetchGetBooksOrChaptersProductionListsAuthorsJoinedByBooksOrChaptersProductionListID();
 
 	addBooksOrChaptersProductionListsAuthorCareersForm.data.booksOrChaptersProductionListID =
 		data.booksOrChapersProductionListID;
 
+	async function fetchGetBooksOrChaptersProductionListsAuthorsJoinedByBooksOrChaptersProductionListID() {        
+        const url = `/api/indicatorsInformation/booksOrChaptersProductionListsAuthors/JoinedByBooksOrChaptersProductionListID/${data.booksOrChapersProductionListID}`;
+		const response = await fetch(url, {
+			method: 'GET'
+		});
+		if (!response.ok) {
+			const errorData = (await response.json()) as CommonError;
+			if (response.status === 401) {
+				throw goto('/');
+			}
+			throw errorData;
+		}
+		return (authorsCareersPromise = response.json());
+	}
+
 	function fetchOnSuccess(event: CustomEvent) {
 		const detail: { status: boolean } = event.detail;
 		if (detail.status) {
-			/* fetchGetAcademicProductionListsAuthorsCareersJoined(); */
+			fetchGetBooksOrChaptersProductionListsAuthorsJoinedByBooksOrChaptersProductionListID();
 		}
 	}
 
@@ -54,4 +79,24 @@
 		{comboMessages}
 		on:created={fetchOnSuccess}
 	/>
+</div>
+
+<div class="mx-auto flex w-full flex-col place-content-center px-8">
+	{#await authorsCareersPromise}
+		<TableSkeleton tableHeightClass="h-[65vh]" />
+	{:then authorsCareers}
+		{#if authorsCareers.length > 0}
+			<AuthorsCareersTable
+				{authorsCareers}
+				{comboMessages}
+				formData={updateBooksOrChaptersProductionListsAuthorCareersForm}
+				on:updated={fetchOnSuccess}
+				on:deleted={fetchOnSuccess}
+			></AuthorsCareersTable>
+		{:else}
+			<Alert title="Sin registros" description={'Ups, no hay autores registrados.'} />
+		{/if}
+	{:catch error}
+		<Alert variant="destructive" description={error.message} />
+	{/await}
 </div>
