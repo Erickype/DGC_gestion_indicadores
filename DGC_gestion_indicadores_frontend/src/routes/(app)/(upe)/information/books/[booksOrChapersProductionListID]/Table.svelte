1b3a1db4b4989cb1a4538ddede624ab68276aefd
Erick Carrasco <erickype@hotmail.com>
2024-10-03 21:28:26 -0500

    Add books or chapter production authors initial table

---
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/books/[booksOrChapersProductionListID]/Table.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+	import { addPagination, addSortBy, addTableFilter } from 'svelte-headless-table/plugins';
+	import { createTable, createRender } from 'svelte-headless-table';
+	import { createEventDispatcher } from 'svelte';
+	import { readable } from 'svelte/store';
+
+	import type { UpdateBooksOrChaptersProductionListsAuthorCareersSchema } from './schema';
+	import DataTableActions from '$lib/components/table/tableActions.svelte';
+	import UpdateModal from '$lib/components/modal/UpdateModal.svelte';
+	import Table from '$lib/components/table/table.svelte';
+	/* 	import UpdateForm from './UpdateForm.svelte';
+	 */
+	import type { BooksOrChaptersProductionListsAuthorsCareersJoined } from '$lib/api/model/api/indicatorsInformation/booksOrChaptersProductionListsAuthor';
+	import type { Infer, SuperValidated } from 'sveltekit-superforms';
+	import type { Message } from '$lib/components/combobox/combobox';
+
+	export let comboMessages: Message[][] | undefined = undefined;
+	export let authorsCareers: BooksOrChaptersProductionListsAuthorsCareersJoined[];
+	export let formData: SuperValidated<
+		Infer<UpdateBooksOrChaptersProductionListsAuthorCareersSchema>,
+		App.Superforms.Message
+	>;
+	let authorCareer: BooksOrChaptersProductionListsAuthorsCareersJoined;
+
+	const filterFields = ['author', 'careers'];
+
+	const table = createTable(readable(authorsCareers), {
+		page: addPagination({
+			initialPageSize: 30
+		}),
+		sort: addSortBy(),
+		filter: addTableFilter({
+			fn: ({ filterValue, value }) => value.toLowerCase().includes(filterValue.toLowerCase())
+		})
+	});
+
+	const columns = table.createColumns([
+		table.column({
+			accessor: 'author',
+			header: 'Autor'
+		}),
+		table.column({
+			accessor: 'careers',
+			header: 'Carreras',
+			cell: ({ value }) => {
+				let label = '';
+				value.map((career) => {
+					label += career.name + ', ';
+				});
+				label = label.slice(0, label.length - 2);
+				return label;
+			}
+		}),
+		table.column({
+			accessor: ({ author_id }) => author_id,
+			header: '',
+			cell: ({ value }) => {
+				const actions = createRender(DataTableActions, { id: value.toString() });
+				actions.on('delete-confirmation', handleDeleteConfirmation);
+				actions.on('update-action', handleUpdateAction);
+				return actions;
+			}
+		})
+	]);
+
+	const dispatch = createEventDispatcher();
+	async function handleDeleteConfirmation(event: any) {}
+
+	let updateFormOpen = false;
+	function handleUpdateAction(event: any) {
+		const detail: { status: boolean; id: string } = event.detail;
+		if (detail.status) {
+			authorCareer = authorsCareers.find(
+				(authorCareer) => authorCareer.author_id.toString() === detail.id
+			)!;
+			updateFormOpen = true;
+		} else {
+			updateFormOpen = false;
+		}
+	}
+
+	async function deleteCareer(id: string) {}
+
+	function handleUpdated(event: any) {
+		const detail: { status: boolean } = event.detail;
+		if (detail.status) {
+			dispatch('updated', {
+				status: true
+			});
+		}
+	}
+</script>
+
+<!-- 
+<UpdateModal
+	modalTitle="Actualizar información de autor"
+	{formData}
+	{comboMessages}
+	formComponent={UpdateForm}
+	bind:updateEntity={authorCareer}
+	bind:open={updateFormOpen}
+	on:updated={handleUpdated}
+/> -->
+
+<div class="w-full">
+	<Table {table} {columns} {filterFields} itemCount={authorsCareers.length} />
+</div>
