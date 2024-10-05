a31f2e8a2e11ab90ca82c6801542b375c80d9a26
Erick Carrasco <erickype@hotmail.com>
2024-10-05 16:37:19 -0500

    Add updateBooksOrChaptersProductionListsAuthorCareers server action

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/books/[booksOrChapersProductionListID]/+page.server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/books/[booksOrChapersProductionListID]/+page.server.ts
@@ -6,8 +6,8 @@ import { error, redirect, type Actions } from "@sveltejs/kit";
 import { mainDashboarRoute } from "$lib/api/util/paths";
 import type { PageServerLoad } from "./$types";
 
-import type { PostBooksOrChaptersProductionListsAuthorCareersRequest } from "$lib/api/model/api/indicatorsInformation/booksOrChaptersProductionListsAuthor";
-import { PostBooksOrChaptersProductionListsAuthorCareers } from "$lib/api/controller/api/indicatorsInformation/booksOrChaptersProductionListsAuthor";
+import type { PostBooksOrChaptersProductionListsAuthorCareersRequest, UpdateBooksOrChaptersProductionListsAuthorCareersRequest } from "$lib/api/model/api/indicatorsInformation/booksOrChaptersProductionListsAuthor";
+import { PostBooksOrChaptersProductionListsAuthorCareers, UpdateBooksOrChaptersProductionListsAuthorCareers } from "$lib/api/controller/api/indicatorsInformation/booksOrChaptersProductionListsAuthor";
 import { generateFormMessageFromHttpResponse, generateFormMessageFromInvalidForm } from "$lib/utils";
 import { LoadCareersWithComboMessages } from "$lib/api/controller/api/career";
 
@@ -52,6 +52,26 @@ export const actions: Actions = {
         }
 
         const response = await PostBooksOrChaptersProductionListsAuthorCareers(token!, academicProductionListsAuthorCareersRequest)
+
+        return generateFormMessageFromHttpResponse(form, response)
+    },
+
+    updateBooksOrChaptersProductionListsAuthorCareers: async (event) => {
+        const form = await superValidate(event, zod(updateBooksOrChaptersProductionListsAuthorCareersSchema))
+
+        if (!form.valid) {
+            return generateFormMessageFromInvalidForm(form)
+        }
+
+        const token = event.cookies.get("AuthorizationToken")
+        const data = form.data
+        const updateBooksOrChaptersProductionListsAuthorCareersRequest: UpdateBooksOrChaptersProductionListsAuthorCareersRequest = {
+            books_or_chapters_production_list_id: data.booksOrChaptersProductionListID,
+            author_id: data.authorID,
+            careers: data.careers
+        }
+
+        const response = await UpdateBooksOrChaptersProductionListsAuthorCareers(token!, updateBooksOrChaptersProductionListsAuthorCareersRequest)
 
         return generateFormMessageFromHttpResponse(form, response)
     },
