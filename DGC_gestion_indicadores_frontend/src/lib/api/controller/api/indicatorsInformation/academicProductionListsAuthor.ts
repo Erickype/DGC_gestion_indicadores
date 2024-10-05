fe3ab36ea21cdb5b45674abdb55148d22bc29b1f
Erick Carrasco <erickype@hotmail.com>
2024-10-05 11:10:23 -0500

    Remove GetAcademicProductionListAuthorPreviousCareers api client

--- DGC_gestion_indicadores_frontend/src/lib/api/controller/api/indicatorsInformation/academicProductionListsAuthor.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/api/indicatorsInformation/academicProductionListsAuthor.ts
@@ -1,6 +1,5 @@
-import { getAcademicProductionListsAuthorPreviousCareersByAuthorID, getAcademicProductionListsAuthorsJoinedByAcademicProductionListID, postAcademicProductionListsAuthorCareersRoute, updateAcademicProductionListsAuthorCareersRoute } from "$lib/api/routes/api/indicatorsInformation/academicProductionListsAuthor";
+import { getAcademicProductionListsAuthorsJoinedByAcademicProductionListID, postAcademicProductionListsAuthorCareersRoute, updateAcademicProductionListsAuthorCareersRoute } from "$lib/api/routes/api/indicatorsInformation/academicProductionListsAuthor";
 import type { AcademicProductionListsAuthorsCareersJoined, PostAcademicProductionListsAuthorCareersRequest, UpdateAcademicProductionListsAuthorCareersRequest } from "$lib/api/model/api/indicatorsInformation/academicProductionListsAuthor";
-import type { Career } from "$lib/api/model/api/career";
 
 import { generateCommonErrorFromFetchError } from "$lib/utils";
 import type { CommonError } from "$lib/api/model/errors";
@@ -65,27 +64,6 @@ export async function GetAcademicProductionListsAuthorsJoinedByAcademicProductio
         }
         const academicProductionListsAuthorsCareersJoined: AcademicProductionListsAuthorsCareersJoined[] = await response.json()
         return academicProductionListsAuthorsCareersJoined
-    } catch (error) {
-        return generateCommonErrorFromFetchError(error)
-    }
-}
-
-export async function GetAcademicProductionListAuthorPreviousCareers(token: string, authorID: string): Promise<Career[] | CommonError> {
-    try {
-        const response = await fetch(getAcademicProductionListsAuthorPreviousCareersByAuthorID + authorID, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': token
-            },
-        });
-
-        if (!response.ok) {
-            const error: CommonError = await response.json()
-            return error
-        }
-        const careers: Career[] = await response.json()
-        return careers
     } catch (error) {
         return generateCommonErrorFromFetchError(error)
     }
--- DGC_gestion_indicadores_frontend/src/lib/api/routes/api/indicatorsInformation/academicProductionListsAuthor.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/routes/api/indicatorsInformation/academicProductionListsAuthor.ts
@@ -3,4 +3,3 @@ import { indicatorsInformationBaseRoute } from "./base";
 export const postAcademicProductionListsAuthorCareersRoute = indicatorsInformationBaseRoute + "/academicProductionListsAuthor/authorCareers"
 export const updateAcademicProductionListsAuthorCareersRoute = indicatorsInformationBaseRoute + "/academicProductionListsAuthor/authorCareers"
 export const getAcademicProductionListsAuthorsJoinedByAcademicProductionListID = indicatorsInformationBaseRoute + "/academicProductionListsAuthors/JoinedByAcademicProductionListID/"
-export const getAcademicProductionListsAuthorPreviousCareersByAuthorID = indicatorsInformationBaseRoute + "/academicProductionListsAuthor/previousCareers/"
