999ff28baeb012c2a2495ae28e23739ba9e30245
Erick Carrasco <erickype@hotmail.com>
2024-10-06 16:06:41 -0500

    Fix table row action type in academic periods

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(admin)/admin/academic-periods/Table.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(admin)/admin/academic-periods/Table.svelte
@@ -101,9 +101,9 @@
 
 	let updateFormOpen = false;
 	function handleUpdateAction(event: any) {
-		const detail: { status: boolean; id: number } = event.detail;
+		const detail: { status: boolean; id: string } = event.detail;
 		if (detail.status) {
-			academicPeriod = periods.find((period) => period.ID === detail.id)!;
+			academicPeriod = periods.find((period) => period.ID.toString() === detail.id)!;
 			updateFormOpen = true;
 		} else {
 			updateFormOpen = false;
