dd376dc9e94de4814ad6835c7058c551371d284e
Erick Carrasco <erickype@hotmail.com>
2024-10-09 10:47:31 -0500

    Add load of evaluation periods in evaluation periods dashboard

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(public)/dashboards/evaluation-periods/+page.server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(public)/dashboards/evaluation-periods/+page.server.ts
@@ -1,8 +1,13 @@
 import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
+import { LoadEvaluationPeriodsWithComboMessages } from "$lib/api/controller/view/evaluationPeriod";
 
 export const load: PageServerLoad = async ({ locals }) => {
-    if(!locals.user){
+    if (!locals.user) {
         throw redirect(302, "/login")
+    }
+
+    return {
+        evaluationPeriodsData: await LoadEvaluationPeriodsWithComboMessages()
     }
 };
--- DGC_gestion_indicadores_frontend/src/routes/(app)/(public)/dashboards/evaluation-periods/+page.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(public)/dashboards/evaluation-periods/+page.svelte
@@ -1,1 +1,10 @@
-hi
+<script lang="ts">
+	import type { PageServerData } from './$types';
+
+	export let data: PageServerData;
+
+	const evaluationPeriodsData = data.evaluationPeriodsData;
+	let selectedEvaluationPeriod: number = evaluationPeriodsData.periods.at(0)!.ID;
+</script>
+
+{selectedEvaluationPeriod}
