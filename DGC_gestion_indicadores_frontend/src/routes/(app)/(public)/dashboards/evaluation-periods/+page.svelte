4ddc49a05893fb4ce7d5b7cd7d3d8b5b1b9b8a4f
Erick Carrasco <erickype@hotmail.com>
2024-10-09 11:56:25 -0500

    Add initial load of indicator 26

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(public)/dashboards/evaluation-periods/+page.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(public)/dashboards/evaluation-periods/+page.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+	import type { CommonError } from '$lib/api/model/errors';
 	import type { PageServerData } from './$types';
+	import { goto } from '$app/navigation';
 
 	import Icon from 'lucide-svelte/icons/circle-gauge';
+	import Activity from 'lucide-svelte/icons/activity';
 
+	import { Progress } from '$lib/components/ui/progress';
+	import * as Card from '$lib/components/ui/card';
+
+	import type { IndicatorEvaluationPeriodJoined } from '$lib/api/model/api/indicators/evaluationPeriod';
 	import AcademicPeriodCombo from '$lib/components/combobox/academicPeriodCombo.svelte';
 
 	export let data: PageServerData;
@@ -10,8 +17,10 @@
 	const evaluationPeriodsData = data.evaluationPeriodsData;
 	let selectedEvaluationPeriod: number = evaluationPeriodsData.periods.at(0)!.ID;
 
-	async function fetchGetIndicatorsByEvaluationPeriodID() {
-		/* const url = `/api/indicators/academicPeriod/${selectedAcademicPeriod}`;
+	let indicatorsPromise: Promise<IndicatorEvaluationPeriodJoined> = fetchGetIndicatorsByTypeIDAndEvaluationPeriodID(26);
+
+	async function fetchGetIndicatorsByTypeIDAndEvaluationPeriodID(indicatorTypeID: number) {
+		const url = `/api/indicators/evaluationPeriod/${selectedEvaluationPeriod + '/' + indicatorTypeID}`;
 		const response = await fetch(url, {
 			method: 'GET'
 		});
@@ -22,7 +31,7 @@
 			}
 			throw errorData;
 		}
-		return (indicatorsPromise = response.json()); */
+		return (indicatorsPromise = response.json());
 	}
 </script>
 
@@ -39,6 +48,33 @@
 	<AcademicPeriodCombo
 		messages={evaluationPeriodsData.messages}
 		bind:selectedValue={selectedEvaluationPeriod}
-		on:message={fetchGetIndicatorsByEvaluationPeriodID}
+		on:message={() => {
+			fetchGetIndicatorsByTypeIDAndEvaluationPeriodID(26);
+		}}
 	></AcademicPeriodCombo>
 </div>
+
+{#await indicatorsPromise}
+	cargando...
+{:then indicator}
+	<Card.Root class="bg-secondary/50">
+		<Card.Header class="pb-2">
+			<Card.Description>Indicador {indicator.indicator_type}</Card.Description>
+			<Card.Title class="flex justify-between text-4xl">
+				{indicator.actual_value.toFixed(2)}
+				<Activity class="text-muted-foreground h-4 w-4" />
+			</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<div class="text-muted-foreground text-xs">
+				Obetivo {indicator.target_value} per cápita
+			</div>
+		</Card.Content>
+		<Card.Footer>
+			<Progress
+				value={(indicator.actual_value * 100) / indicator.target_value}
+				aria-label="25% increase"
+			/>
+		</Card.Footer>
+	</Card.Root>
+{/await}
