c4585d9442f7f37a1c36d5b63d4a35263b156395
Erick Carrasco <erickype@hotmail.com>
2024-10-10 15:30:30 -0500

    Add model request, joined and response for filter socialProjectLists

--- DGC_gestion_indicadores_backend/model/indicatorsInformation/socialProjectProduction/socialProjectListFilter.go
+++ DGC_gestion_indicadores_backend/model/indicatorsInformation/socialProjectProduction/socialProjectListFilter.go
@@ -1,1 +1,23 @@
 package model
+
+type FilterSocialProjectListsByAcademicPeriodRequest struct {
+	AcademicPeriodID int    `json:"academic_period_id"`
+	Career           string `json:"career"`
+	Name             string `json:"name"`
+	PageSize         int    `json:"page_size"`
+	Page             int    `json:"page"`
+}
+
+type SocialProjectListByAcademicPeriodJoined struct {
+	AcademicPeriodID int    `json:"academic_period_id"`
+	CareerID         uint   `json:"career_id"`
+	Career           string `json:"career"`
+	Name             string `json:"name"`
+}
+
+type FilterSocialProjectListsByAcademicPeriodResponse struct {
+	Count              int64                                     `json:"count"`
+	PageSize           int                                       `json:"page_size"`
+	Page               int                                       `json:"page"`
+	SocialProjectLists []SocialProjectListByAcademicPeriodJoined `json:"social_project_lists"`
+}
