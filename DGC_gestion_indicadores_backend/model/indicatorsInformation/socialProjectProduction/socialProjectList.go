d383a8914d32d5047474ac398785271b9505efbc
Erick Carrasco <erickype@hotmail.com>
2024-10-10 15:38:05 -0500

    Add timestamp to social project list

--- DGC_gestion_indicadores_backend/model/indicatorsInformation/socialProjectProduction/socialProjectList.go
+++ DGC_gestion_indicadores_backend/model/indicatorsInformation/socialProjectProduction/socialProjectList.go
@@ -1,11 +1,16 @@
 package model
 
-import "github.com/Erickype/DGC_gestion_indicadores_backend/model"
+import (
+	"github.com/Erickype/DGC_gestion_indicadores_backend/model"
+	"time"
+)
 
 type SocialProjectList struct {
-	AcademicPeriodID uint   `gorm:"primary_key" json:"academic_period_id"`
-	CareerID         uint   `gorm:"primary_key" json:"career_id"`
-	Name             string `gorm:"not null;unique;size:1000" json:"name"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
+	AcademicPeriodID uint      `gorm:"primary_key" json:"academic_period_id"`
+	CareerID         uint      `gorm:"primary_key" json:"career_id"`
+	Name             string    `gorm:"not null;unique;size:1000" json:"name"`
 }
 
 func (sp SocialProjectList) TableName() string {
