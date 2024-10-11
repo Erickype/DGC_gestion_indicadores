47cddb1e6be99f035d3abc37c54d0c2f030bc342
Erick Carrasco <erickype@hotmail.com>
2024-10-11 16:10:40 -0500

    Add initial functions to fetch filter academic periods

--- DGC_gestion_indicadores_frontend/src/lib/components/filters/academicPeriods/academicPeriods.ts
+++ DGC_gestion_indicadores_frontend/src/lib/components/filters/academicPeriods/academicPeriods.ts
@@ -1,5 +1,7 @@
-import type { FilterAcademicPeriodsRequest } from "$lib/api/model/view/academicPeriod";
+import type { FilterAcademicPeriodsRequest, FilterAcademicPeriodsResponse } from "$lib/api/model/view/academicPeriod";
+import type { PopoverFilterDataMap } from "$lib/components/table/types";
 
+import type { CommonError } from "$lib/api/model/errors";
 import { goto } from "$app/navigation";
 import { toast } from "svelte-sonner";
 
@@ -14,4 +16,47 @@ export function newFilterAcademicPeriodsRequest(page_size: number, page: number)
         page: page
     };
     return filterAcademicPeriodsRequest
+}
+
+export function newPopoverFilterDataMap(): PopoverFilterDataMap {
+    let popoverFilterDataMap: PopoverFilterDataMap = new Map();
+    popoverFilterDataMap.set('name', { label: 'Nombre', value: '' });
+    popoverFilterDataMap.set('abbreviation', { label: 'Abreviación', value: '' });
+    popoverFilterDataMap.set('description', { label: 'Descripción', value: '' });
+    popoverFilterDataMap.set('start_date', { label: 'Fecha inicio', value: '' });
+    popoverFilterDataMap.set('end_date', { label: 'Fecha fin', value: '' });
+    return popoverFilterDataMap
+}
+
+export function generateInitialFilterValue(filterAcademicPeriodsRequest: FilterAcademicPeriodsRequest): string | undefined {
+    let values = [
+        filterAcademicPeriodsRequest.name,
+        filterAcademicPeriodsRequest.description,
+        filterAcademicPeriodsRequest.abbreviation,
+        filterAcademicPeriodsRequest.start_date,
+        filterAcademicPeriodsRequest.end_date
+    ];
+
+    let uniqueValues = [...new Set(values.filter((value) => value !== ''))];
+
+    let initialFilterValue: string | undefined =
+        uniqueValues.length === 1 ? uniqueValues[0] : uniqueValues.join(' ');
+
+    return initialFilterValue
+}
+
+export async function fetchFilterAcademicPeriods(filterAcademicPeriodsRequest: FilterAcademicPeriodsRequest): Promise<FilterAcademicPeriodsResponse> {
+    const url = `/api/academicPeriods/filter`;
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(filterAcademicPeriodsRequest)
+    });
+    if (!response.ok) {
+        const errorData = (await response.json()) as CommonError;
+        if (response.status === 401) {
+            throw goto('/');
+        }
+        throw errorData;
+    }
+    return response.json();
 }
