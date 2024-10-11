f87aa6fc0161bf89ca6398a3b06bbce2db7ab725
Erick Carrasco <erickype@hotmail.com>
2024-10-11 15:14:38 -0500

    Add page header in social projects

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/social-projects/+page.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/social-projects/+page.svelte
@@ -1,1 +1,14 @@
-Hi
+<script lang="ts">
+	import Icon from 'lucide-svelte/icons/users-round';
+</script>
+
+<svelte:head>
+	<title>Proyectos Vinculación</title>
+</svelte:head>
+
+<div class="mx-auto flex w-full place-content-center justify-between px-8">
+	<div class="flex items-center gap-1">
+		<Icon class="h-8 w-8" />
+		<h2 class="text-2xl font-bold">Proyectos Vinculación</h2>
+	</div>
+</div>
