1fa18d873459686c56f7a34fd54d2601c8ea88c0
Erick Carrasco <erickype@hotmail.com>
2024-10-12 08:36:21 -0500

    Add GetAcademicPeriodByID client side api

--- DGC_gestion_indicadores_frontend/src/routes/api/academicPeriod/[id]/+server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/api/academicPeriod/[id]/+server.ts
@@ -1,5 +1,16 @@
 import { DeleteAcademicPeriod } from "$lib/api/controller/admin/academicPeriod";
+import { GetAcademicPeriodByID } from "$lib/api/controller/view/academicPeriod";
+import { generateErrorFromCommonError, type CommonError } from "$lib/api/model/errors";
 import { error, json, type RequestHandler } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async ({ params }) => {
+    const id = params.id
+    const response = await GetAcademicPeriodByID(id!)
+    if ((response as CommonError).status_code) {
+        return generateErrorFromCommonError(response as CommonError)
+    }
+    return json(response);
+};
 
 export const DELETE: RequestHandler = async ({ cookies, params }) => {
     const id = params.id
