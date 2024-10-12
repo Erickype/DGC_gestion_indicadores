6d4b49bdc95eae8f99149bbe16e2374a68e225a8
Erick Carrasco <erickype@hotmail.com>
2024-10-12 10:39:45 -0500

    Add load of data used by table in social project lists page

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/social-projects/+page.svelte
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/social-projects/+page.svelte
@@ -11,6 +11,19 @@
 	import Icon from 'lucide-svelte/icons/users-round';
 
 	import AcademicPeriodsServer from '$lib/components/filters/academicPeriods/academicPeriodsServer.svelte';
+	import TableSkeleton from '$lib/components/skeleton/table.svelte';
+	import Alert from '$lib/components/alert/alert.svelte';
+	import Table from './Table.svelte';
+	import type {
+		FilterSocialProjectListsByAcademicPeriodRequest,
+		FilterSocialProjectListsByAcademicPeriodResponse
+	} from '$lib/api/model/api/indicatorsInformation/socialProjectLists';
+	import {
+		fetchFilterSocialProjectLists,
+		newFilterSocialProjectListsByAcademiPeriodRequest,
+		newPopoverFilterDataMap
+	} from '$lib/components/filters/indicatorsInformation/socialProjectLists/socialProjectLists';
+	import type { PopoverFilterDataMap } from '$lib/components/table/types';
 
 	export let data: PageServerData;
 	const filterAcademicPeriodsAuxForm = data.filterAcademicPeriodsAuxForm;
@@ -29,8 +42,48 @@
 		fetchPostFilterSocialProjectLists();
 	});
 
+	let filterSocialProjectListsByAcademicPeriodRequest: FilterSocialProjectListsByAcademicPeriodRequest =
+		newFilterSocialProjectListsByAcademiPeriodRequest(5, 1, $formData.academic_period_id);
+	let filterSocialProjectListsPromise: Promise<FilterSocialProjectListsByAcademicPeriodResponse> =
+		fetchFilterSocialProjectLists(filterSocialProjectListsByAcademicPeriodRequest);
+	let popoverFilterDataMap: PopoverFilterDataMap = newPopoverFilterDataMap();
+
 	function fetchPostFilterSocialProjectLists() {
 		console.log('loading...');
+	}
+
+	function fetchOnSuccess(event: CustomEvent) {
+		/* const detail: { status: boolean } = event.detail;
+		if (detail.status) {
+			filterBooksOrChaptersProductionListPromise = fetchFilterBooksOrChaptersProductionLists(
+				filterBooksOrChaptersProductionListsByAcademicPeriodRequest
+			);
+		} */
+	}
+
+	function handleOnFilterChanged(event: CustomEvent) {
+		/* 	const data: { filter: string } = event.detail;
+		filterBooksOrChaptersProductionListPromise = fetchOnFilterChanged(
+			data.filter.trim(),
+			filterBooksOrChaptersProductionListsByAcademicPeriodRequest,
+			popoverFilterDataMap
+		); */
+	}
+
+	function handleOnDetailedFilter() {
+		/* 	filterBooksOrChaptersProductionListPromise = fetchOnDetailedFilter(
+			filterBooksOrChaptersProductionListsByAcademicPeriodRequest,
+			popoverFilterDataMap
+		).then(({ request, response }) => {
+			filterBooksOrChaptersProductionListsByAcademicPeriodRequest = request;
+			return response;
+		}); */
+	}
+
+	function handlePaginationChanged() {
+		/* filterBooksOrChaptersProductionListPromise = fetchFilterBooksOrChaptersProductionLists(
+			filterBooksOrChaptersProductionListsByAcademicPeriodRequest
+		); */
 	}
 </script>
 
@@ -49,4 +102,28 @@
 	<Form.Field {form} name="academic_period_id" class="w-1/3">
 		<AcademicPeriodsServer {formDataAcademicPeriodID} />
 	</Form.Field>
+</div>
+
+<div class="mx-auto flex w-full flex-col place-content-center px-8">
+	{#await filterSocialProjectListsPromise}
+		<TableSkeleton tableHeightClass="h-[55vh]" />
+	{:then filterSocialProjectListsByAcademicPeriodResponse}
+		{#if filterSocialProjectListsByAcademicPeriodResponse.social_project_lists}
+			<Table
+				bind:filterSocialProjectListsByAcademicPeriodRequest
+				{filterSocialProjectListsByAcademicPeriodResponse}
+				bind:popoverFilterDataMap
+				comboMessages={undefined}
+				on:updated={fetchOnSuccess}
+				on:deleted={fetchOnSuccess}
+				on:filterChanged={handleOnFilterChanged}
+				on:detailedFilter={handleOnDetailedFilter}
+				on:paginationChanged={handlePaginationChanged}
+			></Table>
+		{:else}
+			<Alert title="Sin registros" description={'Ups, no hay artículos registrados.'} />
+		{/if}
+	{:catch error}
+		<Alert variant="destructive" description={error.message} />
+	{/await}
 </div>
