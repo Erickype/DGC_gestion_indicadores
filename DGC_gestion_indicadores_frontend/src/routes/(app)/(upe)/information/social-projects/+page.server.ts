eb509bd1cdcaec568befccb640dedeb71e42d7a8
Erick Carrasco <erickype@hotmail.com>
2024-10-12 15:59:24 -0500

    Add careers combo data and addSocialProjectListForm to page load

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/social-projects/+page.server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/social-projects/+page.server.ts
@@ -3,10 +3,12 @@ import { mainDashboarRoute } from "$lib/api/util/paths";
 import { redirect } from "@sveltejs/kit";
 
 import { filterAcademicPeriodsAuxSchema } from "$lib/utils";
+import { addSocialProjectListSchema } from "./schema";
 import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 
 import { LoadAcademicPeriodsWithComboMessages } from "$lib/api/controller/view/academicPeriod";
+import { LoadCareersWithComboMessages } from "$lib/api/controller/api/career";
 
 export const load: PageServerLoad = async ({ locals, cookies }) => {
     const token = cookies.get("AuthorizationToken")
@@ -20,6 +22,8 @@ export const load: PageServerLoad = async ({ locals, cookies }) => {
     }
     return {
         academicPeriodsData: await LoadAcademicPeriodsWithComboMessages(),
+        careersData: await LoadCareersWithComboMessages(token!),
         filterAcademicPeriodsAuxForm: await superValidate(zod(filterAcademicPeriodsAuxSchema)),
+        addSocialProjectListForm: await superValidate(zod(addSocialProjectListSchema))
     }
 };
