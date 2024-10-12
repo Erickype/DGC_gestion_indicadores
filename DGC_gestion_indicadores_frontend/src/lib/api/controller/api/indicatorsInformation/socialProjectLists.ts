265c4737bded6ca7a983b2f7f1de2de8b30313b5
Erick Carrasco <erickype@hotmail.com>
2024-10-12 18:29:24 -0500

    Add PutSocialProjectList api client

--- DGC_gestion_indicadores_frontend/src/lib/api/controller/api/indicatorsInformation/socialProjectLists.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/api/indicatorsInformation/socialProjectLists.ts
@@ -1,5 +1,5 @@
 import type { FilterSocialProjectListsByAcademicPeriodRequest, FilterSocialProjectListsByAcademicPeriodResponse, SocialProjectList } from "$lib/api/model/api/indicatorsInformation/socialProjectLists";
-import { postFilterSocialProjectListsByAcademicPeriodRoute, postSocialProjectListRoute } from "$lib/api/routes/api/indicatorsInformation/socialProjectLists";
+import { postFilterSocialProjectListsByAcademicPeriodRoute, postSocialProjectListRoute, putSocialProjectListRoute } from "$lib/api/routes/api/indicatorsInformation/socialProjectLists";
 
 import { generateCommonErrorFromFetchError } from "$lib/utils";
 import type { CommonError } from "$lib/api/model/errors";
@@ -8,6 +8,28 @@ export async function PostSocialProjectList(token: string, request: SocialProjec
     try {
         const response = await fetch(postSocialProjectListRoute, {
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            },
+            body: JSON.stringify(request)
+        });
+
+        if (!response.ok) {
+            const error: CommonError = await response.json()
+            return error
+        }
+        const socialProjectList: SocialProjectList = await response.json()
+        return socialProjectList
+    } catch (error) {
+        return generateCommonErrorFromFetchError(error)
+    }
+}
+
+export async function PutSocialProjectList(token: string, request: SocialProjectList): Promise<SocialProjectList | CommonError> {
+    try {
+        const response = await fetch(putSocialProjectListRoute + request.ID, {
+            method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': token
--- DGC_gestion_indicadores_frontend/src/lib/api/routes/api/indicatorsInformation/socialProjectLists.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/routes/api/indicatorsInformation/socialProjectLists.ts
@@ -1,4 +1,5 @@
 import { indicatorsInformationBaseRoute } from "./base";
 
 export const postSocialProjectListRoute = indicatorsInformationBaseRoute + "/socialProjectList"
+export const putSocialProjectListRoute = indicatorsInformationBaseRoute + "/socialProjectList/"
 export const postFilterSocialProjectListsByAcademicPeriodRoute = indicatorsInformationBaseRoute + "/socialProjectLists/filter"
