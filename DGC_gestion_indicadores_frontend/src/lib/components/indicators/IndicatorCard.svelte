c792ad4ce4428ed7f07ebd6cfa7bc5bc44cd36b5
Erick Carrasco <erickype@hotmail.com>
2024-10-13 15:42:30 -0500

    Add indicator card to show arc chart

---
+++ DGC_gestion_indicadores_frontend/src/lib/components/indicators/IndicatorCard.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Card from '$lib/components/ui/card';
+
+	import Activity from 'lucide-svelte/icons/activity';
+
+	import type { IndicatorEvaluationPeriodJoined } from '$lib/api/model/api/indicators/evaluationPeriod';
+	import type { IndicatorAcademicPeriodJoined } from '$lib/api/model/api/indicators/academicPeriods';
+	import IndicatorPie from '$lib/components/indicators/IndicatorPie.svelte';
+
+	export let indicator: IndicatorEvaluationPeriodJoined | IndicatorAcademicPeriodJoined;
+</script>
+
+<Card.Root class="bg-secondary/50">
+	<Card.Header class="pb-2">
+		<Card.Title class="flex items-center justify-between gap-2 text-xl">
+			<p>{indicator.indicator_type}</p>
+			<Button variant="ghost" size="icon">
+				<Activity class="h-4 w-4" />
+			</Button>
+		</Card.Title>
+		<Card.Description>Obetivo {indicator.target_value}</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<IndicatorPie {indicator} isPercentaje={false} />
+	</Card.Content>
+</Card.Root>
