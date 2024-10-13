7be8e72f2d659317844a75513e141db9cbc390b7
Erick Carrasco <erickype@hotmail.com>
2024-10-13 11:05:40 -0500

    Add theme switch

--- DGC_gestion_indicadores_frontend/src/lib/components/menu/header.svelte
+++ DGC_gestion_indicadores_frontend/src/lib/components/menu/header.svelte
@@ -2,6 +2,11 @@
 	import Search from 'lucide-svelte/icons/search';
 	import CircleUser from 'lucide-svelte/icons/circle-user';
 
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+
+	import { toggleMode } from 'mode-watcher';
+
 	import { Button } from '$lib/components/ui/button/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
@@ -21,6 +26,15 @@
 			</div>
 		</form> -->
 	</div>
+	<Button on:click={toggleMode} variant="outline" size="icon">
+		<Sun
+			class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+		/>
+		<Moon
+			class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+		/>
+		<span class="sr-only">Cambiar tema</span>
+	</Button>
 	<DropdownMenu.Root>
 		<DropdownMenu.Trigger asChild let:builder>
 			<Button builders={[builder]} variant="secondary" size="icon" class="rounded-full">
