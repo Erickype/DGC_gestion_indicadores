873f5a1836b6acc7de441ca09555e84ecc1d3ab7
Erick Carrasco <erickype@hotmail.com>
2024-10-13 16:02:14 -0500

    Add GetCalculateIndicatorByTypeIDAndEvaluationPeriod client api

---
+++ DGC_gestion_indicadores_frontend/src/routes/api/indicators/evaluationPeriod/calulate/[evaluationPeriodID/[indicatorTypeID]]/+server.ts
@@ -0,0 +1,17 @@
+import { GetCalculateIndicatorByTypeIDAndEvaluationPeriod } from "$lib/api/controller/api/indicators/evaluationPeriod";
+
+import { generateErrorFromCommonError, type CommonError } from "$lib/api/model/errors";
+import { json, type RequestHandler } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async ({ cookies, params }) => {
+    const token = cookies.get("AuthorizationToken")
+    const indicatorTypeID = params.indicatorTypeID
+    const evaluationPeriodID = params.evaluationPeriodID
+
+    const response = await GetCalculateIndicatorByTypeIDAndEvaluationPeriod(evaluationPeriodID!, indicatorTypeID!, token!)
+
+    if ((response as CommonError).status_code) {
+        return generateErrorFromCommonError(response as CommonError)
+    }
+    return json(response);
+};
