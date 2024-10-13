465234df58b8e694df6b8ecfe1057cd07b65c44e
Erick Carrasco <erickype@hotmail.com>
2024-10-13 15:55:38 -0500

    Add type check for academic period indicator

--- DGC_gestion_indicadores_frontend/src/lib/components/indicators/IndicatorCard.svelte
+++ DGC_gestion_indicadores_frontend/src/lib/components/indicators/IndicatorCard.svelte
@@ -9,19 +9,43 @@
 	import IndicatorPie from '$lib/components/indicators/IndicatorPie.svelte';
 
 	export let indicator: IndicatorEvaluationPeriodJoined | IndicatorAcademicPeriodJoined;
+
+	let isAcademicPeriodIndicator: boolean;
+	if ((indicator as IndicatorAcademicPeriodJoined).academic_period_id) {
+		isAcademicPeriodIndicator = true;
+	} else {
+		isAcademicPeriodIndicator = false;
+	}
 </script>
 
-<Card.Root class="bg-secondary/50">
-	<Card.Header class="pb-2">
-		<Card.Title class="flex items-center justify-between gap-2 text-xl">
-			<p>{indicator.indicator_type}</p>
-			<Button variant="ghost" size="icon">
-				<Activity class="h-4 w-4" />
-			</Button>
-		</Card.Title>
-		<Card.Description>Obetivo {indicator.target_value}</Card.Description>
-	</Card.Header>
-	<Card.Content>
-		<IndicatorPie {indicator} isPercentaje={false} />
-	</Card.Content>
-</Card.Root>
+{#if isAcademicPeriodIndicator}
+	<Card.Root class="bg-secondary/50">
+		<Card.Header class="pb-2">
+			<Card.Title class="flex items-center justify-between gap-2 text-xl">
+				<p>{indicator.indicator_type}</p>
+				<Button variant="ghost" size="icon">
+					<Activity class="h-4 w-4" />
+				</Button>
+			</Card.Title>
+			<Card.Description>Obetivo {indicator.target_value}</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<IndicatorPie {indicator} isPercentaje={false} />
+		</Card.Content>
+	</Card.Root>
+{:else}
+	<Card.Root class="bg-secondary/50">
+		<Card.Header class="pb-2">
+			<Card.Title class="flex items-center justify-between gap-2 text-xl">
+				<p>{indicator.indicator_type}</p>
+				<Button variant="ghost" size="icon">
+					<Activity class="h-4 w-4" />
+				</Button>
+			</Card.Title>
+			<Card.Description>Obetivo {indicator.target_value}</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<IndicatorPie {indicator} isPercentaje={false} />
+		</Card.Content>
+	</Card.Root>
+{/if}
