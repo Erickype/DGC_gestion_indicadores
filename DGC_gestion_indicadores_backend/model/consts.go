d0c2b764794e7f29a15ce42b2416598ad9d6f0a9
Erick Carrasco <erickype@hotmail.com>
2024-10-14 17:24:01 -0500

    Add constants for indicator 29

--- DGC_gestion_indicadores_backend/model/consts.go
+++ DGC_gestion_indicadores_backend/model/consts.go
@@ -8,10 +8,12 @@ const (
 	Indicator16 = 16
 	Indicator17 = 17
 	Indicator26 = 26
+	Indicator29 = 29
 
 	Indicator16TargetValue = 20
 	Indicator17TargetValue = 50
 	Indicator26TargetValue = 1.5
+	Indicator29TargetValue = 1.5
 
 	DegreeLevelPhD = "Cuarto nivel Doctorado"
 
