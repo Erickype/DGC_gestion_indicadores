fa41150465b7c4b9b818e6350eb334a24a4296b9
Erick Carrasco <erickype@hotmail.com>
2024-10-15 16:00:25 -0500

    Add PostGradeRateList api client

--- DGC_gestion_indicadores_frontend/src/lib/api/controller/api/indicatorsInformation/gradeRateLists.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/api/indicatorsInformation/gradeRateLists.ts
@@ -1,5 +1,5 @@
-import { getGradeRateListsByAcademicPeriodRoute } from "$lib/api/routes/api/indicatorsInformation/gradeRateLists";
-import type { GradeRateListJoined } from "$lib/api/model/api/indicatorsInformation/gradeRateLists";
+import { getGradeRateListsByAcademicPeriodRoute, postGradeRateListRoute } from "$lib/api/routes/api/indicatorsInformation/gradeRateLists";
+import type { GradeRateList, GradeRateListJoined } from "$lib/api/model/api/indicatorsInformation/gradeRateLists";
 
 import { generateCommonErrorFromFetchError } from "$lib/utils";
 import type { CommonError } from "$lib/api/model/errors";
@@ -20,6 +20,28 @@ export async function GetGradeRateListsByAcademicPeriod(token: string, academicP
         }
         const gradeRateListsJoined: GradeRateListJoined[] = await response.json()
         return gradeRateListsJoined
+    } catch (error) {
+        return generateCommonErrorFromFetchError(error)
+    }
+}
+
+export async function PostGradeRateList(token: string, request: GradeRateList): Promise<GradeRateList | CommonError> {
+    try {
+        const response = await fetch(postGradeRateListRoute, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            },
+            body: JSON.stringify(request)
+        });
+
+        if (!response.ok) {
+            const error: CommonError = await response.json()
+            return error
+        }
+        const gradeRateList: GradeRateList = await response.json()
+        return gradeRateList
     } catch (error) {
         return generateCommonErrorFromFetchError(error)
     }
--- DGC_gestion_indicadores_frontend/src/lib/api/routes/api/indicatorsInformation/gradeRateLists.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/routes/api/indicatorsInformation/gradeRateLists.ts
@@ -1,3 +1,4 @@
 import { indicatorsInformationBaseRoute } from "./base";
 
 export const getGradeRateListsByAcademicPeriodRoute = indicatorsInformationBaseRoute + "/gradeRateLists/"
+export const postGradeRateListRoute = indicatorsInformationBaseRoute + "/gradeRateList"
