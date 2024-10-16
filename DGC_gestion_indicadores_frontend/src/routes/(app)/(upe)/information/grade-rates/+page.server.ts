81c46fb56c1e60561f0a5ff5a4f57c22eaed24a3
Erick Carrasco <erickype@hotmail.com>
2024-10-15 21:00:52 -0500

    Add updateGradeRateList server action

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/grade-rates/+page.server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/grade-rates/+page.server.ts
@@ -3,14 +3,14 @@ import type { PageServerLoad, Actions } from "./$types";
 import { redirect } from "@sveltejs/kit"
 
 import { filterAcademicPeriodsAuxSchema, generateFormMessageFromHttpResponse, generateFormMessageFromInvalidForm } from "$lib/utils";
+import { addGradeRateListSchema, updateGradeRateListSchema } from "./schema";
 import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 
+import { PostGradeRateList, UpdateGradeRateList } from "$lib/api/controller/api/indicatorsInformation/gradeRateLists";
 import { LoadAcademicPeriodsWithComboMessages } from "$lib/api/controller/view/academicPeriod";
-import { LoadCareersWithComboMessages } from "$lib/api/controller/api/career";
-import { addGradeRateListSchema, updateGradeRateListSchema } from "./schema";
-import { PostGradeRateList } from "$lib/api/controller/api/indicatorsInformation/gradeRateLists";
 import type { GradeRateList } from "$lib/api/model/api/indicatorsInformation/gradeRateLists";
+import { LoadCareersWithComboMessages } from "$lib/api/controller/api/career";
 
 export const load: PageServerLoad = async ({ cookies, locals }) => {
     const token = cookies.get("AuthorizationToken")
@@ -50,6 +50,29 @@ export const actions: Actions = {
         }
 
         const response = await PostGradeRateList(token!, gradeRateList)
+
+        return generateFormMessageFromHttpResponse(form, response)
+    },
+
+    updateGradeRateList: async (event) => {
+        const form = await superValidate(event, zod(updateGradeRateListSchema))
+
+        if (!form.valid) {
+            return generateFormMessageFromInvalidForm(form)
+        }
+
+        const token = event.cookies.get("AuthorizationToken")
+        const data = form.data
+        const gradeRateList: GradeRateList = {
+            academic_period_id: data.academic_period_id,
+            career_id: data.career_id,
+            count_graduated_students: data.count_graduated_students,
+            count_court_students: data.count_court_students,
+            count_admitted_matriculated_students: data.count_admitted_matriculated_students,
+            count_admitted_students: data.count_admitted_students
+        }
+
+        const response = await UpdateGradeRateList(token!, gradeRateList)
 
         return generateFormMessageFromHttpResponse(form, response)
     }
