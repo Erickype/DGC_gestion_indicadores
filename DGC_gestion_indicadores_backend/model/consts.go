c45c7f4006209f50db3d2cc646b1189cd8f8b27c
Erick Carrasco <erickype@hotmail.com>
2024-10-16 11:21:03 -0500

    Add indicator 19 consts

--- DGC_gestion_indicadores_backend/model/consts.go
+++ DGC_gestion_indicadores_backend/model/consts.go
@@ -7,12 +7,14 @@ const (
 
 	Indicator16 = 16
 	Indicator17 = 17
+	Indicator19 = 19
 	Indicator21 = 21
 	Indicator26 = 26
 	Indicator29 = 29
 
 	Indicator16TargetValue = 20
 	Indicator17TargetValue = 50
+	Indicator19TargetValue = 14
 	Indicator21TargetValue = 50
 	Indicator26TargetValue = 1.5
 	Indicator29TargetValue = 1.5
