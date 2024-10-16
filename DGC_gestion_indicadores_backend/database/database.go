8220084b95398ade3ccd66eb1c6617b3f90e4a81
Erick Carrasco <erickype@hotmail.com>
2024-10-16 16:34:31 -0500

    Update ssl mode from env

--- DGC_gestion_indicadores_backend/database/database.go
+++ DGC_gestion_indicadores_backend/database/database.go
@@ -22,9 +22,10 @@ func connectDB() *gorm.DB {
 	password := os.Getenv("POSTGRES_PASSWORD")
 	dbname := os.Getenv("POSTGRES_DB")
 	port := os.Getenv("POSTGRES_PORT")
+	sslMode := os.Getenv("SSL_MODE")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
-		host, username, password, dbname, port)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=America/Guayaquil",
+		host, username, password, dbname, port, sslMode)
 	log.Println("dsn : ", dsn)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{TranslateError: true})
