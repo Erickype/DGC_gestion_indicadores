1f130bf87e678f592f720e22de61c113e78bf65c
Erick Carrasco <erickype@hotmail.com>
2024-10-18 14:08:11 -0500

    Add CalculateIndicatorsByAcademicPeriod api client

--- DGC_gestion_indicadores_frontend/src/lib/api/controller/api/indicators/academicPeriods.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/api/indicators/academicPeriods.ts
@@ -1,6 +1,6 @@
 import type { IndicatorAcademicPeriodJoined, IndicatorsAcademicPeriod } from "$lib/api/model/api/indicators/academicPeriods";
 import type { CommonError } from "$lib/api/model/errors";
-import { getCalculateIndicatorByTypeIDAndAcademicPeriodRoute, getIndicatorsByAcademicPeriodIDRoute } from "$lib/api/routes/api/indicators/academicPeriods";
+import { getCalculateIndicatorByTypeIDAndAcademicPeriodRoute, getCalculateIndicatorsByAndAcademicPeriodRoute, getIndicatorsByAcademicPeriodIDRoute } from "$lib/api/routes/api/indicators/academicPeriods";
 import { generateCommonErrorFromFetchError } from "$lib/utils";
 
 export async function GetIndicatorsByAcademicPeriod(academic_period_id: string, token: string) {
@@ -27,6 +27,27 @@ export async function GetIndicatorsByAcademicPeriod(academic_period_id: string, 
 export async function GetCalculateIndicatorByTypeIDAndAcademicPeriod(academic_period_id: string, indicator_type_id: string, token: string) {
     try {
         const response = await fetch(getCalculateIndicatorByTypeIDAndAcademicPeriodRoute + academic_period_id + "/" + indicator_type_id, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            }
+        });
+
+        if (!response.ok) {
+            const error: CommonError = await response.json()
+            return error
+        }
+        const indicators: IndicatorsAcademicPeriod[] = await response.json()
+        return indicators
+    } catch (error) {
+        return generateCommonErrorFromFetchError(error)
+    }
+}
+
+export async function GetCalculateIndicatorsByAcademicPeriod(academic_period_id: string, token: string) {
+    try {
+        const response = await fetch(getCalculateIndicatorsByAndAcademicPeriodRoute + academic_period_id, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
--- DGC_gestion_indicadores_frontend/src/lib/api/routes/api/indicators/academicPeriods.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/routes/api/indicators/academicPeriods.ts
@@ -2,3 +2,4 @@ import { indicatorsBaseRoute } from "./base";
 
 export const getIndicatorsByAcademicPeriodIDRoute = indicatorsBaseRoute + "/academicPeriod/"
 export const getCalculateIndicatorByTypeIDAndAcademicPeriodRoute = indicatorsBaseRoute + "/academicPeriod/calculate/"
+export const getCalculateIndicatorsByAndAcademicPeriodRoute = indicatorsBaseRoute + "/academicPeriod/calculate/"
