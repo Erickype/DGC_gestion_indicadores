d1328c750ce8b844aa4fd5953b1680ed759e42ba
Erick Carrasco <erickype@hotmail.com>
2024-10-18 14:11:56 -0500

    Add redirect in case no user

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(public)/dashboards/academic-periods/+page.server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(public)/dashboards/academic-periods/+page.server.ts
@@ -1,7 +1,12 @@
 import { LoadAcademicPeriodsWithComboMessages } from "$lib/api/controller/view/academicPeriod";
 import type { PageServerLoad } from "./$types";
+import { redirect } from "@sveltejs/kit";
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({locals}) => {
+    if (!locals.user) {
+        throw redirect(302, "/login")
+    }
+
     return {
         academicPeriodsData: await LoadAcademicPeriodsWithComboMessages(),
     }
