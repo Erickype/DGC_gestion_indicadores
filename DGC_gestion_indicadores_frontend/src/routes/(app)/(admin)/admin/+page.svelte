f2e582bfe89029fda9776bfb7aa2da91b34b5293
Erick Carrasco <erickype@hotmail.com>
2024-10-18 21:29:48 -0500

    Remove and add header to users page

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(admin)/admin/+page.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-	<title>Admin</title>
-</svelte:head>
-
-<div class="flex items-center justify-center mt-10">
-	<h1 class="text-xl">Administador</h1>
-</div>
---
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(admin)/admin/users/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Icon from 'lucide-svelte/icons/user-round-search';
+</script>
+
+<svelte:head>
+	<title>Usuarios</title>
+</svelte:head>
+
+<div class="mx-auto flex w-full place-content-center justify-between px-8">
+	<div class="flex items-center gap-1">
+		<Icon class="h-8 w-8" />
+		<h2 class="text-2xl font-bold">Usuarios</h2>
+	</div>
+</div>
+
+<div class="mx-auto flex w-full place-content-center px-8">
+</div>
