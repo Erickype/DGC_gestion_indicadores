4e514be58ae4b891f943fd038f96bfb207e51777
Erick Carrasco <erickype@hotmail.com>
2024-10-18 21:40:56 -0500

    Add client api GetUsers

---
+++ DGC_gestion_indicadores_frontend/src/routes/api/admin/users/+server.ts
@@ -0,0 +1,15 @@
+import { GetUsers } from "$lib/api/controller/admin/user";
+
+import { generateErrorFromCommonError, type CommonError } from "$lib/api/model/errors";
+import { json, type RequestHandler } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async ({ cookies }) => {
+    const token = cookies.get("AuthorizationToken")
+
+    const response = await GetUsers(token!)
+
+    if ((response as CommonError).status_code) {
+        return generateErrorFromCommonError(response as CommonError)
+    }
+    return json(response);
+};
