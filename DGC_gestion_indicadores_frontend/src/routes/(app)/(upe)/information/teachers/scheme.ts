03f5421030d0ac1cc05b5605801d9a89141e5f44
Erick Carrasco <erickype@hotmail.com>
2024-11-01 11:46:48 -0500

    Modify update teacher list schema to enable conditional scaled grade based on contract type == "Nombramiento"

--- DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/teachers/scheme.ts
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/teachers/scheme.ts
@@ -59,16 +59,22 @@ export const updateTeacherSchema = z.object({
     }).gt(0, {
         message: "Ingrese una dedicación válida"
     }),
-    scaledGrade: z.number({
-        required_error: "Grado escalafonado requerido"
-    }).gt(0, {
-        message: "Ingrese un grado escalafonado válido"
-    }),
     contractType: z.number({
         required_error: "Tipo contrato requerido"
     }).gt(0, {
         message: "Ingrese un tipo contrato válido"
+    }),
+    scaledGrade: z.number().nullable().refine(value => value === null || value > 0, {
+        message: "Ingrese un grado escalafonado válido"
     })
+}).refine(input => {
+    if (input.contractType === 1 && input.scaledGrade === null) {
+        return false;
+    }
+    return true;
+}, {
+    message: "Grado escalafonario requerido",
+    path: ["scaledGrade"]
 });
 
 export type UpdateTeacherSchema = typeof updateTeacherSchema;
