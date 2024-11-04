1cd53d390f64665bd8683a7574f882e118baae67
Erick Carrasco <erickype@hotmail.com>
2024-11-04 17:39:07 -0500

    Add toISO8601 to utils

--- DGC_gestion_indicadores_frontend/src/lib/utils.ts
+++ DGC_gestion_indicadores_frontend/src/lib/utils.ts
@@ -113,6 +113,14 @@ export function generateCommonErrorFromFetchError(error: unknown | any): CommonE
 	return errorMessage
 }
 
+//Dates
+export function toISO8601(dateString: string): string {
+	const date = new Date(dateString);
+	const year = date.getUTCFullYear();
+	const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // getUTCMonth() is zero-based
+	const day = String(date.getUTCDate()).padStart(2, '0');
+	return `${year}-${month}-${day}`;
+}
 
 // DraggableFilterStructure
 export interface DragableFilterFields {
