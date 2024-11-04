9dafb25aa4451176471b7b5d65477941e5977464
Erick Carrasco <erickype@hotmail.com>
2024-11-04 14:51:00 -0500

    Add artistic production schema

---
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(upe)/information/artistic-production/schema.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+
+export const addArtisticProductionListSchema = z.object({
+    academic_period_id: z.number({
+        required_error: "Periodo académico requerido"
+    }).gt(0, {
+        message: "Ingrese una periodo académico válido"
+    }),
+
+    international_artistic_work: z.coerce.number({
+        required_error: "Obra internacional requerida"
+    }).gt(0, {
+        message: "Ingrese una valor válido"
+    }),
+
+    national_artistic_work: z.coerce.number({
+        required_error: "Obra nacional requerida"
+    }).gt(0, {
+        message: "Ingrese una valor válido"
+    }),
+});
+
+export const updateArtisticProductionListSchema = z.object({
+    academic_period_id: z.number({
+        required_error: "Periodo académico requerido"
+    }).gt(0, {
+        message: "Ingrese una periodo académico válido"
+    }),
+
+    international_artistic_work: z.coerce.number({
+        required_error: "Obra internacional requerida"
+    }).gt(0, {
+        message: "Ingrese una valor válido"
+    }),
+
+    national_artistic_work: z.coerce.number({
+        required_error: "Obra nacional requerida"
+    }).gt(0, {
+        message: "Ingrese una valor válido"
+    }),
+});
+
+export type AddArtisticProductionListSchema = typeof addArtisticProductionListSchema;
+export type UpdateArtisticProductionListSchema = typeof updateArtisticProductionListSchema;
