2b5b9cef50228a80fd5aee15a979ede8a94ebbf9
Erick Carrasco <erickype@hotmail.com>
2024-11-08 17:16:22 -0500

    Add PostFilterPostGraduatePrograms client api

---
+++ DGC_gestion_indicadores_frontend/src/routes/api/indicatorsInformation/postgraduate/programs/filter/+server.ts
@@ -0,0 +1,14 @@
+import { PostFilterPostGraduatePrograms } from "$lib/api/controller/api/indicatorsInformation/postgraduate";
+
+import { generateErrorFromCommonError, type CommonError } from "$lib/api/model/errors";
+import { json, type RequestHandler } from "@sveltejs/kit";
+
+export const POST: RequestHandler = async ({ cookies, request }) => {
+    const token = cookies.get("AuthorizationToken")
+    const response = await PostFilterPostGraduatePrograms(token!, await request.json())
+
+    if ((response as CommonError).status_code) {
+        return generateErrorFromCommonError(response as CommonError)
+    }
+    return json(response);
+};
