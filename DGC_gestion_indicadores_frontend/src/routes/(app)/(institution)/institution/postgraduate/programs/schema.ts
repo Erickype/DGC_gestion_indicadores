a84a23d86d3502900102bb7ef7d318400acb1fd5
Erick Carrasco <erickype@hotmail.com>
2024-11-08 20:51:37 -0500

    Add PostgraduateProgram schema add and update

---
+++ DGC_gestion_indicadores_frontend/src/routes/(app)/(institution)/institution/postgraduate/programs/schema.ts
@@ -0,0 +1,50 @@
+import { z } from "zod";
+
+export const addPostgraduateProgramSchema = z.object({
+    name: z.string({
+        required_error: "Nombre requerido"
+    }).max(1000),
+
+    start_year: z.coerce.number({
+        required_error: "Año inicio requerido"
+    }).gt(0, {
+        message: "Ingrese una valor válido"
+    }),
+
+    end_year: z.coerce.number({
+        required_error: "Año fin requerido"
+    }).gt(0, {
+        message: "Ingrese una valor válido"
+    }),
+
+    is_active: z.boolean({
+        required_error: "Programa activo requerido"
+    }).default(true),
+});
+
+export const updatePostgraduateProgramSchema = z.object({
+    id: z.number().gt(0),
+
+    name: z.string({
+        required_error: "Nombre requerido"
+    }).max(1000),
+
+    start_year: z.coerce.number({
+        required_error: "Año inicio requerido"
+    }).gt(0, {
+        message: "Ingrese una valor válido"
+    }),
+
+    end_year: z.coerce.number({
+        required_error: "Año fin requerido"
+    }).gt(0, {
+        message: "Ingrese una valor válido"
+    }),
+
+    is_active: z.boolean({
+        required_error: "Programa activo requerido"
+    }).default(true),
+});
+
+export type AddPostgraduateProgramSchema = typeof addPostgraduateProgramSchema;
+export type UpdatePostgraduateProgramSchema = typeof updatePostgraduateProgramSchema;
