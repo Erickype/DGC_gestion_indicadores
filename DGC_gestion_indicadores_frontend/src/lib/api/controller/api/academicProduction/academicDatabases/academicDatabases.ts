35bd2d92a7fd6c855dcd859957287d5e31bae605
Erick Carrasco <erickype@hotmail.com>
2024-11-14 12:28:30 -0500

    Add LoadAcademicDatabasesWithComboMessages function

--- DGC_gestion_indicadores_frontend/src/lib/api/controller/api/academicProduction/academicDatabases/academicDatabases.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/api/academicProduction/academicDatabases/academicDatabases.ts
@@ -1,8 +1,9 @@
 import { getAcademicDatabasesRoute } from "$lib/api/routes/api/academicProduction/academicDatabases/academicDatabases";
 import type { AcademicDatabase } from "$lib/api/model/api/academicProduction/academicDatabases/academicDatabases";
 
+import { generateErrorFromCommonError, type CommonError } from "$lib/api/model/errors";
 import { generateCommonErrorFromFetchError } from "$lib/utils";
-import type { CommonError } from "$lib/api/model/errors";
+import type { Message } from "$lib/components/combobox/combobox";
 
 export async function GetAcademicDatabases(token: string) {
     try {
@@ -21,5 +22,25 @@ export async function GetAcademicDatabases(token: string) {
         return databases
     } catch (error) {
         return generateCommonErrorFromFetchError(error)
+    }
+}
+
+export async function LoadAcademicDatabasesWithComboMessages(token: string) {
+    const response = await GetAcademicDatabases(token);
+    if ((response as CommonError).status) {
+        throw generateErrorFromCommonError(response as CommonError)
+    }
+    const careers = response as AcademicDatabase[];
+    let messages: Message[] = []
+    messages = messages.concat(
+        careers.map((career) => ({
+            value: career.ID!,
+            label: career.name,
+        }))
+    );
+
+    return {
+        careers,
+        messages
     }
 }
