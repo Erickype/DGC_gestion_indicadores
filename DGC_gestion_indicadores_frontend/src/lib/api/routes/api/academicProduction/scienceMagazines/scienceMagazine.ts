d040666e2b72cca7f9ee99d585f188b17f186e44
Erick Carrasco <erickype@hotmail.com>
2024-11-14 16:20:05 -0500

    Fix getScienceMagazineFilterJoinedByScienceMagazineIDRoute route

--- DGC_gestion_indicadores_frontend/src/lib/api/routes/api/academicProduction/scienceMagazines/scienceMagazine.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/routes/api/academicProduction/scienceMagazines/scienceMagazine.ts
@@ -2,6 +2,6 @@ import { apiBaseURL } from "../../base";
 
 export const getScienceMagazinesRoute = apiBaseURL + "/scienceMagazines"
 export const postFilterScienceMagazinesRoute = apiBaseURL + "/scienceMagazines/filter"
-export const getScienceMagazineFilterJoinedByScienceMagazineIDRoute = apiBaseURL + "/scienceMagazines/filter/"
+export const getScienceMagazineFilterJoinedByScienceMagazineIDRoute = apiBaseURL + "/scienceMagazine/filter/"
 export const postScienceMagazineRoute = apiBaseURL + "/scienceMagazine"
 export const updateScienceMagazineRoute = apiBaseURL + "/scienceMagazine/"
