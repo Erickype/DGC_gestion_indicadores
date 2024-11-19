4a36874d6c3586b0a692c4007addda8be0a50a2d
Erick Carrasco <erickype@hotmail.com>
2024-11-18 19:45:52 -0500

    Add GetCalculateIndicatorsByPostgraduateCohortYear client api

--- DGC_gestion_indicadores_frontend/src/routes/api/indicators/postgraduate/calculate/[cohortYear]/+server.ts
+++ DGC_gestion_indicadores_frontend/src/routes/api/indicators/postgraduate/calculate/[cohortYear]/+server.ts
@@ -1,4 +1,4 @@
-import { GetIndicatorsByCohortYear } from "$lib/api/controller/api/indicators/postgraduateCohortYears";
+import { GetCalculateIndicatorsByPostgraduateCohortYear } from "$lib/api/controller/api/indicators/postgraduateCohortYears";
 
 import { generateErrorFromCommonError, type CommonError } from "$lib/api/model/errors";
 import { json, type RequestHandler } from "@sveltejs/kit";
@@ -6,7 +6,8 @@ import { json, type RequestHandler } from "@sveltejs/kit";
 export const GET: RequestHandler = async ({ cookies, params }) => {
     const token = cookies.get("AuthorizationToken")
     const cohortYear = params.cohortYear
-    const response = await GetIndicatorsByCohortYear(cohortYear!, token!)
+
+    const response = await GetCalculateIndicatorsByPostgraduateCohortYear(cohortYear!, token!)
 
     if ((response as CommonError).status_code) {
         return generateErrorFromCommonError(response as CommonError)
