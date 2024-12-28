70ad5027cc8bf12612db391bdf1c986a09b9ac25
Erick Carrasco <erickype@hotmail.com>
2024-12-28 12:40:38 -0500

    Improve error handling in DeleteEvaluationPeriod function and return appropriate response types

--- DGC_gestion_indicadores_frontend/src/lib/api/controller/admin/evaluationPeriod.ts
+++ DGC_gestion_indicadores_frontend/src/lib/api/controller/admin/evaluationPeriod.ts
@@ -1,5 +1,8 @@
+import type { CommonDeleteResponse } from "$lib/api/model/common";
+import type { CommonError } from "$lib/api/model/errors";
 import type { PostEvaluationPeriodRequest, UpdateEvaluationPeriodRequest } from "$lib/api/model/view/evaluationPeriod";
 import { deleteEvaluationPeriod, postEvaluationPeriod, updateEvaluationPeriod } from "$lib/api/routes/admin/evaluationPeriod";
+import { generateCommonErrorFromFetchError } from "$lib/utils";
 
 export async function PostEvaluationPeriod(period: PostEvaluationPeriodRequest, token: string) {
     return await fetch(postEvaluationPeriod, {
@@ -23,12 +26,22 @@ export async function UpdateEvaluationPeriod(period: UpdateEvaluationPeriodReque
     });
 }
 
-export async function DeleteEvaluationPeriod(id: string, token: string) {
-    return await fetch(deleteEvaluationPeriod + id, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': token
+export async function DeleteEvaluationPeriod(id: string, token: string): Promise<CommonDeleteResponse | CommonError> {
+    try {
+        const response = await fetch(deleteEvaluationPeriod + id, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            }
+        })
+        if (!response.ok) {
+            const error: CommonError = await response.json()
+            return error
         }
-    })
+        const author: CommonDeleteResponse = await response.json()
+        return author;
+    } catch (error) {
+        return generateCommonErrorFromFetchError(error)
+    }
 }
